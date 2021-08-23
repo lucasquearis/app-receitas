5cb0847b77b05912377520442e2848e9603f0e91
MGSiqueira <marcos.siqueira98@gmail.com>
2021-08-23 18:44:28 -0300

    Todas as pages e rotas criadas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Login, Drinks, Meals, MealDetails, DrinkDetails, MealInProgress,
+  DrinkInProgress, Explore, ExploreMeals, ExploreDrinks, ExploreMealByIngredients,
+  ExploreDrinkByIngredients, ExploreMealByArea, Profile, RecipesDone,
+  RecipesFavorites } from './pages';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          path="/comidas/:id/in-progress"
+          render={ (props) => <MealInProgress { ...props } /> }
+        />
+        <Route
+          path="/bebidas/:id/in-progress"
+          render={ (props) => <DrinkInProgress { ...props } /> }
+        />
+        <Route path="/comidas/:id" render={ (props) => <MealDetails { ...props } /> } />
+        <Route path="/bebidas/:id" render={ (props) => <DrinkDetails { ...props } /> } />
+        <Route
+          path="/explorar/comidas/ingredientes"
+          component={ ExploreMealByIngredients }
+        />
+        <Route
+          path="/explorar/bebidas/ingredientes"
+          component={ ExploreDrinkByIngredients }
+        />
+        <Route path="/explorar/bebidas/ingredientes" component={ ExploreMealByArea } />
+        <Route path="/explorar/comidas" component={ ExploreMeals } />
+        <Route path="/explorar/bebidas" component={ ExploreDrinks } />
+        <Route path="/receitas-feitas" component={ RecipesDone } />
+        <Route path="/receitas-favoritas" component={ RecipesFavorites } />
+        <Route path="/perfil" component={ Profile } />
+        <Route path="/comidas" component={ Meals } />
+        <Route path="/bebidas" component={ Drinks } />
+        <Route path="/explorar" component={ Explore } />
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
