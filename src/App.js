235c732768c70baa36525897ca95f52587328200
Armando <a.guerra58@hotmail.com>
2021-08-23 19:07:32 -0300

    32-Header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Provider from './provider/Provider';
 import Login from './pages/Login';
+import MainFoods from './pages/MainFoods';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <Provider>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/comidas" component={ MainFoods } />
+        <Route exact path="/perfil" component={ Profile } />
       </Switch>
     </Provider>
   );
---
+++ src/pages/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Profile() {
+  return (
+    <Header />
+  );
+}
+
+export default Profile;
