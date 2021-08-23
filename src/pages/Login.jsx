5f1408703bf6bf1dcffd15fff87e2b833a608a64
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-08-23 18:50:54 -0300

    Requisito 2 testado e aprovado localmente

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function Login() {
-  return <h1>oi</h1>;
+  return (
+    <div>
+      <TextField
+        data-testid="email-input"
+        id="standard-name"
+        label="Email"
+        // value={ email }
+        // onChange={ handleChange }
+      />
+      <TextField
+        data-testid="password-input"
+        id="standard-password-input"
+        label="Password"
+        // value={ password }
+        // onChange={ handleChange }
+        type="password"
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        data-testid="login-submit-btn"
+      >
+        Entrar
+      </Button>
+    </div>
+  );
 }
 
 export default Login;
