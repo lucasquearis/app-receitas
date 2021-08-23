04e2b2189ba1476266fb10056049193a2904e6f6
Matheus Gonçalves <55060810+matheusgbl@users.noreply.github.com>
2021-08-23 19:05:30 -0300

    Requisito 11

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
@@ -10,7 +11,12 @@ export default function Header(
     <header style={ { display: 'flex', justifyContent: 'space-around' } }>
       <nav>
         {!showProfileIcon
-          ? <img data-testid="profile-top-btn" src={ iconProfile } alt="Profile" />
+          ? (
+
+            <Link to="/perfil">
+              <img data-testid="profile-top-btn" src={ iconProfile } alt="Profile" />
+            </Link>
+          )
           : null}
       </nav>
       <nav>
