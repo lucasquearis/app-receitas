033005a2c827f9d02d5aeac651094f5f570d19bd
Ygor Saturnino <82230734+YgorSaturnino@users.noreply.github.com>
2021-08-23 17:30:26 -0300

    Revert "Faz merge dos primeiros arquivos"
    This reverts commit 4c0eeb0b217cfa8b1433688e360cce338ada8078.

---
+++ src/pages/Drinks.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Drinks() {
+  return (
+    <div />
+  );
+}
+
+export default Drinks;
---
+++ src/redux/actions/index.jsx
@@ -0,0 +1,2 @@
+export const TMP = 'TMP';
+export const action = (payload) => ({ type: TMP, payload });
