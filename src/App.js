3a55657e8f399e33e758c89519742ee8958daab7
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-23 17:15:19 -0300

    criando arquivo de Rotas, estrutura basica de Login e aplicando BrowserRouter em index

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Provider from './context/Provider';
+import Routes from './routes/Routes';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,16 +8,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Provider>
-      <div className="meals">
-        <span className="logo">TRYBE</span>
-        <object
-          className="rocksGlass"
-          type="image/svg+xml"
-          data={ rockGlass }
-        >
-          Glass
-        </object>
-      </div>
+      <Routes>
+        <div className="meals">
+          <span className="logo">TRYBE</span>
+          <object
+            className="rocksGlass"
+            type="image/svg+xml"
+            data={ rockGlass }
+          >
+            Glass
+          </object>
+        </div>
+      </Routes>
     </Provider>
   );
 }
---
+++ src/routes/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from '../pages/Login';
+
+function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+    </Switch>
+  );
+}
+
+export default Routes;
