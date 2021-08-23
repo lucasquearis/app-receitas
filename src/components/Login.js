07cc50f5f33d345074e21f94c70982f28ce4d6b5
Ramon Paschoal <rpaschoal90@gmail.com>
2021-08-23 18:27:29 -0300

    Requisito 3, 4, 5 feitos

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../context/Context';
 
 function Login() {
+  const { buttonDisabled, handleInputs, user } = useContext(Context);
+  console.log(buttonDisabled);
+
   return (
     <div>
       <label htmlFor="email-input">
         E-mail:
         <input
+          name="email"
           id="email-input"
           type="email"
           data-testid="email-input"
+          value={ user.email }
+          onChange={ handleInputs }
         />
       </label>
       Password:
       <label htmlFor="password-input">
         <input
+          name="password"
           id="password-input"
           type="password"
           data-testid="password-input"
+          value={ user.password }
+          onChange={ handleInputs }
         />
       </label>
-      <button type="button" data-testid="login-submit-btn">Entrar</button>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ buttonDisabled.disabled }
+      >
+        Entrar
+      </button>
     </div>
   );
 }
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
 
 function Provider({ children }) {
-  const context = {};
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState({
+    disabled: true,
+  });
+
+  const validButton = () => {
+    const { email, password } = user;
+    const { disabled } = buttonDisabled;
+    const rgeex = /(.*)@(.*).com/;
+    const magicNumber = 6;
+    const ifRegex = rgeex.test(email) && (password.length >= magicNumber);
+    console.log(ifRegex);
+    if (rgeex.test(email) && (password.length >= magicNumber)) {
+      setButtonDisabled({
+        disabled: false,
+      });
+    }
+    return disabled;
+  };
+
+  const handleInputs = ({ target }) => {
+    const { name, value } = target;
+    console.log(value);
+    setUser({
+      ...user,
+      [name]: value,
+    });
+    validButton();
+  };
+
+  const context = {
+    buttonDisabled,
+    handleInputs,
+    user,
+  };
   return (
     <Context.Provider value={ context }>
       { children }
