6f61a6b123579fcfeaac8b73602bb81312084961
CaioMorato <68545727+CaioMorato@users.noreply.github.com>
2021-08-23 20:34:38 -0300

    [Grupo 16] - Nuwanda - Iniciando projeto (#69)
    * criando context
    
    * provider feito
    
    * proptypes + provider feito e importado
    
    * finalizaçao setup
    
    * Update Homepage.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
+// vitals
 import React from 'react';
+import Routes from './Routes';
+// styles
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Routes />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+// vitals
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import RecipesProvider from './context/RecipesProvider';
+// styles
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <RecipesProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RecipesProvider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
