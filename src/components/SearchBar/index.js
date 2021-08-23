bc2b4e95ac94c611cacd6de8d83c1362813be641
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-23 18:03:16 -0400

    create search bar

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './style.css';
+
+import Button from '@material-ui/core/Button';
+import InputRadio from '../InputRadio';
+
+function SearchBar() {
+  return (
+    <div>
+      <input type="text" data-testid="search-input" />
+      <InputRadio
+        value="ingredient"
+        label="Ingrediente"
+        data-testid="ingredient-search-radio"
+      />
+      <InputRadio
+        value="name"
+        label="Nome"
+        data-testid="name-search-radio"
+      />
+      <InputRadio
+        value="first-letter"
+        label="Primeira Letra"
+        data-testid="first-letter-search-radio"
+      />
+      <Button
+        id="search-button"
+        variant="contained"
+        data-testid="exec-search-btn"
+      >
+        Pesquisar
+      </Button>
+    </div>
+  );
+}
+
+export default SearchBar;
