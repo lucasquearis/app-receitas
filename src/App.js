2afc9609ae880f04fc63aa2ce3515e88e5858862
thales <thalesdaniel2013@gmail.com>
2021-08-23 20:37:34 -0300

    feito estruturaçao final

--- src/App.js
+++ src/App.js
@@ -11,6 +11,21 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route to="/" component={ Login } />
+          <Route to="/comidas" component={ Login } />
+          <Route to="/bebidas" component={ Login } />
+          <Route to="/comidas/{id-da-receita}" component={ Login } />
+          <Route to="/bebidas/{id-da-receita}" component={ Login } />
+          <Route to="/comidas/{id-da-receita}/in-progress" component={ Login } />
+          <Route to="/bebidas/{id-da-receita}/in-progress" component={ Login } />
+          <Route to="/explorar" component={ Login } />
+          <Route to="/explorar/comidas" component={ Login } />
+          <Route to="/explorar/bebidas" component={ Login } />
+          <Route to="/explorar/comidas/ingredientes" component={ Login } />
+          <Route to="/explorar/bebidas/ingredientes" component={ Login } />
+          <Route to="/explorar/comidas/area" component={ Login } />
+          <Route to="/perfil" component={ Login } />
+          <Route to="/receitas-feitas" component={ Login } />
+          <Route to="/receitas-favoritas" component={ Login } />
         </Switch>
       </BrowserRouter>
     </Provider>
