7da09b91b379898b20f9308dba4c8f61afad77d2
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-08-23 18:54:59 -0300

    inputs do login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,34 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h2>Login</h2>
+      <div>
+        <form>
+          <label htmlFor="email-input">
+            Email:
+            <input
+              type="email"
+              placeholder="Insira seu email"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="password-input">
+            Senha:
+            <input
+              type="email"
+              placeholder="Digite sua senha"
+              data-testid="password-input"
+              disabled
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-btn"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Food from './pages/Food';
 import Home from './pages/Home';
 import Drink from './pages/Drink';
@@ -9,24 +9,26 @@ import FoodRecipe from './pages/FoodRecipe';
 
 function Routes() {
   return (
-    <Switch>
-      <Route path="/" component={ Home } />
-      <Route path="/comidas" component={ Food } />
-      <Route path="/bebidas" component={ Drink } />
-      <Route path={ `/comidas/${id}` } component={ null } />
-      <Route path={ `/bebidas/${id}` } component={ null } />
-      <Route path={ `/comidas/${id}/in-progress` } component={ null } />
-      <Route path={ `/bebidas/${id}/in-progress` } component={ null } />
-      <Route path="/explorar" component={ Explore } />
-      <Route path="/explorar/comidas" component={ null } />
-      <Route path="/explorar/bebidas" component={ null } />
-      <Route path="/explorar/comidas/ingredientes" component={ null } />
-      <Route path="/explorar/bebidas/ingredientes" component={ null } />
-      <Route path="/explorar/comidas/area" component={ null } />
-      <Route path="/perfil" component={ Perfil } />
-      <Route path="/receitas-feitas" component={ FoodRecipe } />
-      <Route path="/receitas-favoritas" component={ Perfil } />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={ Home } />
+        <Route path="/comidas" component={ Food } />
+        <Route path="/bebidas" component={ Drink } />
+        {/* <Route path={ `/comidas/${id}` } component={ null } />
+        <Route path={ `/bebidas/${id}` } component={ null } />
+        <Route path={ `/comidas/${id}/in-progress` } component={ null } />
+      <Route path={ `/bebidas/${id}/in-progress` } component={ null } /> */}
+        <Route path="/explorar" component={ Explore } />
+        <Route path="/explorar/comidas" component={ null } />
+        <Route path="/explorar/bebidas" component={ null } />
+        <Route path="/explorar/comidas/ingredientes" component={ null } />
+        <Route path="/explorar/bebidas/ingredientes" component={ null } />
+        <Route path="/explorar/comidas/area" component={ null } />
+        <Route path="/perfil" component={ Perfil } />
+        <Route path="/receitas-feitas" component={ FoodRecipe } />
+        <Route path="/receitas-favoritas" component={ Perfil } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
