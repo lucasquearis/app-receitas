be2cb71bb02ba2615a52d6b939dcb4644bfe675f
marcelaops <81485596+marcelaops@users.noreply.github.com>
2021-08-23 18:41:10 -0300

    [Grupo 15 ] Requisitos 2 ao 4 (#59)
    * iniciando, organizando
    
    * Tela Login - inputs e btn
    
    Co-authored-by: Marcela Silva <marcela.ops@hotmail.br>

---
+++ src/context/Provider.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Context from './Context';
+
+function Provider({ children }) {
+  const [user, setUser] = useState({
+    user: {
+      email: '',
+    },
+  });
+
+  const contextValue = { user, setUser };
+
+  return (
+    <Context.Provider value={ contextValue }>
+      {children}
+    </Context.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
