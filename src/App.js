428870bc14d24a562056abe5dbe74db7c03239fa
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-08-23 16:30:27 -0300

    Criadas paginas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import DetalheReceitaComida from './Pages/DetalheReceitaComida';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       >
         Glass
       </object>
+      <DetalheReceitaComida />
     </div>
   );
 }
---
+++ src/Pages/ExplorarComidaIngrediente.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ExplorarComidaIngrediente() {
+  return (
+    <h1>Tela de ExplorarComidaIngrediente</h1>
+  );
+}
