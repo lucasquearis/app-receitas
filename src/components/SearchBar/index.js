e44e79df1cf2496e7adf0046ca40665089b2c145
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-23 18:16:24 -0400

    add radio group and class

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -2,27 +2,30 @@ import React from 'react';
 import './style.css';
 
 import Button from '@material-ui/core/Button';
+import RadioGroup from '@material-ui/core/RadioGroup';
 import InputRadio from '../InputRadio';
 
 function SearchBar() {
   return (
-    <div>
+    <div className="search-bar-container">
       <input type="text" data-testid="search-input" />
-      <InputRadio
-        value="ingredient"
-        label="Ingrediente"
-        data-testid="ingredient-search-radio"
-      />
-      <InputRadio
-        value="name"
-        label="Nome"
-        data-testid="name-search-radio"
-      />
-      <InputRadio
-        value="first-letter"
-        label="Primeira letra"
-        data-testid="first-letter-search-radio"
-      />
+      <RadioGroup row name="seatch-bar-input-radio" aria-label="gender">
+        <InputRadio
+          value="ingredient"
+          label="Ingrediente"
+          data-testid="ingredient-search-radio"
+        />
+        <InputRadio
+          value="name"
+          label="Nome"
+          data-testid="name-search-radio"
+        />
+        <InputRadio
+          value="first-letter"
+          label="Primeira letra"
+          data-testid="first-letter-search-radio"
+        />
+      </RadioGroup>
       <Button
         id="search-button"
         variant="contained"
