7292a2fb7f054973aac3fe7097f0d494bd5183de
Adryan Rosa <adryanrosa374@gmail.com>
2021-08-23 17:03:21 -0300

    Adiciona verificação e redirecionamento para /comidas

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 import { InputGroup, FormControl, Button } from 'react-bootstrap';
 
 function Login() {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [isValid, setIsValid] = React.useState(false);
+  const [shouldRedirect, setShouldRedirect] = React.useState(false);
+
+  React.useEffect(() => {
+    const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // Regular expression from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript"
+    const PASSWORD_MIN_CHARS = 6;
+
+    setIsValid(EMAIL_REGEX.test(email) && password.length >= PASSWORD_MIN_CHARS);
+  }, [password, email]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const user = {
+      email,
+    };
+
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify(user));
+    setShouldRedirect(true);
+  };
+
+  if (shouldRedirect) return <Redirect to="/comidas" />;
 
   return (
-    <form className="p-3">
+    <form className="p-3" onSubmit={ (event) => handleSubmit(event) }>
       <InputGroup className="mb-2">
         <FormControl
           data-testid="email-input"
@@ -27,11 +53,11 @@ function Login() {
       <Button
         data-testid="login-submit-btn"
         type="submit"
-        variant="dark"
+        disabled={ !isValid }
+        variant="primary"
         className="btn-block"
-        disabled="true"
       >
-        Block level button
+        Entrar
       </Button>
     </form>
   );
