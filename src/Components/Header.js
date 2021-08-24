fe5a9edf5ded5905aa7258f3fe8c42c45d58442b
Josimar Souza <contact.josimarsouza@gmail.com>
2021-08-24 11:19:38 -0300

    Implementa click no icone de busca

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import PerfilIcon from '../images/profileIcon.svg';
 import SearchIcon from '../images/searchIcon.svg';
 
 export default function Header(props) {
+  const [showSearch, setShowSearch] = useState(false);
+
   const { title, searchIcon } = props;
+
+  const handleSearchClick = () => {
+    setShowSearch(!showSearch);
+  };
+
+  const showSearchIcon = () => {
+    if (searchIcon) {
+      return (
+        <button
+          type="button"
+          onClick={ handleSearchClick }
+        >
+          <img src={ SearchIcon } alt="procurar" />
+        </button>
+      );
+    }
+
+    return (
+      <div />
+    );
+  };
+
   return (
     <header className="header">
-      <img data-testid="profile-top-btn" src={ PerfilIcon } alt="perfil" />
+      <Link to="/perfil">
+        <img data-testid="profile-top-btn" src={ PerfilIcon } alt="perfil" />
+      </Link>
       <h3 data-testid="page-title">{ title }</h3>
-      { searchIcon ? <img src={ SearchIcon } alt="procurar" /> : <div /> }
+      { showSearchIcon() }
     </header>
   );
 }
