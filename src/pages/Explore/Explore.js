45b3a3b0db041da2099d1d060c7d24322762e7d8
Marcela Silva <marcela.ops@hotmail.br>
2021-08-24 18:20:44 -0300

    resolve requisitos 67, 68 e 69 - `Explore.js`

--- src/pages/Explore/Explore.js
+++ src/pages/Explore/Explore.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 
 function Foods() {
+  const history = useHistory();
+
   return (
-    <Header name="Explorar" search={ false } />
+    <div>
+      <Header name="Explorar" search={ false } />
+      <button
+        data-testid="explore-food"
+        type="button"
+        onClick={ () => history.push('/explorar/comidas') }
+      >
+        Explorar Comidas
+
+      </button>
+      <button
+        data-testid="explore-drinks"
+        type="button"
+        onClick={ () => history.push('/explorar/bebidas') }
+      >
+        Explorar Bebidas
+
+      </button>
+    </div>
   );
 }
 
