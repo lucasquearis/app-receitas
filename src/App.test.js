4daec1bde1e0515cd39b7dca0fe06a36eea06040
Caroline Benichio Teixeira <78769105+carolbenichio@users.noreply.github.com>
2021-08-24 16:23:37 -0300

    Tela de Login finalizada (#162)
    * Requisito 3, 4 e 5 - emails, senhas e botao
    
    * Requisito 6, 7, 8 - localStorage e link

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import index from './index';
 
 test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<index />);
   const linkElement = getByText(/TRYBE/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,65 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 
 function Login() {
+  const SEIS = 6;
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = ({ target }) => {
+    setUser({
+      ...user,
+      [target.name]: target.value,
+    });
+  };
+
+  const emailIsValid = (email) => {
+    const pattern = /\S+@\S+\.\S+/;
+    return pattern.test(email);
+  };
+
+  const handleClick = () => {
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({
+      email: user.email,
+    }));
+  };
+
   return (
     <div>
-      <TextField
+      <input
         data-testid="email-input"
         id="standard-name"
         label="Email"
-        // value={ email }
-        // onChange={ handleChange }
+        value={ user.email }
+        name="email"
+        onChange={ handleChange }
       />
-      <TextField
+      <input
         data-testid="password-input"
         id="standard-password-input"
         label="Password"
-        // value={ password }
-        // onChange={ handleChange }
+        value={ user.password }
+        name="password"
+        onChange={ handleChange }
         type="password"
       />
-      <Button
-        variant="contained"
-        color="primary"
-        data-testid="login-submit-btn"
-      >
-        Entrar
-      </Button>
+      <Link to="/comidas">
+        <Button
+          type="submit"
+          variant="contained"
+          disabled={ !(emailIsValid(user.email) && user.password.length > SEIS) }
+          color="primary"
+          data-testid="login-submit-btn"
+          onClick={ handleClick }
+        >
+          Entrar
+        </Button>
+      </Link>
     </div>
   );
 }
