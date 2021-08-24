7079e671cc8c600ed947f7d582488ff3c7aa124d
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-24 19:12:13 -0300

    adicionando testes para verificar rotas ao clicar nos icones

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -17,4 +17,31 @@ describe('Testes para o Footer', () => {
     expect(drinkIcon).toBeInTheDocument();
     expect(exploreIcon).toBeInTheDocument();
   });
+
+  it('Verifica se ao clicar no icone de bebidas vai a pagina correta', () => {
+    const { history } = renderWithRouter(<Footer />);
+    const drinkIcon = screen.getByTestId(/drinks-bottom-btn/i);
+    userEvent.click(drinkIcon);
+    const { location: { pathname } } = history;
+
+    expect(pathname).toBe('/bebidas');
+  });
+
+  it('Verifica se ao clicar no icone de comidas vai a pagina correta', () => {
+    const { history } = renderWithRouter(<Footer />);
+    const mealIcon = screen.getByTestId(/food-bottom-btn/i);
+    userEvent.click(mealIcon);
+    const { location: { pathname } } = history;
+
+    expect(pathname).toBe('/comidas');
+  });
+
+  it('Verifica se ao clicar no icone de explorar vai a pagina correta', () => {
+    const { history } = renderWithRouter(<Footer />);
+    const exploreIcon = screen.getByTestId(/explore-bottom-btn/i);
+    userEvent.click(exploreIcon);
+    const { location: { pathname } } = history;
+
+    expect(pathname).toBe('/explorar');
+  });
 });
