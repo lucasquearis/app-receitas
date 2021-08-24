b5787870febcbb213d738e377f797685e572fa07
Daniel Batista <62209642+danielbped@users.noreply.github.com>
2021-08-24 17:48:02 -0300

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from '../components/Header';
 import { useHistory } from 'react-router-dom';
+import Header from '../components/Header';
 
 function Profile() {
   const user = JSON.parse(localStorage.getItem('user'));
