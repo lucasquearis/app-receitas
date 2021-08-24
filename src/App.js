395a0c8a3f32abf447836b562e965b81a3bef05f
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-24 17:34:37 -0300

    colocando footer embaixo da tela

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Provider from './context/Provider';
 import Routes from './routes/Routes';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Provider>
       <Routes />
+      <Footer />
     </Provider>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,19 @@ import { Link } from 'react-router-dom';
 import mealIcon from '../images/mealIcon.svg';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
+import './Footer.css';
 
 function Footer() {
   return (
-    <footer data-testid="footer">
-      <Link to="/" data-testid="drinks-bottom-btn">
-        { drinkIcon }
+    <footer data-testid="footer" className="app-footer">
+      <Link to="/bebidas" data-testid="drinks-bottom-btn">
+        <img src={ drinkIcon } alt="icone bebidas" />
       </Link>
-      <Link to="/" data-testid="explore-bottom-btn">
-        { exploreIcon }
+      <Link to="/explorar" data-testid="explore-bottom-btn">
+        <img src={ exploreIcon } alt="icone bebidas" />
       </Link>
-      <Link to="/" data-testid="food-bottom-btn">
-        { mealIcon }
+      <Link to="/comidas" data-testid="food-bottom-btn">
+        <img src={ mealIcon } alt="icone bebidas" /> 
       </Link>
     </footer>
   );
