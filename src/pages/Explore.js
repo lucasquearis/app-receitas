da78225a62e974312e74f1c0517ec3988fe18b1c
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-08-24 17:49:57 -0300

    adiciona botoes e suas respectivas rotas na pagina de explorar

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 function Explore() {
   return (
     <div>
-      <p>Explore</p>
+      <h1>Explore:</h1>
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-food">Explorar Comidas</button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+      </Link>
       <Footer />
     </div>
   );
