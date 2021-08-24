6b1d1914f7e86384c1f25dd860ec7ee37e54cc1b
thales <thalesdaniel2013@gmail.com>
2021-08-24 19:25:45 -0300

    feito testes do login

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import event from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import Login from '../pages/Login';
 
@@ -13,6 +13,23 @@ describe('Testando pagina Login', () => {
 
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
-    expect(submitButton).toBeDisable();
+    expect(submitButton).toBeDisabled();
+  });
+
+  it('verifica se apos o preenchimento dos inputs o botão é habilitado', () => {
+    const { history } = renderWithRouter(<Login />);
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const submitButton = screen.getByTestId('login-submit-btn');
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(submitButton).toBeDisabled();
+    userEvent.type(emailInput, 'email@email.com');
+    userEvent.type(passwordInput, '1324141');
+    expect(submitButton).not.toBeDisabled();
+    userEvent.click(submitButton);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/comidas');
   });
 });
