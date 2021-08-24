1245379112f4e2f7361f6a612174b99cb1ea2407
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-24 18:00:34 -0400

    update context with recipes hooks

--- src/Context/ContextApp.js
+++ src/Context/ContextApp.js
@@ -1,13 +1,24 @@
 import React, { createContext } from 'react';
 import PropTypes from 'prop-types';
 
+import recipesHooks from '../Hooks/recipesHooks';
+
 export const ContextApp = createContext();
 
-export const AppProvider = ({ children }) => (
-  <ContextApp.Provider value="value">
-    {children}
-  </ContextApp.Provider>
-);
+export const AppProvider = ({ children }) => {
+  const { searchRecipes, recipes } = recipesHooks();
+
+  const ContProps = {
+    recipes,
+    searchRecipes,
+  };
+
+  return (
+    <ContextApp.Provider value={ ContProps }>
+      {children}
+    </ContextApp.Provider>
+  );
+};
 
 AppProvider.propTypes = {
   children: PropTypes.node.isRequired,
