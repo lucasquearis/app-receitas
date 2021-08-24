04890bd5fffda1bc6ae8ea587d087e9b1178a92f
Julio Pereira <jandrade.pereira1@gmail.com>
2021-08-24 17:26:56 -0300

    Requisito 9 - Refatora Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-function Header() {
+function Header({ title, loading }) {
+  const [showBar, setShowBar] = useState(false);
+
+  const renderSearchBar = () => (showBar ? setShowBar(false) : setShowBar(true));
+
+  if (loading) {
+    return (
+      <header className="header-container">
+        <Link to="/perfil">
+          <img
+            data-testid="profile-top-btn"
+            src={ profileIcon }
+            alt="profile"
+          />
+        </Link>
+        <h2 data-testid="page-title">{ title }</h2>
+        <Link to="/">
+          <button
+            type="button"
+            data-test-id="search-top-btn"
+            onClick={ renderSearchBar }
+          >
+            <img src={ searchIcon } alt="search icon" />
+          </button>
+        </Link>
+      </header>
+    );
+  }
+
   return (
-    <header>
-      <Link data-testid="profile-top-btn" to="/perfil">
-        <img src={ profileIcon } alt="profile" />
-      </Link>
-      <h2 data-testid="page-title">Aqui sera dinamico</h2>
-      <Link to="/">
-        <button
-          type="button"
-          data-test-id="search-top-btn"
-        >
-          <img src={ searchIcon } alt="search icon" />
-        </button>
+    <header className="header-container">
+      <Link to="/perfil">
+        <img
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+          alt="profile"
+        />
       </Link>
     </header>
   );
 }
+
+Header.propTypes = {
+  title: PropTypes.string,
+}.isRequired;
 
 export default Header;
