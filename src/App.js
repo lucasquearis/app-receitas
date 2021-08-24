68d4e289ac6ce028001b1c881f91af3e74dd853b
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-08-24 19:15:52 -0300

    feito todos os testes da pagina login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Provider from './context/MyContextProvider';
