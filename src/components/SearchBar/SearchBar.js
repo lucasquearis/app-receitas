8dce7788f36b941da97329f8c94aab1e545b3f5e
Pedro <peg.gep27@gmail.com>
2021-08-24 18:57:03 -0300

    Requisito 16 - Correçao de diretorio

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import { searchOnClick } from '../utils';
+import { searchOnClick } from './utils';
 
 const SearchBar = () => {
   const [search, setSearch] = useState({
