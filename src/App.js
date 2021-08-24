956539a17d62b1b81effc233c79ddf038bbcdbdd
Teika <shimworks.tk@gmail.com>
2021-08-24 18:59:20 -0300

    faz parte do req 33

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router';
 import { AppProvider } from './Context/ContextApp';
 import Login from './Pages/Login/Login';
 import Comidas from './Pages/Comidas/Comidas';
+import RecipeDetails from './Pages/RecipeDetails/RecipeDetails';
 
 function App() {
   return (
     <AppProvider>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/:feedType/:id" component={ RecipeDetails } />
         <Route path="/comidas" component={ Comidas } />
       </Switch>
     </AppProvider>
--- src/Components/Btn.jsx
+++ src/Components/Btn.jsx
@@ -2,25 +2,15 @@ import { Button } from '@material-ui/core';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Btn({ testid, name, disabled, handleClick }) {
-  const props = {
-    name,
-    'data-testid': testid,
-    type: 'button',
-    variant: 'contained',
-    disabled,
-    onClick: handleClick,
-  };
+function Btn(props) {
+  const { name } = props;
   return (
     <Button { ...props }>{name}</Button>
   );
 }
 
 Btn.propTypes = {
-  testid: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  disabled: PropTypes.bool.isRequired,
-  handleClick: PropTypes.func.isRequired,
 };
 
 export default Btn;
