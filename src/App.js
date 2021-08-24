65afd10cc00d114d1c263e4825e8b7c1bf089037
Erick Silva <erick.silva6653@gmail.com>
2021-08-24 20:12:20 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import {
-  Meals,
-  Login,
-  Profile,
-  Drinks,
-  RecipeDetails } from './pages';
+import { Meals, Login, Profile, Drinks, RecipeDetails } from './pages';
 import Provider from './context/Provider';
 
 function App() {
