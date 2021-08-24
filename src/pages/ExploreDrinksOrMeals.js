6c31fb57a745c614b6808f97dbc1b20f94f95434
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-08-24 20:29:35 -0300

    adiciona condicional e  botoes na pagina de explorar comida/bebida

--- src/pages/ExploreDrinksOrMeals.js
+++ src/pages/ExploreDrinksOrMeals.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 function ExploreDrinksOrMeals() {
+  const { pathname } = useLocation();
+  if (pathname === '/explorar/comidas') {
+    return (
+      <div>
+        <p>Explore Drinks or Meals</p>
+        <Link to="/explorar/comidas/ingredientes">
+          <button type="button" data-testid="explore-by-ingredient">
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to="/explorar/comidas/area">
+          <button type="button" data-testid="explore-by-area">
+            Por Local de Origem
+          </button>
+        </Link>
+        <Link to="/explorar/comidas">
+          <button type="button" data-testid="explore-surprise">
+            Me Surpreenda!
+          </button>
+        </Link>
+        <Footer />
+      </div>
+    );
+  }
   return (
     <div>
-      <p>Explore Drinks or Meals</p>
+      <Link to="/explorar/bebidas/ingredientes">
+        <button type="button" data-testid="explore-by-ingredient">
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-surprise">
+          Me Surpreenda!
+        </button>
+      </Link>
       <Footer />
     </div>
   );
