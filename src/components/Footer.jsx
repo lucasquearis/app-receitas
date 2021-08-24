8fecf5c527cbfd9f1768e1c545fb7312d25e1755
Luís Seixas <luiseixas2@gmail.com>
2021-08-24 20:28:34 -0300

    add req 20

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import '../App.css';
 
 export default function Footer() {
   return (
-    <footer data-testid="footer">
+    <footer data-testid="footer" className="footer">
+
       <Link to="/bebidas">
-        <button type="button" data-testid="drinks-bottom-btn">DRINK</button>
+        <img
+          src={ drinkIcon }
+          alt="imagem-drink"
+          data-testid="drinks-bottom-btn"
+        />
       </Link>
+
       <Link to="/explorar">
-        <button type="button" data-testid="explore-bottom-btn"> EXPLORAR </button>
+        <img
+          src={ exploreIcon }
+          alt="imagem-explorar"
+          data-testid="explore-bottom-btn"
+        />
       </Link>
+
       <Link to="/comidas">
-        <button type="button" data-testid="food-bottom-btn"> FOOD </button>
+        <img
+          src={ mealIcon }
+          alt="imagem-comida"
+          data-testid="food-bottom-btn"
+        />
       </Link>
     </footer>
   );
