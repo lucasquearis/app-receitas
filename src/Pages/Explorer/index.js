7ece71f93c186e8e76a516cdd54a4a9969cdefc9
Halysson2 <halyssonsena83@gmail.comgit config --global user,name Halysson2padim123git config --global user.email halyssonsena83@gmail.com>
2021-08-24 18:51:14 -0300

    Tela de Explorar - requisitos 67, 68 e 69

--- src/Pages/Explorer/index.js
+++ src/Pages/Explorer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
@@ -6,6 +7,12 @@ export default function Explorer() {
   return (
     <div>
       <Header>Explorar</Header>
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-food">Explorar Comidas</button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+      </Link>
       <Footer />
     </div>
   );
