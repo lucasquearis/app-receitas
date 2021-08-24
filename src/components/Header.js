17edac643938fb9df36b98ba86e253e34410ccd3
Caio Morato <morato_dev@protonmail.com>
2021-08-24 13:41:49 -0300

    parte onde sera colocado o componente searchBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 // vitals
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 // styles
 import '../styles/Header.css';
 import profilePicture from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 const renderTitle = (path) => {
   if (path.endsWith('/ingredientes')) return 'Explorar Ingredientes';
@@ -23,23 +24,39 @@ const renderTitle = (path) => {
 };
 
 function Header() {
+  const [showSearchBar, setShowSearchBar] = useState(false);
   const history = useHistory();
   const { location: { pathname } } = history;
-  console.log(pathname);
 
   return (
-    <header className="header-div">
+    <>
+      <header className="header-div">
+        <div>
+          <img
+            src={ profilePicture }
+            alt="Ícone que indica o botão pra ir para o perfil de usuário"
+          />
+        </div>
+        <div>
+          <h1>{renderTitle(pathname)}</h1>
+        </div>
+        <button
+          type="button"
+          className="search-button"
+          onClick={ () => { setShowSearchBar(!showSearchBar); } }
+        >
+          <img
+            src={ searchIcon }
+            alt="Ícone que indica o botão pra ativar a barra de busca"
+          />
+        </button>
+      </header>
       <div>
-        <img
-          src={ profilePicture }
-          alt="Ícone que indica o botão pra ir para o perfil de usuário"
-        />
+        {showSearchBar
+          ? 'SearchBar Component'
+          : 'not showing '}
       </div>
-      <div>
-        <h1>{renderTitle(pathname)}</h1>
-      </div>
-      <div>Botão de busca</div>
-    </header>
+    </>
   );
 }
 
