4ce5516a81b650292b5253129b42a695b90fc0c8
thiagopcdev <thiagopc_dev@hotmail.com>
2021-08-24 12:22:53 -0300

    Adiciona DataContext

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './routes';
+import DataProvider from './Context/DataContext';
 
 function App() {
-  return <Routes />;
+  return (
+    <DataProvider>
+      <Routes />
+    </DataProvider>
+  );
 }
 
 export default App;
