e99756cbbff3d1b15c2ab2b3d1c00de8d5349cb8
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-24 17:29:28 -0300

    adicinando imagens aos links do footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import mealIcon from '../images/mealIcon.svg';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
 
 function Footer() {
   return (
-    <footer>
-
+    <footer data-testid="footer">
+      <Link to="/" data-testid="drinks-bottom-btn">
+        { drinkIcon }
+      </Link>
+      <Link to="/" data-testid="explore-bottom-btn">
+        { exploreIcon }
+      </Link>
+      <Link to="/" data-testid="food-bottom-btn">
+        { mealIcon }
+      </Link>
     </footer>
   );
 }
