766b929b7727c2c2e97d22344b195885f5c2ac0e
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-24 02:10:14 -0300

    teste para verificar elementos na tela

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from './App';
 
-test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testes tela de Login', () => {
+  it('verifica se existem 2 inputs e 1 botao na tela', () => {
+    renderWithRouter(<App />);
+    const emailInput = screen.getByTestId(/email-input/i);
+    const passwordInput = screen.getByTestId(/password-input/i);
+    const loginButton = screen.getByTestId(/login-submit-btn/i);
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+  });
 });
