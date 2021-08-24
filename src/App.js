531de7f8e2a70a324948cc947ed65798e2d95cf3
Eder Santos <3derms@gmail.com>
2021-08-24 16:18:51 -0300

    Cria pagina Explorar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Route } from 'react-router-dom';
 import Provider from './context/Provider';
 import Login from './pages/Login';
 import Comidas from './pages/Comidas';
+import Explorar from './pages/Explorar';
 
 function App() {
   return (
     <Provider>
       <Route exact path="/" component={ Login } />
       <Route path="/comidas" component={ Comidas } />
+      <Route path="/explorar/" component={ Explorar } />
     </Provider>
   );
 }
---
+++ src/pages/Explorar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Explorar() {
+  return (
+    <div>
+      <p
+        data-testid="explore-food"
+      >
+        Explore Food
+      </p>
+      <p
+        type="button"
+        data-testid="explore-drinks"
+      >
+        Explore Drinks
+      </p>
+    </div>
+  );
+}
