349d5d77440ac0ad72ae27db436a5f23e7c5e03a
thales <thalesdaniel2013@gmail.com>
2021-08-24 16:24:05 -0300

    teste inicial de login incompleto

---
+++ src/tests/Login.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import event from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import Login from '../pages/Login';
+
+describe('Testando pagina Login', () => {
+  it('verifica se contem os inputs de textos e o botão submit', () => {
+    renderWithRouter(<Login />);
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const submitButton = screen.getByTestId('login-submit-btn');
+
+    expect(emailInput).toHaveLength(1);
+    expect(passwordInput).toHaveLength(1);
+    expect(submitButton).toBeDisable();
+  });
+});
