707034bdff6e80d03688e1c3a8cdfcd1eaeca955
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-08-24 17:29:09 -0300

    corrige alt da imagem

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,13 @@ function Footer() {
   return (
     <footer className="footer" data-testid="footer">
       <Link to="/bebidas">
-        <img src={ drinkIcon } alt="profile-icon" data-testid="drinks-bottom-btn" />
+        <img src={ drinkIcon } alt="drink-icon" data-testid="drinks-bottom-btn" />
       </Link>
       <Link to="/explorar">
-        <img src={ exploreIcon } alt="profile-icon" data-testid="explore-bottom-btn" />
+        <img src={ exploreIcon } alt="explore-icon" data-testid="explore-bottom-btn" />
       </Link>
       <Link to="/comidas">
-        <img src={ mealIcon } alt="profile-icon" data-testid="food-bottom-btn" />
+        <img src={ mealIcon } alt="meal-icon" data-testid="food-bottom-btn" />
       </Link>
     </footer>
   );
