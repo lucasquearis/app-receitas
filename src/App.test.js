76926a13d6d1dda1629be7b1cf4c14bc96087cc2
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-24 02:42:17 -0300

    testes para botao desabilitado, rota e local storage

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from './App';
 
 describe('Testes tela de Login', () => {
-  it('verifica se existem 2 inputs e 1 botao na tela', () => {
+  const emailMock = 'test@test.test';
+  const wrongEmailMock = 'test.test.test';
+  const passwordMock = 'aaaaaaa';
+  const wrongPasswordMock = 'aaaaaa';
+
+  it('Verifica se existem 2 inputs e 1 botao na tela', () => {
     renderWithRouter(<App />);
     const emailInput = screen.getByTestId(/email-input/i);
     const passwordInput = screen.getByTestId(/password-input/i);
@@ -13,5 +19,59 @@ describe('Testes tela de Login', () => {
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
     expect(loginButton).toBeInTheDocument();
+  });
+
+  it('Verifica que o botao fica desabilitado ate usar email e password validos', () => {
+    renderWithRouter(<App />);
+    const emailInput = screen.getByTestId(/email-input/i);
+    const passwordInput = screen.getByTestId(/password-input/i);
+    const loginButton = screen.getByTestId(/login-submit-btn/i);
+    expect(loginButton).toBeDisabled();
+
+    userEvent.type(emailInput, wrongEmailMock);
+    userEvent.type(passwordInput, wrongPasswordMock);
+    expect(loginButton).toBeDisabled();
+
+    userEvent.type(emailInput, emailMock);
+    expect(loginButton).toBeDisabled();
+
+    userEvent.type(passwordInput, passwordMock);
+    expect(loginButton).not.toBeDisabled();
+  });
+
+  it('Verifica que vai para pagina comidas ao clicar no botao Entrar', () => {
+    const { history } = renderWithRouter(<App />);
+    const emailInput = screen.getByTestId(/email-input/i);
+    const passwordInput = screen.getByTestId(/password-input/i);
+    const loginButton = screen.getByTestId(/login-submit-btn/i);
+
+    userEvent.type(emailInput, emailMock);
+    userEvent.type(passwordInput, passwordMock);
+    userEvent.click(loginButton);
+
+    const { location: { pathname } } = history;
+
+    expect(pathname).toBe('/comidas');
+  });
+
+  it('Verifica que as informacoes no local Storage ao clicar no botao', () => {
+    renderWithRouter(<App />);
+    const emailInput = screen.getByTestId(/email-input/i);
+    const passwordInput = screen.getByTestId(/password-input/i);
+    const loginButton = screen.getByTestId(/login-submit-btn/i);
+
+    userEvent.type(emailInput, emailMock);
+    userEvent.type(passwordInput, passwordMock);
+    userEvent.click(loginButton);
+
+    const mealsToken = localStorage.getItem('mealsToken');
+    const cocktailsToken = localStorage.getItem('cocktailsToken');
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    console.log(user);
+
+    expect(mealsToken).toBe('1');
+    expect(cocktailsToken).toBe('1');
+    expect(user).toStrictEqual({ email: emailMock });
   });
 });
