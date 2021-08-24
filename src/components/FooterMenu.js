e777b5e01e5a52926157148b605d4e08799a9cd4
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-24 12:50:54 -0300

    implementa links para redirecionar suas rotas

--- src/components/FooterMenu.js
+++ src/components/FooterMenu.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 const FooterMenu = () => (
   <footer data-testis="footer">
-    <Button
-      type="button"
-      data-testid="drinks-bottom-btn"
-    >
-      <img src={ drinkIcon } alt="Botão drink" />
-    </Button>
-    <Button
-      type="button"
-      data-testid="explore-bottom-btn"
+    <Link to="/bebidas">
+      <Button
+        type="button"
+        data-testid="drinks-bottom-btn"
+      >
+        <img src={ drinkIcon } alt="Botão drink" />
+      </Button>
+    </Link>
+    <Link to="/explorar">
+      <Button
+        type="button"
+        data-testid="explore-bottom-btn"
 
-    >
-      <img src={ exploreIcon } alt="Botão explorar" />
-    </Button>
-    <Button
-      type="button"
-      data-testid="food-bottom-btn"
-    >
-      <img src={ mealIcon } alt="Botão refeição" />
-    </Button>
+      >
+        <img src={ exploreIcon } alt="Botão explorar" />
+      </Button>
+    </Link>
+    <Link to="/comidas">
+      <Button
+        type="button"
+        data-testid="food-bottom-btn"
+      >
+        <img src={ mealIcon } alt="Botão refeição" />
+      </Button>
+    </Link>
   </footer>
 );
 
