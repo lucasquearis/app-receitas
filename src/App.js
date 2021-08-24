a61375da900ec0381be3d36aad26c50142b36a15
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-08-24 18:12:48 -0300

    grupo 17 - implementa o Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import Comidas from './pages/comidas/Comidas';
 import Login from './pages/login/Login';
+import Perfil from './pages/perfil/Perfil';
+import Bebidas from './pages/bebidas/Bebidas';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/comidas" component={ Comidas } />
+      <Route path="/perfil" component={ Perfil } />
+      <Route path="/bebidas" component={ Bebidas } />
     </Switch>
   );
 }
---
+++ src/pages/bebidas/Bebidas.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Header from '../../components/Header';
+
+export default class Bebidas extends Component {
+  render() {
+    return (
+      <Header titulo="Bebidas" />
+    );
+  }
+}
