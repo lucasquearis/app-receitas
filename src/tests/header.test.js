b246c1ffbc31e20f7120600c648d3783fc7165e4
Erick Silva <erick.silva6653@gmail.com>
2021-08-24 12:52:37 -0300

    arruma testes renderizando pelo App

--- src/tests/header.test.js
+++ src/tests/header.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen, fireEvent } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
-import Meals from '../pages/Meals';
+import App from '../App';
 
 const PROFILE_BTN_SELECTOR = 'profile-top-btn';
 const SEARCH_BTN_SELECTOR = 'search-top-btn';
@@ -9,22 +9,27 @@ const PAGE_TITLE_SELECTOR = 'page-title';
 
 describe('Header tests', () => {
   it('Should have a header with profile icon, title and search icon', () => {
-    renderWithRouter(<Meals />);
-    expect(screen.getByTestId(PROFILE_BTN_SELECTOR)).toBeInTheDocument();
-    expect(screen.getByTestId(PAGE_TITLE_SELECTOR)).toBeInTheDocument();
-    expect(screen.getByTestId(SEARCH_BTN_SELECTOR)).toBeInTheDocument();
+    const { history } = renderWithRouter(<App />);
+    history.push('/comidas');
+    expect(screen.findByTestId(PROFILE_BTN_SELECTOR)).toBeDefined();
+    expect(screen.findByTestId(PAGE_TITLE_SELECTOR)).toBeDefined();
+    expect(screen.findByTestId(SEARCH_BTN_SELECTOR)).toBeDefined();
   });
 
-  it('Redirect to profile page when clicking profile button', () => {
-    const { history } = renderWithRouter(<Meals />);
-    fireEvent.click(screen.getByTestId(PROFILE_BTN_SELECTOR));
+  it('Redirect to profile page when clicking profile button', async () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/comidas');
+    const profileBtn = await screen.findByTestId(PROFILE_BTN_SELECTOR);
+    fireEvent.click(profileBtn);
     const { pathname } = history.location;
     expect(pathname).toBe('/perfil');
   });
 
-  it('Show search bar when clicking search icon', () => {
-    renderWithRouter(<Meals />);
-    fireEvent.click(screen.getByTestId(SEARCH_BTN_SELECTOR));
-    expect(screen.getByTestId('search-input')).toBeInTheDocument();
+  it('Show search bar when clicking search icon', async () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/comidas');
+    const searchBtn = await screen.findByTestId(SEARCH_BTN_SELECTOR);
+    fireEvent.click(searchBtn);
+    expect(screen.findByTestId('search-input')).toBeDefined();
   });
 });
