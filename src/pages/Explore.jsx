ee18e626cbb07522205bbf8e90ee4e46a8c3d666
ryanlaiber <43113152+ryanlaiber@users.noreply.github.com>
2021-08-24 17:58:34 -0300

    [GRUPO 27][RYAN LAIBER][REQUISITOS 67, 68 e 69] (#184)
    * adiciona logica de redirecionamento baseado em length da lista meals
    
    * implementa requisitos 67, 68 e 69

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Header, Footer } from '../components';
 
 function Explore() {
   return (
     <div>
       <Header title="Explorar" />
+      <Link data-testid="explore-food" to="/explorar/comidas">Explorar Comidas</Link>
+      <Link data-testid="explore-drinks" to="/explorar/bebidas">Explorar Bebidas</Link>
       <Footer />
     </div>
   );
