e2923e26051c721207c44d11fb2bbc5b8e3bafda
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-24 17:03:30 -0400

    add src in button

--- src/components/IconButton/index.jsx
+++ src/components/IconButton/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './style.css';
 import PropTypes from 'prop-types';
 
 function IconButton({ image, ...rest }) {
   return (
-    <button type="button" { ...rest }>
+    <button className="icon-button" type="button" { ...rest } src={ image }>
       <img src={ image } alt="Icon" />
     </button>
   );
