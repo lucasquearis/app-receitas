f71228f8f9496f68e43ed095fc7108416f88260c
Rodrigo <rodrigo.facury14@gmail.com>
2021-08-24 17:31:53 -0300

    Requisitos 28, 29 e 30

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import CategoryButton from '../components/CategoryButton';
 import RecipeCard from '../components/RecipeCard';
 
 function Drinks() {
@@ -6,32 +7,51 @@ function Drinks() {
   const [drinkCategories, setDrinkCategories] = useState([]);
   const MAX_RECIPES = 12;
   const MAX_CATEGORIES = 5;
+  const drinkEndpoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+  const drinkCategoriesEndpoint = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
 
-  useEffect(() => {
-    const drinkEndpoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-    const drinkCategoriesEndpoint = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
-
+  const getAllDrinks = () => {
     fetch(drinkEndpoint)
       .then((res) => res.json())
       .then(({ drinks }) => setDrinkRecipes(drinks));
+  };
+
+  useEffect(() => {
+    getAllDrinks();
 
     fetch(drinkCategoriesEndpoint)
       .then((res) => res.json())
       .then(({ drinks }) => setDrinkCategories(drinks));
   }, []);
 
+  const handleClick = ({ target }) => {
+    if (target.className !== 'clicked') {
+      const URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${target.innerText}`;
+      const allFilters = document.getElementsByName('category-filter');
+
+      fetch(URL)
+        .then((res) => res.json())
+        .then(({ drinks }) => setDrinkRecipes(drinks));
+
+      allFilters.forEach((filter) => { filter.className = ''; });
+
+      target.className = 'clicked';
+    } else {
+      getAllDrinks();
+      target.className = '';
+    }
+  };
+
   return (
     <div>
       { drinkCategories.map(({ strCategory }, index) => {
         if (index < MAX_CATEGORIES) {
           return (
-            <button
-              data-testid={ `${strCategory}-category-filter` }
-              type="button"
+            <CategoryButton
               key={ strCategory }
-            >
-              { strCategory }
-            </button>
+              strCategory={ strCategory }
+              handleClick={ handleClick }
+            />
           );
         }
         return null;
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import CategoryButton from '../components/CategoryButton';
 import RecipeCard from '../components/RecipeCard';
 
 function Foods() {
@@ -6,32 +7,51 @@ function Foods() {
   const [foodCategories, setFoodCategories] = useState([]);
   const MAX_RECIPES = 12;
   const MAX_CATEGORIES = 5;
+  const foodEndpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+  const foodCategoriesEndpoint = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 
-  useEffect(() => {
-    const foodEndpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-    const foodCategoriesEndpoint = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
-
+  const getAllFoods = () => {
     fetch(foodEndpoint)
       .then((res) => res.json())
       .then(({ meals }) => setFoodRecipes(meals));
+  };
+
+  useEffect(() => {
+    getAllFoods();
 
     fetch(foodCategoriesEndpoint)
       .then((res) => res.json())
       .then(({ meals }) => setFoodCategories(meals));
   }, []);
 
+  const handleClick = ({ target }) => {
+    if (target.className !== 'clicked') {
+      const URL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${target.innerText}`;
+      const allFilters = document.getElementsByName('category-filter');
+
+      fetch(URL)
+        .then((res) => res.json())
+        .then(({ meals }) => setFoodRecipes(meals));
+
+      allFilters.forEach((filter) => { filter.className = ''; });
+
+      target.className = 'clicked';
+    } else {
+      getAllFoods();
+      target.className = '';
+    }
+  };
+
   return (
     <div>
       { foodCategories.map(({ strCategory }, index) => {
         if (index < MAX_CATEGORIES) {
           return (
-            <button
-              data-testid={ `${strCategory}-category-filter` }
-              type="button"
+            <CategoryButton
               key={ strCategory }
-            >
-              { strCategory }
-            </button>
+              strCategory={ strCategory }
+              handleClick={ handleClick }
+            />
           );
         }
         return null;
