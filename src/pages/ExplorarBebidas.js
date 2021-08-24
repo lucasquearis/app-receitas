542f5f4639ca0501606d1ea253e649e86d863bb2
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-08-24 19:14:09 -0300

    Transforma BebidasOuComidas.js em ExplorarComidas.js e ExplorarBebidas.js

--- src/pages/ExplorarBebidas.js
+++ src/pages/ExplorarBebidas.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+
 import MenuInferior from '../components/MenuInferior';
 
 export default function ExplorarBebidas() {
+  const history = useHistory();
   return (
     <div>
-      ExplorarBebidas
+      <button
+        data-testid="explore-by-ingredient"
+        type="button"
+        onClick={ () => history.push('/explorar/comidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        data-testid="explore-by-area"
+        type="button"
+        onClick={ () => history.push('explorar/comidas') }
+      >
+        Por Local de Origem
+      </button>
+      <button
+        data-testid="explore-surprise"
+        type="button"
+        onClick={ () => history.push('explore/surpresas') } // Este path aponta para qual componente? Requisito 70.
+      >
+        Me Surpreenda!
+      </button>
       <MenuInferior />
     </div>
   );
