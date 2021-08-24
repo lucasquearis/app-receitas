1ce1261443cafac236d5a47acd90ce1be65c984f
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-08-24 16:56:11 -0300

    adiciona icones correspondentes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Footer.css';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
   return (
     <footer className="footer" data-testid="footer">
-      <Link to="/bebidas" data-testid="drinks-bottom-btn">
-        <img src="../images/drinkIcon.svg" alt="Bebidas" />
+      <Link to="/bebidas">
+        <img src={ drinkIcon } alt="profile-icon" data-testid="drinks-bottom-btn" />
       </Link>
-      <Link to="/explorar" data-testid="explore-bottom-btn">
-        <img src="../images/exploreIcon.svg" alt="Explorar" />
+      <Link to="/explorar">
+        <img src={ exploreIcon } alt="profile-icon" data-testid="explore-bottom-btn" />
       </Link>
-      <Link to="/comidas" data-testid="food-bottom-btn">
-        <img src="./images/mealIcon.svg" alt="Comidas" />
+      <Link to="/comidas">
+        <img src={ mealIcon } alt="profile-icon" data-testid="food-bottom-btn" />
       </Link>
     </footer>
   );
