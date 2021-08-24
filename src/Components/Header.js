41e410d79bd177067ca0b450b03856f0d16050ce
Josimar Souza <contact.josimarsouza@gmail.com>
2021-08-24 10:21:40 -0300

    Cria elementos Iniciais no header e css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Header.css';
+import PropTypes from 'prop-types';
+import PerfilIcon from '../images/profileIcon.svg';
+import SearchIcon from '../images/searchIcon.svg';
 
-export default function Header() {
+export default function Header(props) {
+  const { title, searchIcon } = props;
   return (
-    <header>
-      Header da Página
+    <header className="header">
+      <img data-testid="profile-top-btn" src={ PerfilIcon } alt="perfil" />
+      <h3 data-testid="page-title">{ title }</h3>
+      { searchIcon && <img src={ SearchIcon } alt="procurar" /> }
     </header>
   );
 }
+
+Header.defaultProps = {
+  searchIcon: true,
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  searchIcon: PropTypes.bool,
+};
--- src/Pages/Comidas.js
+++ src/Pages/Comidas.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Header from '../Components/Header';
 
 export default function Comidas() {
   return (
-    <h1>Tela de Comidas</h1>
+    <section>
+      <Header title="Comidas" searchIcon />
+    </section>
   );
 }
