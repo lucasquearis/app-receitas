bfd5d4d324ffc83bdc87fcc65539ea65a05c416f
Thales Daniel <82240828+Thales-Daniel@users.noreply.github.com>
2021-08-24 12:11:35 -0300

    [group-20] Login (#103)
    * feito estruturaçao do projeto
    
    * continua desenvolvimento da fetch
    
    * feito fetch certinha
    
    * finalizado estrutuaçao inicial
    
    * feito estruturaçao final
    
    * feito novamente alteraçoes na estruturaçao
    
    * feito requisitos de login de 2 ao 8
    
    * feito requisitos de login, de 2 a 8 e css basico
    
    Co-authored-by: Abner <gedeaoabner@gmail.com>

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,62 @@
-import React, { useContext } from 'react';
-import MyContext from '../context/MyContext';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import '../cssPages/Login.css';
 
 function Login() {
-  const { setData, handleClick, data, name, setName } = useContext(MyContext);
-  console.log(data);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [validation, setValidation] = useState(true);
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    function validator() {
+      const min = 6;
+      const okEmail = /^[a-z0-9](\.|-|_|[a-z]|\d)+@([a-z]|\d)+\.[a-z]{2,3}(\.[a-z]{2})?$/
+        .test(email);
+      const validPassword = password.length > min;
+      if (okEmail && validPassword === true) {
+        return setValidation(false);
+      }
+    }
+    validator();
+  }, [email, password]);
+
+  function onSubmit(event) {
+    event.preventDefault();
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+    setRedirect(true);
+  }
+  if (redirect) return <Redirect to="/comidas" />;
   return (
-    <section>
-      <input type="text" onChange={ (event) => setName(event.target.value) } />
-      <button
-        type="button"
-        onClick={ () => handleClick('food', 'procuraComida', name, setData) }
-      >
-        ABC
-      </button>
-    </section>
+    <form onSubmit={ onSubmit } className="container">
+      <div className="login-container">
+        <input
+          type="email"
+          className="login"
+          value={ email }
+          onChange={ (event) => setEmail(event.target.value) }
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          className="login"
+          data-testid="password-input"
+          value={ password }
+          onChange={ (event) => setPassword(event.target.value) }
+        />
+
+        <button
+          className="btn"
+          type="submit"
+          disabled={ validation }
+          data-testid="login-submit-btn"
+        >
+          Entrar
+        </button>
+      </div>
+    </form>
   );
 }
 
