e1d042ba3ceab7fb867d80a56e3a895f0e9f12d1
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-08-24 16:28:59 -0300

    Criada pagina de login funcional

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Input({ handleChange, name, text, type, testId, value }) {
+export default function Input({
+  handleChange,
+  name,
+  text,
+  type,
+  testId,
+  value,
+  placeholder,
+}) {
   return (
     <label htmlFor={ name }>
       { text }
@@ -11,6 +20,7 @@ export default function Input({ handleChange, name, text, type, testId, value })
         value={ value }
         onChange={ handleChange }
         data-testid={ testId }
+        placeholder={ placeholder }
       />
     </label>
   );
@@ -20,16 +30,20 @@ Input.defaultProps = {
   text: '',
   type: 'text',
   testId: 'none',
+  placeholder: '',
+  value: null,
+  handleChange: null,
 };
 
 Input.propTypes = {
   testId: PropTypes.string,
-  handleChange: PropTypes.func.isRequired,
+  handleChange: PropTypes.func,
   name: PropTypes.string.isRequired,
   text: PropTypes.string,
   type: PropTypes.string,
+  placeholder: PropTypes.string,
   value: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
-  ]).isRequired,
+  ]),
 };
