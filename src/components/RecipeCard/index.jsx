b82f8da12abc811590a1ea3fffa1055da63d9fda
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-24 19:01:15 -0400

    add params

--- src/components/RecipeCard/index.jsx
+++ src/components/RecipeCard/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function RecipeCard({ image, name }) {
+function RecipeCard({ image, name, index, testId }) {
   return (
-    <div>
-      <img src={ image } alt="Recipe" />
-      <p>{ name }</p>
+    <div data-testid={ testId }>
+      <img src={ image } alt="Recipe" data-testid={ `${index}-card-img` } />
+      <p data-testid={ `${index}-card-name` }>{ name }</p>
     </div>
   );
 }
@@ -13,6 +13,8 @@ function RecipeCard({ image, name }) {
 RecipeCard.propTypes = {
   image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  testId: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default RecipeCard;
