d6cf02cb4786d3692742c2eb2cadb81ce3d3539a
Rodrigo <rodrigo.facury14@gmail.com>
2021-08-24 14:51:19 -0300

    Requisito 27

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -3,17 +3,39 @@ import RecipeCard from '../components/RecipeCard';
 
 function Drinks() {
   const [drinkRecipes, setDrinkRecipes] = useState([]);
+  const [drinkCategories, setDrinkCategories] = useState([]);
   const MAX_RECIPES = 12;
+  const MAX_CATEGORIES = 5;
 
   useEffect(() => {
     const drinkEndpoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    const drinkCategoriesEndpoint = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+
     fetch(drinkEndpoint)
       .then((res) => res.json())
       .then(({ drinks }) => setDrinkRecipes(drinks));
+
+    fetch(drinkCategoriesEndpoint)
+      .then((res) => res.json())
+      .then(({ drinks }) => setDrinkCategories(drinks));
   }, []);
 
   return (
     <div>
+      { drinkCategories.map(({ strCategory }, index) => {
+        if (index < MAX_CATEGORIES) {
+          return (
+            <button
+              data-testid={ `${strCategory}-category-filter` }
+              type="button"
+              key={ strCategory }
+            >
+              { strCategory }
+            </button>
+          );
+        }
+        return null;
+      }) }
       { drinkRecipes.map(({ strDrinkThumb, strDrink }, index) => {
         if (index < MAX_RECIPES) {
           return (
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -3,17 +3,39 @@ import RecipeCard from '../components/RecipeCard';
 
 function Foods() {
   const [foodRecipes, setFoodRecipes] = useState([]);
+  const [foodCategories, setFoodCategories] = useState([]);
   const MAX_RECIPES = 12;
+  const MAX_CATEGORIES = 5;
 
   useEffect(() => {
     const foodEndpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    const foodCategoriesEndpoint = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+
     fetch(foodEndpoint)
       .then((res) => res.json())
       .then(({ meals }) => setFoodRecipes(meals));
+
+    fetch(foodCategoriesEndpoint)
+      .then((res) => res.json())
+      .then(({ meals }) => setFoodCategories(meals));
   }, []);
 
   return (
     <div>
+      { foodCategories.map(({ strCategory }, index) => {
+        if (index < MAX_CATEGORIES) {
+          return (
+            <button
+              data-testid={ `${strCategory}-category-filter` }
+              type="button"
+              key={ strCategory }
+            >
+              { strCategory }
+            </button>
+          );
+        }
+        return null;
+      }) }
       { foodRecipes.map(({ strMealThumb, strMeal }, index) => {
         if (index < MAX_RECIPES) {
           return (
