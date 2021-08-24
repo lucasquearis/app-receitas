4549c31fc98d2e04d193ed516ce41803550e7e77
RyanLaiber <ryanlaiber@gmail.com>
2021-08-24 16:02:25 -0300

    adiciona logica de redirecionamento baseado em length da lista meals

--- src/pages/MainMeals.jsx
+++ src/pages/MainMeals.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { Header } from '../components';
 
 function MainMeals() {
+  const recipes = useSelector(({ meals }) => meals.recipes);
+  const history = useHistory();
+  useEffect(() => {
+    if (recipes.meals && recipes.meals.length === 1) {
+      const mealId = recipes.meals[0].idMeal;
+      history.push(`/comidas/${mealId}`);
+    }
+  }, [recipes]);
   return (
     <div>
       <Header title="Comidas" searchIcon />
