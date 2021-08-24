38f319fc3975050ab71786f94ce6f3f9a84339df
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2021-08-24 15:39:40 -0300

    Levando os dados da Api ate os elementos da pagina

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -2,49 +2,104 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
+import rockGlass from '../images/rockGlass.svg';
+import IngredientsList from '../components/IngredientsList';
 
 function RecipeDetails(props) {
-  const [recipe, setRecipe] = useState({});
+  const [recipe, setRecipe] = useState();
+  const [enType, setEnType] = useState('drinks');
+  const [enCasedType, setEnCasedType] = useState('Drink');
+  const { match } = props;
+  const { type, id } = match.params;
+  // const maxIngredients = 15;
+
   useEffect(() => {
     const getRecipe = async () => {
-      const { match } = props;
-      const { type, id } = match.params;
       let endpoint = '';
       if (type === 'comidas') {
         endpoint = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+        setEnType('meals');
+        setEnCasedType('Meal');
       } else {
         endpoint = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
       }
-      const result = await fetch(endpoint).then((response) => response.json());
-      setRecipe(result);
+      await fetch(endpoint)
+        .then((data) => data.json())
+        .then((response) => {
+          setRecipe(response);
+        });
     };
     getRecipe();
   }, []);
-  const index = 0;
+
+  useEffect(() => {
+
+  }, [recipe]);
+
   return (
     <div>
-      <object
-        type="image/svg+xml"
-        data={ blackHeartIcon }
-        data-testid="recipe-photo"
-      >
-        Foto do prato
-      </object>
-      <h1 data-testid="recipe-title">Título</h1>
-      <object type="image/svg+xml" data={ shareIcon } data-testid="share-btn">
-        Compartilhar
-      </object>
-      <object type="image/svg+xml" data={ blackHeartIcon } data-testid="favorite-btn">
-        Adicionar a favoritos
-      </object>
-      <h2 data-testid="recipe-category">Categoria</h2>
-      <ul>
-        <li data-testid={ `${index}-ingredient-name-and-measure` }>Ingredientes</li>
-      </ul>
-      <p data-testid="instructions">Instruções</p>
-      <iframe title="Video" data-testid="video" src="https://www.youtube.com/embed/kJkQFVqySUw" />
-      <div data-testid={ `${index}-recomendation-card` }>Recomendações</div>
-      <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+      {
+        recipe
+          ? (
+            <div>
+              <img
+                src={ recipe[enType][0][`str${enCasedType}Thumb`] }
+                alt="Foto do Prato"
+                data-testid="recipe-photo"
+              />
+              <h1
+                data-testid="recipe-title"
+              >
+                { recipe[enType][0][`str${enCasedType}`] }
+              </h1>
+              <object type="image/svg+xml" data={ shareIcon } data-testid="share-btn">
+                Compartilhar
+              </object>
+              <object
+                type="image/svg+xml"
+                data={ blackHeartIcon }
+                data-testid="favorite-btn"
+              >
+                Adicionar a favoritos
+              </object>
+              <h2 data-testid="recipe-category">
+                { type === 'comidas'
+                  ? recipe[enType][0].strCategory
+                  : recipe[enType][0].strAlcoholic }
+              </h2>
+              <ul>
+                <IngredientsList recipe={ recipe[enType][0] } />
+              </ul>
+              <p data-testid="instructions">{ recipe[enType][0].strInstructions }</p>
+              <iframe title="Video" data-testid="video" src="https://www.youtube.com/embed/kJkQFVqySUw" />
+              {
+                recipe && recipe[enType].map((rec, index) => (
+                  <div
+                    key={ index }
+                    data-testid={ `${index}-recomendation-card` }
+                  >
+                    Recomendações
+                  </div>
+                ))
+              }
+              <button
+                type="button"
+                data-testid="start-recipe-btn"
+              >
+                Iniciar Receita
+              </button>
+            </div>
+          )
+          : (
+            <object
+              className="rocksGlass"
+              type="image/svg+xml"
+              data={ rockGlass }
+            >
+              Glass
+            </object>
+          )
+      }
     </div>
   );
 }
