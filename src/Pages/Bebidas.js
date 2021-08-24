6cb2b3b64432fdc610c7ed8336a9a06f9dac3a91
Josimar Souza <contact.josimarsouza@gmail.com>
2021-08-24 17:39:10 -0300

    Cria o componente card

--- src/Pages/Bebidas.js
+++ src/Pages/Bebidas.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import buscarTodasBebidasPorLetra from '../service/BebidasAPI';
+import Card from '../Components/Card';
 
 export default function Bebidas() {
+  const [bebidas, setBebidas] = useState([]);
+  const fetchFoods = async () => {
+    const maxFood = 12;
+    const results = await buscarTodasBebidasPorLetra('b');
+    const comidasFiltered = results.filter((result, index) => index < maxFood);
+    setBebidas(comidasFiltered);
+  };
+
+  fetchFoods();
+  if (bebidas.length <= 0) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <h1>Tela de Bebidas</h1>
+    <section>
+      {
+        bebidas.map(
+          (bebida, index) => (
+            <Card key={ bebida.idDrink } bebida={ bebida } index={ index } />
+          ),
+        )
+      }
+    </section>
   );
 }
--- src/service/ComidasAPI.js
+++ src/service/ComidasAPI.js
@@ -1,8 +1,13 @@
-async function buscarCategorias() {
+export async function buscarCategorias() {
   const ENDPOINT = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 
   const response = await fetch(ENDPOINT).then((data) => data.json());
   return response;
 }
 
-export default buscarCategorias;
+export async function buscarTodasComidasPorLetra(letra) {
+  const ENDPOINT = `https://www.themealdb.com/api/json/v1/1/search.php?f=${letra}`;
+
+  const response = await fetch(ENDPOINT).then((data) => data.json());
+  return response.meals;
+}
