306f666493d7739c8308b5eb79472d658c906cad
Caio César <caiovg32@gmail.com>
2021-08-24 15:43:17 -0300

    req-22

---
+++ src/pages/Bebidas/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Menuinferior from '../../Components/MenuInferior';
+
+function Bebidas() {
+  return (
+    <div>
+      <Menuinferior />
+    </div>
+  );
+}
+
+export default Bebidas;
---
+++ src/pages/Comidas/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuInferior from '../../Components/MenuInferior';
+
+function Comidas() {
+  return (
+    <div>
+      <MenuInferior />
+    </div>
+  );
+}
+
+export default Comidas;
