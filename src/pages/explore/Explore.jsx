5fac692df158c08cf078480f0fa49ffae179f9bd
David Azevedo <zevid.dev@outlook.com>
2021-08-24 17:39:16 -0300

    Renderizado Header na pagina de explorar

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 export default function Explore() {
   return (
     <div>
+      <Header title="Explorar" />
       Explore
     </div>
   );
