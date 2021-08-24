03db8508519ce3210bad238da018a9e8f30e9da6
John Pierre <74274114+johnpligeiro@users.noreply.github.com>
2021-08-24 13:20:41 -0300

    [Main Group 32] Footer (#114)
    * feat: footer - reqs 19 e 20
    
    * put footer on profile and main-food

---
+++ src/components/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+// O menu inferior deve ter possuir o atributo data-testid="footer";
+// drinks deve possuir o atributo data-testid="drinks-bottom-btn";
+// explorar deve possuir o atributo data-testid="explore-bottom-btn";
+// comidas deve possuir o atributo data-testid="food-bottom-btn".
+
+const Footer = () => (
+  <footer data-testid="footer" className="footer">
+    <Link
+      to="/bebidas"
+    >
+      <img
+        src={ drinkIcon }
+        alt="drinks"
+        data-testid="drinks-bottom-btn"
+      />
+    </Link>
+    <Link
+      to="/explorar"
+    >
+      <img
+        alt="explore"
+        src={ exploreIcon }
+        data-testid="explore-bottom-btn"
+      />
+    </Link>
+    <Link
+      to="/comidas"
+    >
+      <img
+        src={ mealIcon }
+        alt="meal"
+        data-testid="food-bottom-btn"
+      />
+    </Link>
+  </footer>
+);
+export default Footer;
