12996025ba71cbcd491b893e6a8568eb874859b4
matheustkaczyk <matheus111904@hotmail.com>
2021-08-25 15:13:46 -0300

    criado funcao onclick no card, mas o lint esta doidao

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const handleClick = ({ idMeal }) => {
+  console.log(idMeal);
+};
+
 const RecipeCard = ({ recipe, index }) => (
-  <div className="recipe-card" data-testid={ `${index}-recipe-card` }>
-    <img src={ recipe.strMealThumb } alt="recipe" data-testid={ `${index}-card-img` } />
+  <div
+    role="tab"
+    tabIndex={ 0 }
+    onClick={ () => handleClick(recipe) }
+    onKeyDown={ () => handleClick(recipe) }
+    className="recipe-card"
+    data-testid={ `${index}-recipe-card` }
+  >
+    <img
+      src={ recipe.strMealThumb }
+      alt="recipe"
+      data-testid={ `${index}-card-img` }
+    />
     <p data-testid={ `${index}-card-name` }>{ recipe.strMeal }</p>
   </div>
 );
