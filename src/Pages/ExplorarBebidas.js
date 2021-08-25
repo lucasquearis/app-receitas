352d79349f88d0095a4aa3960f9f9aa2dfb26487
Wadson Almeida <wadson.dev92@gmail.com>
2021-08-25 17:32:01 -0300

    atualizaçoes

--- src/Pages/ExplorarBebidas.js
+++ src/Pages/ExplorarBebidas.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../Components/Header';
+import Footer from '../Components/Footer';
 
 export default function ExplorarBebidas() {
   return (
-    <section>
-      <Header title="Explorar Bebidas" />
-    </section>
+    <>
+      <section>
+        <Header title="ExplorarBebidas" searchIcon />
+      </section>
+      <Footer />
+    </>
   );
 }
