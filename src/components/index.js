17558286684fbb682d8ae364f35d106e3aef0af0
Milton Castro <44573806+castromilton07@users.noreply.github.com>
2021-08-25 19:08:23 -0300

    [GROUP 11] - [SCREEN-DONE-RECIPES] (#252)
    * ADD-REQ-25-26[WithoutCSS]
    
    * Restore package.json and package-lock.json
    
    * Add script lint:styles in package.json
    
    * ADD-REQ-27-28-29-30-31-32
    
    * ADD-CCS[toBeEqualPrototype]
    
    * ADD-CONFIG[package-lock.json]-toBeEqualMainGroup11
    
    * ADD-CONFIG[package-lock.json & package-lock.json]-toBeEqualMainGroup11
    
    * ADD-ADJUST-OF-COMPONENT-NAME[Profile]
    
    * ADD-BASIC-STRUCTURE-OF-REQ-54-to-59
    
    * ADD-MOCK-TO-TEST-REQ-54-55-56-57-58-59
    
    * REM->Mocks.js[FileWithManyLines]
    
    * ADD-NEW-COMPONENT-TO-PAGE-DONE-RECIPES[DoneCard.js]
    
    * ADD-ADJUST-TO-FILE-DONE-RECIPES[wrongNameOfString]
    
    * ADD-MINIMUM-STYLE-ADJUSTS
    
    * ADD-CORRECTION-OF-STYLELINT-ERRORS
    
    * ADD-REQ-54-55-56-57-58-59[AllDone]
    
    * REMOVE-MOCK-IMPORT

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Input } from './Input';
 export { default as Button } from './Button';
 export { default as Select } from './Select';
 export { default as Card } from './Card';
+export { default as DoneCard } from './DoneCard';
