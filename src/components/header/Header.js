408edfaabb18c4b1c1224d35fe94ca47ab305d92
Augusto <augustocm2002@yahoo.com.br>
2021-08-25 15:12:43 -0300

    Cria logica para a SearchBar e completa requisito 12

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,37 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 import SearchBar from '../SearchBar/SearchBar';
 
-const Header = ({ children }) => (
-  <div>
-    <header>
-      <Link to="/perfil">
-        <button type="button">
-          <img
-            data-testid="profile-top-btn"
-            src={ profileIcon }
-            alt="profile icon"
-          />
-        </button>
-      </Link>
-      <h1 data-testid="page-title">{ children }</h1>
-      <button type="button">
-        <div>
-          <img
-            src={ searchIcon }
-            alt="search icon"
-            data-testid="search-top-btn"
-          />
-          <SearchBar />
-        </div>
-      </button>
-    </header>
-  </div>
-);
+const Header = ({ children }) => {
+  const [withSearchBar, setWithSearchBar] = useState(false);
 
+  const onOffSearchBar = () => {
+    setWithSearchBar(!withSearchBar);
+  };
+  return (
+    <div>
+      <header>
+        <Link to="/perfil">
+          <button type="button">
+            <img
+              data-testid="profile-top-btn"
+              src={ profileIcon }
+              alt="profile icon"
+            />
+          </button>
+        </Link>
+        <h1 data-testid="page-title">{ children }</h1>
+        <button type="button" onClick={ onOffSearchBar }>
+          <div>
+            <img
+              src={ searchIcon }
+              alt="search icon"
+              data-testid="search-top-btn"
+            />
+            { withSearchBar && <SearchBar /> }
+          </div>
+        </button>
+      </header>
+    </div>
+  );
+};
 Header.propTypes = {
   children: PropTypes.string.isRequired,
 };
