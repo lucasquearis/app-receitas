7d77af121ffbfb60f66b0f551bdb92865d8ebbbc
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-08-25 15:58:33 -0300

    arrumand lint, importando login 2x

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-
 import Login from './pages/login/Login';
 import Comidas from './pages/comidas/Comidas';
-import Login from './pages/login/Login';
 import Perfil from './pages/perfil/Perfil';
 import Bebidas from './pages/bebidas/Bebidas';
 
