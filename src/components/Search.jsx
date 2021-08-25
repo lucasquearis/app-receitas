a7fcfa3ce43542884ff4ff22c51fa6c3603ca18f
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-08-25 19:14:49 -0300

    barra de busca requisito 14

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,26 +1,89 @@
 import React, { Component } from 'react';
+import fetch from '../services/fetch';
 
 export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      radio: '',
+      text: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  async handleClick() {
+    const { radio, text } = this.state;
+    if (radio === 'Ingrediente') {
+      const ingredientes = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${text}`);
+      return ingredientes;
+    }
+    if (radio === 'Nome') {
+      const nomes = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${text}`);
+      return nomes;
+    }
+    if (radio === 'Primeira letra' && text.length === 1) {
+      const primeiras = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${text}`);
+      return primeiras;
+    } alert('Sua busca deve conter somente 1 (um) caracter');
+  }
+
   render() {
     return (
       <div>
         <label htmlFor="search-input">
           Search:
-          <input type="text" data-testid="search-input" />
+          <input
+            type="text"
+            name="text"
+            data-testid="search-input"
+            onChange={ this.handleChange }
+          />
         </label>
-        <label htmlFor="search-input">
-          Ingrediente
-          <input type="radio" data-testid="ingredient-search-radio" />
-        </label>
-        <label htmlFor="search-input">
-        Nome
-          <input type="radio" data-testid="name-search-radio" />
-        </label>
-        <label htmlFor="search-input">
-        Primeira letra 
-          <input type="radio" data-testid="first-letter-search-radio" />
-        </label>
-        <button data-testid="exec-search-btn">Search</button>
+        <div name="radio" onChange={ this.handleChange }>
+          <label htmlFor="search-input">
+            Ingrediente
+            <input
+              id="ingrediente"
+              type="radio"
+              name="radio"
+              data-testid="ingredient-search-radio"
+              value="Ingrediente"
+            />
+          </label>
+          <label htmlFor="search-input">
+            Nome
+            <input
+              id="nome"
+              type="radio"
+              name="radio"
+              data-testid="name-search-radio"
+              value="Nome"
+            />
+          </label>
+          <label htmlFor="search-input">
+            Primeira letra
+            <input
+              id="primeira-letra"
+              type="radio"
+              name="radio"
+              data-testid="first-letter-search-radio"
+              value="Primeira letra"
+            />
+          </label>
+        </div>
+        <button
+          type="button"
+          data-testid="exec-search-btn"
+          onClick={ this.handleClick }
+        >
+          Search
+        </button>
       </div>
     );
   }
