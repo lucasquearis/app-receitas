a92e191e00aeb998258016fb3de7a63b3496aa97
David Azevedo <zevid.dev@outlook.com>
2021-08-24 23:31:19 -0300

    Utiliza useState para definir a renderizacao do SearchBar ao clicar no searchIcon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { string, bool } from 'prop-types';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import './header.css';
+import SearchBar from './SearchBar';
 
 function Header({ title, showButton }) {
+  const [showAndHideSearchBar, setShowAndHideSearchBar] = useState(false);
   return (
-    <header className="container-header">
-      <Link to="/perfil">
-        <img
-          data-testid="profile-top-btn"
-          src={ profileIcon }
-          alt="Profile user"
-        />
-      </Link>
-      {
-        title
-          ? (
-            <h2
-              data-testid="page-title"
-            >
-              {title}
-            </h2>
-          )
-          : ''
-      }
-      {
-        showButton
-          ? (
-            <img
-              data-testid="search-top-btn"
-              src={ searchIcon }
-              alt="Profile user"
-            />
-          )
-          : ''
-      }
-    </header>
+    <>
+      <header className="container-header">
+        <Link to="/perfil">
+          <img
+            data-testid="profile-top-btn"
+            src={ profileIcon }
+            alt="Profile user"
+          />
+        </Link>
+        {
+          title
+            ? (
+              <h2
+                data-testid="page-title"
+              >
+                {title}
+              </h2>
+            )
+            : ''
+        }
+        {
+          showButton
+            ? (
+              <button
+                type="button"
+                className="btn-search"
+                onClick={ () => setShowAndHideSearchBar(!showAndHideSearchBar) }
+              >
+                <img
+                  data-testid="search-top-btn"
+                  src={ searchIcon }
+                  alt="Profile user"
+                />
+              </button>
+            )
+            : ''
+        }
+      </header>
+      {showAndHideSearchBar && <SearchBar /> }
+    </>
   );
 }
 
