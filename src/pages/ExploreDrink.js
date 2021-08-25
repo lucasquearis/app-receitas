dda0e491cd6f6dab19b1cc35579fb5b858085529
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-08-25 18:45:22 -0300

    Resolvendo problemas no requisito 21

--- src/pages/ExploreDrink.js
+++ src/pages/ExploreDrink.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function ExploreDrink() {
   return (
     <div>
       <Header title="Explorar Bebidas" />
+      <Footer />
     </div>
   );
 }
