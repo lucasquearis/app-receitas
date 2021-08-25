e84dc80f5fa1de802aac5894ef5c85c03993d626
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-25 02:01:43 -0300

    adicionando Links e email a pagina

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Header from '../components/Header';
+import { Link } from 'react-router-dom';
+import Button from '../components/Button';
 import Footer from '../components/Footer';
 
 function Perfil() {
+  const { email }= JSON.parse(localStorage.getItem('user'));
   return (
     <div>
       <Header title="Perfil" />
+      <main>
+        <div data-testid="profile-email">{ email }</div>
+        <Link to="/receitas-feitas" data-testid="profile-done-btn">
+          Receitas Feitas
+        </Link>
+        <Link to="/receitas-favoritas" data-testid="profile-favorite-btn">
+          Receitas Feitas
+        </Link>
+      </main>
       <Footer />
     </div>
   );
