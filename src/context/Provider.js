ed63b7f02ca58109911fb439d5402aad9dd9123c
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-08-24 23:54:28 -0300

    Add requisição das listas `Meals` e `Drinks`

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
+import { fetchApi, MEALS_LIST, DRINKS_LIST } from '../services';
 
 function Provider({ children }) {
+  const [mealsList, setMealsList] = useState([]);
+  const [drinksList, setDrinksList] = useState([]);
+
+  useEffect(() => {
+    const getRecipes = async () => {
+      const listLimit = 12;
+      try {
+        const { meals } = await fetchApi(MEALS_LIST);
+        setMealsList(meals.filter((_meal, index) => index < listLimit));
+
+        const { drinks } = await fetchApi(DRINKS_LIST);
+        setDrinksList(drinks.filter((_drink, index) => index < listLimit));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getRecipes();
+  }, []);
+
   const contextValue = {
+    mealsList,
+    drinksList,
   };
   return (
     <AppContext.Provider value={ contextValue }>
