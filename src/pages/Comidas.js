559913e4a7006350aeb0f4f53ea4be848178db7f
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-25 16:12:06 -0300

    arrumando a pagina

--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Comidas() {
-  const [meal, setMeal] = useState({});
-  useEffect(() => {
-    const getMeal = async () => {
-      const URL = 'www.themealdb.com/api/json/v1/1/lookup.php?i=52772';
-      const response = await fetch(URL);
-      const data = await response.json();
-      console.log(data);
-    }
-    getMeal();
-  }, []);
   return (
     <div>
       <Header title="Comidas" />
-      <main>
-
-      </main>
       <Footer />
     </div>
   );
