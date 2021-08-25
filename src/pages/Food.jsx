6547129ff0b05a45300c6f36efc1bd84d35580d8
Thalita Cecilier <70970971+ThalitaC@users.noreply.github.com>
2021-08-25 16:38:15 -0300

    requisito 19 (#301)

--- src/pages/Food.jsx
+++ src/pages/Food.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import HeaderWithSearch from '../components/HeaderWithSearch';
 
 function Food() {
   return (
     <div>
       <HeaderWithSearch>Comidas</HeaderWithSearch>
+      <Footer />
     </div>
   );
 }
