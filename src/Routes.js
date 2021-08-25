90a31dbe2ada3ad8519649b9709af4f3e8661656
Caio Morato <morato_dev@protonmail.com>
2021-08-25 16:17:49 -0300

    setup da pagina receitas favoritas

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 // components
 import Login from './pages/Login';
 import Profile from './pages/Profile';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route exact path="/profile" component={ Profile } />
+      <Route exact path="/receitas-favoritas" component={ FavoriteRecipes } />
     </Switch>
   );
 }
---
+++ src/pages/FavoriteRecipes.js
@@ -0,0 +1,13 @@
+// vitals
+import React from 'react';
+// components
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function FavoriteRecipes() {
+
+  return (
+    
+  )
+}
+export default FavoriteRecipes;
