86b0d8b75e18e26eedd914406265101b5ab91444
RyanLaiber <ryanlaiber@gmail.com>
2021-08-25 17:35:53 -0300

    json.parse error

--- src/components/CategoryFilter/CategoryFilter.jsx
+++ src/components/CategoryFilter/CategoryFilter.jsx
@@ -14,6 +14,7 @@ function CategoryFilter() {
           <CategoryButton
             key={ strCategory }
             category={ strCategory }
+            path={ pathname }
           />)) }
       </div>
     );
