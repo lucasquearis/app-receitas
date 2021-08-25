5938782ac22763d7025c3396ffcd2086def030ca
raph2ll <raphael.ph2ll@gmail.com>
2021-08-25 19:08:44 -0300

    Resolve 70 a 73

--- src/Pages/DrinksExplore.jsx
+++ src/Pages/DrinksExplore.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import ButtonCard from '../Components/ButtonCard';
+import { getRandomId } from '../helper';
 
 function DrinksExplore() {
+  const randomId = getRandomId();
+  const path = `/comidas/${randomId}`;
+
   return (
-    <>
-      <Header title="Comidas" loading />
+    <div>
+      <Header title="Bebidas" loading />
       <Footer />
-    </>
+      <ButtonCard
+        page="/explorar/bebidas/ingredientes"
+        testId="explore-by-ingredient"
+        buttonText="Por Ingredientes"
+      />
+      <ButtonCard
+        page={ path }
+        testId="explore-surprise"
+        buttonText="Me Surpreenda!"
+      />
+    </div>
   );
 }
 
