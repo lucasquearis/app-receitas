f1f9d12db843c08e1858a4fe360732a0698a7170
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-25 16:43:53 -0300

    adicinando logica para favoritar receita ao clicar no icone

--- src/pages/ComidasDetails.js
+++ src/pages/ComidasDetails.js
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function ComidasDetails(props) {
   const [meal, setMeal] = useState({});
+  const { match: { params : { id } } } = props;
+  const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  const favorite = favoriteRecipes && favoriteRecipes.find((recipe) => recipe.idMeal === id);
+  const [isFav, setIsFav] = useState(favorite);
+ 
+  const favoritingRecipe = () => {
+    if (isFav) {
+      setIsFav(false);
+      const newFavoriteRecipes = favoriteRecipes.filter((recipe) => recipe.idMeal !== id);
+      localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoriteRecipes));
+    } else {
+      setIsFav(true);
+      const newFavoriteRecipes = favoriteRecipes ? [...favoriteRecipes, meal] : [meal];
+      localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoriteRecipes));
+    }
+  }
+
   useEffect(() => {
     const getMeal = async () => {
-      const { match: { params : { id } } } = props;
       const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
       const response = await fetch(URL);
       const data = await response.json();
@@ -15,7 +34,20 @@ function ComidasDetails(props) {
 
   return (
     <main>
-      Comidas Detalhes
+      <div>
+        <img data-testid="recipe-photo" src={meal.strMealThumb} alt="imagem do prato"/>
+      </div>
+      <div>
+        {meal.strMeal}
+        <div>
+          <img src={ shareIcon } alt="imagem de compartilhar" />
+          <img
+            src={ isFav ? blackHeartIcon : whiteHeartIcon }
+            alt="imagem de favoritar"
+            onClick={ favoritingRecipe }
+          />
+        </div>
+      </div>
     </main>
   );
 }
