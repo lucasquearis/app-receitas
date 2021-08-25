b2391b1c8d74a1bf482708a361bea81e5eaabf80
Matheus Gonçalves <55060810+matheusgbl@users.noreply.github.com>
2021-08-25 13:06:26 -0300

    [Grupo 6] - Página de perfil (#260)
    * Requisito 13
    
    * Requisito 14
    
    * Requisito 15
    
    * Requisito 16 incompleto
    
    * Commit para resolução de conflitos
    
    * Requisito 16
    
    * Cria card para realização do requisito 17
    
    * Mudança no component de pesquisa
    
    * Requisito 18
    
    * Corrige erros nos requisitos 10 e 12
    
    * Component de perfil finalizado

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const reduxEmail = useSelector((state) => state.user.email);
+  const storageEmail = JSON.parse(localStorage.getItem('user')).email;
+
+  const exitApp = () => {
+    localStorage.clear();
+  };
+
   return (
     <section>
       <Header showExploreIcon pageTitle="Perfil" />
+      <div>
+        <h3 data-testid="profile-email">
+          {!reduxEmail ? storageEmail : reduxEmail}
+        </h3>
+      </div>
+      <div>
+        <Link to="/receitas-feitas">
+          <Button data-testid="profile-done-btn" type="button">
+            Receitas Feitas
+          </Button>
+        </Link>
+        <Link to="/receitas-favoritas">
+          <Button data-testid="profile-favorite-btn" type="button">
+            Receitas Favoritas
+          </Button>
+        </Link>
+        <Link to="/">
+          <Button
+            onClick={ exitApp }
+            data-testid="profile-logout-btn"
+            type="button"
+          >
+            Sair
+          </Button>
+        </Link>
+      </div>
     </section>
   );
 }
