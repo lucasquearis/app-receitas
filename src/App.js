99b55d3aedf6d4d63b996a3cb86f2355989ff048
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-08-25 15:06:29 -0300

    arrumando EsLint

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './App.css';
 import {
   Switch,
-  Route
+  Route,
 } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Comidas from './pages/comidas/Comidas';
 
 function App() {
-  return ( 
-    <Switch >
-      <Route exact path="/"component={ Login } />
-      <Route path="/comidas"component={Comidas} />
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/comidas" component={ Comidas } />
     </Switch>
   );
 }
