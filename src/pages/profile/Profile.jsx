58a714618b27e9e98e5c9d1922a4d28f4d686f52
Pedro H M Martins <67984483+martinsph@users.noreply.github.com>
2021-08-25 17:00:06 -0300

    add req 82 ate 87 (#308)

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import Footer from '../../components/Footer';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default function Profile() {
+  const profileEmail = JSON.parse(localStorage.getItem('user')).email;
+  function clearLocalStorage() {
+    localStorage.clear();
+  }
   return (
     <>
       <Header title="Perfil" />
-      Profile
+      <h1 data-testid="profile-email">{ profileEmail }</h1>
+      <Link to="/receitas-feitas">
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ clearLocalStorage }
+        >
+          Sair
+        </button>
+      </Link>
       <Footer />
     </>
   );
