7d3f4eebf3b192ad4b629e38382df8b1030fceac
Elias Forte <82238233+eliforte@users.noreply.github.com>
2021-08-25 14:07:22 -0300

    [Group 16] - Elias - Requisitos 2 ao 8 (#146)
    * requisito 2
    
    * adiciona funçao handleChange nos inputs de senha e login
    
    * funçao de desabilitar e habilitar o botao de "Entrar"
    
    * salva no localStorage mealsToken e cocktailsToken
    
    * salva no localStorage o email do usuario
    
    * adiciona roda "/" para o componente Login
    
    * componente Login concluido
    
    * update login.js
    
    removido os comentários

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 // components
+import Login from './pages/Login';
 import Profile from './pages/Profile';
 
 function Routes() {
   return (
     <Switch>
+      <Route exact path="/" component={ Login } />
       <Route exact path="/profile" component={ Profile } />
     </Switch>
   );
---
+++ src/pages/Login.js
@@ -0,0 +1,72 @@
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import myContext from '../context/myContext';
+
+function Login() {
+  const history = useHistory();
+  const { setInfoUser } = useContext(myContext);
+  const [inputLogin, setInputLogin] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setInputLogin({
+      ...inputLogin,
+      [name]: value,
+    });
+  };
+
+  const handleClick = () => {
+    setInfoUser(inputLogin);
+    history.push('/comidas');
+  };
+
+  const verifyPassword = () => {
+    const emailVerify = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]/i;
+    const passwordVerify = 6;
+    if (emailVerify.test(inputLogin.email)
+    && passwordVerify < inputLogin.password.length) {
+      return false;
+    }
+    return true;
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Olá! Digite seu usário e senha</h2>
+        <form>
+          <label htmlFor="email">
+            <input
+              data-testid="email-input"
+              onChange={ handleChange }
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+          </label>
+          <label htmlFor="password">
+            <input
+              data-testid="password-input"
+              type="password"
+              placeholder="Senha"
+              name="password"
+              onChange={ handleChange }
+            />
+          </label>
+          <button
+            data-testid="login-submit-btn"
+            type="button"
+            onClick={ handleClick }
+            disabled={ verifyPassword() }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
