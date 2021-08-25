09b8895acc4f842572a802d6aeadd26f8d766401
Adryan Rosa <81928512+adryanrosa@users.noreply.github.com>
2021-08-25 16:27:10 -0700

    Corrige rotas em App (#362)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Provider from './context/Provider';
+import DrinkDetails from './pages/Details/Drink';
+import MealDetails from './pages/Details/Meal';
 import Footer from './components/Footer';
 import Login from './pages/Login';
 import Recipes from './pages/Recipes';
+import Profile from './pages/Profile';
 import InProgress from './pages/InProgress';
 
 import './App.css';
@@ -16,21 +19,26 @@ function App() {
         <Route exact path="/" component={ Login } />
         <Route exact path="/comidas" component={ Recipes } />
         <Route exact path="/bebidas" component={ Recipes } />
-        <Route exact path="/comidas/:id" />
-        <Route exact path="/bebidas/:id" />
+        <Route exact path="/comidas/:id" component={ MealDetails } />
+        <Route exact path="/bebidas/:id" component={ DrinkDetails } />
         <Route path="/comidas/:id/in-progress" component={ InProgress } />
         <Route path="/bebidas/:id/in-progress" component={ InProgress } />
-        <Route path="/explorar" />
-        <Route path="/explorar/comidas" />
-        <Route path="/explorar/bebidas" />
+        <Route exact path="/explorar" />
+        <Route exact path="/explorar/comidas" />
+        <Route exact path="/explorar/bebidas" />
         <Route path="/explorar/comidas/ingredientes" />
         <Route path="/explorar/bebidas/ingredientes" />
         <Route path="/explorar/comidas/area" />
-        <Route path="/perfil" />
+        <Route path="/perfil" component={ Profile } />
         <Route path="/receitas-feitas" />
         <Route path="/receitas-favoritas" />
       </Switch>
-      <Footer />
+      <Switch>
+        <Route exact path="/" />
+        <Route path="/comidas/:id" />
+        <Route path="/bebidas/:id" />
+        <Route path="/" component={ Footer } />
+      </Switch>
     </Provider>
   );
 }
