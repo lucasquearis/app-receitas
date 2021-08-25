3fdf47f591e1accd5e8839c65fe4c202c1523409
Augusto <augustocm2002@yahoo.com.br>
2021-08-25 18:07:26 -0300

    Implementa os elementos da tela de explorar bebidas ou comidas e desenvolve os botoes. - Resolve requisitos 70 e 71

--- src/pages/exploreDrink/ExploreDrink.js
+++ src/pages/exploreDrink/ExploreDrink.js
@@ -4,6 +4,8 @@ import HeaderWithoutSearch from '../../components/header/HeaderWithoutSearch';
 const ExploreDrink = () => (
   <div>
     <HeaderWithoutSearch>Explorar Bebidas</HeaderWithoutSearch>
+    <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+    <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
   </div>
 );
 
--- src/pages/exploreFood/ExploreFood.js
+++ src/pages/exploreFood/ExploreFood.js
@@ -4,6 +4,9 @@ import HeaderWithoutSearch from '../../components/header/HeaderWithoutSearch';
 const ExploreFood = () => (
   <div>
     <HeaderWithoutSearch>Explorar Comidas</HeaderWithoutSearch>
+    <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+    <button type="button" data-testid="explore-by-area">Por Local de Origem</button>
+    <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
   </div>
 );
 
