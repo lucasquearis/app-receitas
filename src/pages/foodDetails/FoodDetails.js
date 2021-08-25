b319a9915dda446c87e92fc94a571251627e1af7
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-25 15:41:32 -0300

    implementa chamada de API em FoodDetails

--- src/pages/foodDetails/FoodDetails.js
+++ src/pages/foodDetails/FoodDetails.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-const FoodDetails = () => (
-  <div />
-);
+const FoodDetails = ({ match: { params: id } }) => {
+  const [meal, setMeal] = useState({});
+  useEffect(() => {
+    const fecthDetails = () => {
+      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id.id}`)
+        .then((response) => response.json())
+        .then((response) => setMeal(response));
+    };
+    fecthDetails();
+  }, [id]);
+  console.log(meal);
+  return (
+    <div />
+  );
+};
+
+FoodDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.node.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default FoodDetails;
