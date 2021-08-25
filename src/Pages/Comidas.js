407dab7cdea35a30e5c6802193611c6a12452d65
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-08-25 14:45:49 -0300

    Adiciona tela de comidas

--- src/Pages/Comidas.js
+++ src/Pages/Comidas.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as ComidasAPI from '../service/ComidasAPI';
+import Card from '../Components/Card';
 
 export default function Comidas() {
+  const [comidas, setComidas] = useState([]);
+  const fetchFoods = async () => {
+    const maxFood = 12;
+    const results = await ComidasAPI.buscarTodasComidasPorLetra('b');
+    const comidasFiltered = results.filter((result, index) => index < maxFood);
+    setComidas(comidasFiltered);
+  };
+
+  fetchFoods();
+  if (comidas.length <= 0) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <h1>Tela de Comidas</h1>
+    <section>
+      {
+        comidas.map(
+          (comida, index) => (
+            <Card key={ comida.idMeal } comida={ comida } index={ index } isFood />
+          ),
+        )
+      }
+    </section>
   );
 }
