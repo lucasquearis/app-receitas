02093294280c67d77617e08b2d727c3a7f0d2899
jmlinger <jmlinger10@gmail.com>
2021-08-25 18:15:16 -0300

    passando info 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,11 @@ import { Link } from 'react-router-dom';
 const Footer = () => (
   <footer data-testid="footer">
     <Link to="/bebidas" data-testid="drinks-bottom-btn">
-      <button type="button">
-        Bebidas
-      </button>
+      <img src="../images/drinkIcon.svg" alt="icon" />
     </Link>
     <Link to="/explorar" data-testid="explore-bottom-btn">
       <button type="button">
         Explorar
-        <image src="../images/drinkIcon" />
       </button>
     </Link>
     <Link to="/comidas" data-testid="food-bottom-btn">
