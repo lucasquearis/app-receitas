fdbdccd4b543907d62892b4eee100046ce1082e2
Wellington <junior.wel02@exemplo.br>
2021-08-25 18:30:15 -0300

    falta 26 e 32 por estarem bugadas

--- src/components/CategoriesButtons.jsx
+++ src/components/CategoriesButtons.jsx
@@ -1,15 +1,16 @@
+import PropTypes from 'prop-types';
 import React, { useContext } from 'react';
 import DrinkContext from '../context/DrinkContext';
 import FoodContext from '../context/FoodContext';
-import { fetchDrinkByCategorie } from '../services/cocktailAPI';
-import { fetchFoodByCategorie } from '../services/mealAPI';
+import { fetchDrinkByCategorie, fetchInicialDrinks } from '../services/cocktailAPI';
+import { fetchFoodByCategorie, fetchInicialFoods } from '../services/mealAPI';
 
 function CategoriesButtons({ categories, type }) {
   const { setDrinks } = useContext(DrinkContext);
   const { setFoods } = useContext(FoodContext);
   const categoriesLimit = 5;
 
-  const onClick = async (categorie) => {
+  async function onClick(categorie) {
     if (type === 'bebidas') {
       const { drinks } = await fetchDrinkByCategorie(categorie);
       setDrinks(drinks);
@@ -17,25 +18,57 @@ function CategoriesButtons({ categories, type }) {
       const { meals } = await fetchFoodByCategorie(categorie);
       setFoods(meals);
     }
-  };
+  }
+
+  async function onClickAll() {
+    if (type === 'bebidas') {
+      const { drinks } = await fetchInicialDrinks();
+      setDrinks(drinks);
+    } else {
+      const { meals } = await fetchInicialFoods();
+      setFoods(meals);
+    }
+  }
 
   return (
-    categories.map((name, index) => (
-      index < categoriesLimit ? (
-        <label htmlFor={ `${name.strCategory}` } key={ index }>
-          <input
-            type="checkbox"
-            id={ `${name.strCategory}` }
-            key={ index }
-            name={ `${name.strCategory} category` }
-            data-testid={ `${name.strCategory}-category-filter` }
-            value={ name.strCategory }
-            onClick={ () => onClick(name.strCategory) }
-          />
-          { name.strCategory }
-        </label>) : null
-    ))
+    <div>
+      <label htmlFor="All">
+        <input
+          type="radio"
+          id="All"
+          name="radio-category"
+          data-testid="All-category-filter"
+          value="All"
+          onClick={ () => onClickAll() }
+        />
+        All
+      </label>
+      { categories.map((name, index) => (
+        index < categoriesLimit ? (
+          <label htmlFor={ `${name.strCategory}` } key={ index }>
+            <input
+              type="radio"
+              id={ `${name.strCategory}` }
+              key={ index }
+              name="radio-category"
+              data-testid={ `${name.strCategory}-category-filter` }
+              value={ name.strCategory }
+              onClick={ () => onClick(name.strCategory) }
+            />
+            { name.strCategory }
+          </label>
+        ) : null)) }
+    </div>
   );
 }
+
+CategoriesButtons.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  type: PropTypes.string,
+};
+
+CategoriesButtons.defaultProps = {
+  type: PropTypes.any,
+};
 
 export default CategoriesButtons;
