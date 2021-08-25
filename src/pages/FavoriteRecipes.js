67d8fa3bdb1f8209627a7468fbffe0b1d0225579
Caio Morato <morato_dev@protonmail.com>
2021-08-25 16:31:18 -0300

    componentes iniciais e botoes

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -2,12 +2,34 @@
 import React from 'react';
 // components
 import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Footer from '../components/FooterMenu';
+// styles
+import '../styles/FavoriteRecipes.css'
 
 function FavoriteRecipes() {
+  const localStorageData = JSON.parse(localStorage.getItem('favoriteRecipes'));
 
   return (
-    
-  )
+    <main>
+      <Header />
+      <div className="filter-buttons">
+        <button type="button">
+          All
+        </button>
+        <button type="button">
+          Food
+        </button>
+        <button type="button">
+          Drinks
+        </button>
+      </div>
+      <div className="favorites-container">
+        <div>
+          a
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
 }
 export default FavoriteRecipes;
