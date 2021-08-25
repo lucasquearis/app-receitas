9cdd86a862f1397c9c268e4eb523b4b0c2c359bd
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-25 17:14:57 -0300

    adicionando logica para ingredientes

--- src/pages/ComidasDetails.js
+++ src/pages/ComidasDetails.js
@@ -4,11 +4,25 @@ import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function ComidasDetails(props) {
-  const [meal, setMeal] = useState({});
   const { match: { params : { id } } } = props;
   const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
   const favorite = favoriteRecipes && favoriteRecipes.find((recipe) => recipe.idMeal === id);
+  const [meal, setMeal] = useState({});
   const [isFav, setIsFav] = useState(favorite);
+
+  useEffect(() => {
+    const getMeal = async () => {
+      const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+      try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setMeal(data.meals[0]);
+      } catch {
+
+      }
+    }
+    getMeal();
+  }, []);
  
   const favoritingRecipe = () => {
     if (isFav) {
@@ -21,16 +35,17 @@ function ComidasDetails(props) {
       localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoriteRecipes));
     }
   }
-
-  useEffect(() => {
-    const getMeal = async () => {
-      const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
-      const response = await fetch(URL);
-      const data = await response.json();
-      setMeal(data.meals[0]);
+  const renderingIngredients = () => {
+    const ingredients = [];
+    const measures = [];
+    for (let index = 1; index <= 20; index += 1) {
+      if(meal[`strIngredient${index}`]){
+        ingredients.push(meal[`strIngredient${index}`]);
+        measures.push(meal[`strMeasure${index}`]);
+      }
     }
-    getMeal();
-  }, []);
+    return { ingredients, measures };
+  }
 
   return (
     <main>
@@ -38,14 +53,30 @@ function ComidasDetails(props) {
         <img data-testid="recipe-photo" src={meal.strMealThumb} alt="imagem do prato"/>
       </div>
       <div>
-        {meal.strMeal}
+        <h1 data-testid="recipe-title">{meal.strMeal}</h1>
         <div>
-          <img src={ shareIcon } alt="imagem de compartilhar" />
+          <img
+            src={ shareIcon }
+            alt="imagem de compartilhar"
+            data-testid="share-btn"
+          />
           <img
             src={ isFav ? blackHeartIcon : whiteHeartIcon }
             alt="imagem de favoritar"
             onClick={ favoritingRecipe }
+            data-testid="favorite-btn"
           />
+        </div>
+        <p data-testid="recipe-category">{meal.strCategory}</p>
+        <div>
+          <ul>
+            { renderingIngredients().ingredients.map((ingredient, index) => {
+              const measures = renderingIngredients().measures;
+              console.log(measures);
+              return <li key={ ingredient }>{ ingredient } - {measures[index]} </li>;
+            }
+            ) }
+          </ul>
         </div>
       </div>
     </main>
