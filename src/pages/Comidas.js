73b9616a4577dcf82bd8ffe8eac813cf8bd606b4
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-25 16:10:33 -0300

    adicionando useEffect para fetch do url

--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Comidas() {
+  const [meal, setMeal] = useState({});
+  useEffect(() => {
+    const getMeal = async () => {
+      const URL = 'www.themealdb.com/api/json/v1/1/lookup.php?i=52772';
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+    }
+    getMeal();
+  }, []);
   return (
     <div>
       <Header title="Comidas" />
+      <main>
+
+      </main>
       <Footer />
     </div>
   );
