8d663ee33811b13915e3fe06da15f949a91ea699
Kevin Oliveira <kevin.zero@hotmail.com>
2021-08-25 13:48:59 -0300

    [GRUPO 27][KEVIN OLIVEIRA][REQUISITOS 33 A 38] (#237)
    * Finaliza requisitos 33, 34 e 38
    
    * Corrige App.css

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Header } from './Header/Header';
 export { default as Footer } from './Footer/Footer';
 export { default as LoginForm } from './LoginForm/LoginForm';
+export { default as RecipeDetails } from './RecipeDetails/RecipeDetails';
 export { default as RecipeList } from './RecipeList/RecipeList';
