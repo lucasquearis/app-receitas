e144800986468bdb352d26484dc5708ebdb704cf
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-08-25 14:37:43 -0300

    Inicia testes

---
+++ src/tests/mealMock.js
@@ -0,0 +1,17 @@
+// const mealMock = {
+//   idMeal: '52977',
+//   strMeal: 'Corba',
+//   strMealThumb: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+// };
+
+// const mealMock = async () => {
+//   try {
+//     const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
+
+// module.exports = mealMock;
