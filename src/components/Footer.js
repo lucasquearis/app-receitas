474e7b9981294ad2bc0ae77d6d685210433c6ad5
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-08-25 09:01:16 -0300

    Estruturação basica do footer.

---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function Footer() {
+  return (
+    <footer
+      style={ {
+        position: 'absolute',
+        bottom: '0',
+      } }
+      data-testid="footer"
+    >
+      <Link to="/bebidas">
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="Icone de Bebidas"
+        />
+      </Link>
+      <Link to="/explorar">
+        <img
+          data-testid="explore-bottom-btn"
+          src={ exploreIcon }
+          alt="Icone de Exploração"
+        />
+      </Link>
+      <Link to="/comidas">
+        <img
+          data-testid="food-bottom-btn"
+          src={ mealIcon }
+          alt="Icone de Comidas"
+        />
+      </Link>
+    </footer>
+  );
+}
+
+export default Footer;
