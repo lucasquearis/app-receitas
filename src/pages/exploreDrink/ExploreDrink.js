0f72ef236a8ea3c9cd84c97d919cf6705a1f93ce
Augusto <augustocm2002@yahoo.com.br>
2021-08-25 19:04:49 -0300

    Acrescenta Link e faz requisito 72

--- src/pages/exploreDrink/ExploreDrink.js
+++ src/pages/exploreDrink/ExploreDrink.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderWithoutSearch from '../../components/header/HeaderWithoutSearch';
 
 const ExploreDrink = () => (
   <div>
     <HeaderWithoutSearch>Explorar Bebidas</HeaderWithoutSearch>
-    <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+    <Link to="/explorar/bebidas/ingredientes">
+      <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+    </Link>
     <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
   </div>
 );
--- src/pages/exploreFood/ExploreFood.js
+++ src/pages/exploreFood/ExploreFood.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderWithoutSearch from '../../components/header/HeaderWithoutSearch';
 
 const ExploreFood = () => (
   <div>
     <HeaderWithoutSearch>Explorar Comidas</HeaderWithoutSearch>
-    <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+    <Link to="/explorar/comidas/ingredientes">
+      <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+    </Link>
     <button type="button" data-testid="explore-by-area">Por Local de Origem</button>
     <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
   </div>
