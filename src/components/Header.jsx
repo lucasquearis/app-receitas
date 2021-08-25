3423f9307d173944a29c470118a3f2f84c9c1f5f
camila-mp <ca.malvp@gmail.com>
2021-08-25 10:47:29 -0300

    conclui requisitos 11 e 12: redirecionar para /perfil ao clicar no icone perfil e mostrar SearchBar ao clicar no icone de busca

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
+import SearchBar from './SearchBar';
 import './header.css';
 
 function Header({ title, searchIcon = '' }) {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
+  const clickSearch = () => {
+    if (showSearchBar === false) {
+      setShowSearchBar(true);
+    } else {
+      setShowSearchBar(false);
+    }
+  };
+
   const renderSearchIcon = () => {
     if (searchIcon) {
-      return <img data-testid="search-top-btn" src={ searchIcon } alt="icone da busca" />;
+      return (
+        <button type="button" className="search-button" onClick={ clickSearch }>
+          <img
+            data-testid="search-top-btn"
+            src={ searchIcon }
+            alt="icone da busca"
+          />
+        </button>
+      );
     }
+    return <div />;
   };
 
   return (
     <header>
       <div className="header-div">
-        <img data-testid="profile-top-btn" src={ profileIcon } alt="icone do perfil" />
+        <Link to="/perfil" className="profile-link">
+          <img
+            id="profile-icon"
+            data-testid="profile-top-btn"
+            src={ profileIcon }
+            alt="icone do perfil"
+          />
+        </Link>
         <h3 data-testid="page-title">{ title }</h3>
         { renderSearchIcon() }
       </div>
+      { showSearchBar && <SearchBar /> }
     </header>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import './header.css';
 
 export default function SearchBar() {
   return (
-    <div>Searchbar</div>
+    <form className="search-form">
+      <input
+        placeholder="Buscar receita"
+        data-testid="search-input"
+        type="text"
+      />
+      <div className="search-radio-btns">
+        <label htmlFor="radio-ingredients">
+          Ingredientes
+          <input
+            data-testid="ingredient-search-radio"
+            id="radio-ingredients"
+            type="radio"
+            value="Ingredientes"
+          />
+        </label>
+        <label htmlFor="radio-name">
+          Nome
+          <input
+            data-testid="name-search-radio"
+            id="radio-name"
+            type="radio"
+            value="Nome"
+          />
+        </label>
+        <label htmlFor="radio-first-letter">
+          Primeira letra
+          <input
+            data-testid="first-letter-search-radio"
+            id="radio-first-letter"
+            type="radio"
+            value="Primeira letra"
+          />
+        </label>
+      </div>
+    </form>
   );
 }
