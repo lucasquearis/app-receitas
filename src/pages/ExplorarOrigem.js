7a994e1d02e927178a378ebb32bfbd7afcb62d03
akirakiraa <andretaki2@gmail.com>
2021-08-25 15:50:48 -0300

    req 21 fix

--- src/pages/ExplorarOrigem.js
+++ src/pages/ExplorarOrigem.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function ExplorarOrigem() {
   return (
     <div>
       <Header titulo="Explorar Origem" showProfileIcon="sim" />
+      <Footer />
     </div>
   );
 }
