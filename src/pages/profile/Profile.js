878a56cb6e7b5b5d8217e86378188bf2a5f7296f
Pedro <peg.gep27@gmail.com>
2021-08-25 19:10:26 -0300

    Requisitos 82, 83 e 84 - Mostra o email do usuario, guardado no localStorage, e cria botoes para receitas feitas, receitas favoritas e sair

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import HeaderWithoutSearch from '../../components/header/HeaderWithoutSearch';
 
-const Profile = () => (
-  <div>
-    <HeaderWithoutSearch>Perfil</HeaderWithoutSearch>
-  </div>
-);
+const Profile = () => {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+  console.log(email);
+  return (
+    <div>
+      <HeaderWithoutSearch>Perfil</HeaderWithoutSearch>
+      <p data-testid="profile-email">{ email }</p>
+      <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
+      <button type="button" data-testid="profile-logout-btn">Sair</button>
+    </div>
+  );
+};
 
 export default Profile;
