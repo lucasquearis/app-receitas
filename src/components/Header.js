6f411ef138cdecaa4ae69a468cbe4d5db38ba4d8
Pedro Resck <78392325+PedroResck@users.noreply.github.com>
2021-08-25 18:07:28 -0300

    [Grupo 4] - Requisitos 9/10/11/12 (#337)
    * Cria o header
    
    * Cria o header para todas as paginas do app - Requisitos 9/10/11/12
    
    Co-authored-by: Ygor Saturnino <82230734+YgorSaturnino@users.noreply.github.com>

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import profileIcon from '../images/profileIcon.svg';
+
+class HeaderSearch extends React.Component {
+  handleSearch() {
+    const search = document.querySelector('#searchbar');
+    console.log(search);
+    if (search.style.visibility === 'hidden') {
+      search.style.visibility = 'visible';
+    } else {
+      search.style.visibility = 'hidden';
+    }
+  }
+
+  render() {
+    const { title } = this.props;
+    return (
+      <div>
+        <Link to="/perfil">
+          <img
+            data-testid="profile-top-btn"
+            src={ profileIcon }
+            alt="imagem do link do perfil"
+          />
+        </Link>
+        <h1 data-testid="page-title">{title}</h1>
+      </div>
+    );
+  }
+}
+
+HeaderSearch.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default HeaderSearch;
