e282448dc4f4ada3fc4525c15b6b45e37802bb1a
José Carlos <kalarruda@hotmail.com>
2021-08-26 10:45:54 -0300

    requisitos 67 e 68 - adiciona botoes de explorar bebidas e comidas

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
+import { Link } from 'react-router-dom';
 
 function Explore() {
   return (
     <div>
       <Header title="Explorar" />
+      <Link>
+        <button data-testid="explore-food">
+          Explorar Comidas
+        </button>
+      </Link>
+      <Link>
+        <button data-testid="explore-drinks">
+          Explorar Bebidas
+        </button>
+      </Link>
       <Footer />
     </div>
   );
