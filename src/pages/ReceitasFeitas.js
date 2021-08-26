f4a3f0bccce371e02a8d464829acb67488c12e49
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-26 16:50:30 -0300

    adicionando link e imagem ao card das comidas feitas

--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Button from '../components/Button';
 
@@ -7,21 +8,32 @@ function ReceitasFeitas() {
   const [filteredRecipes, setFilteredRecipes] = useState(doneRecipes);
 
   const handleClickFilteredRecipes = (type) => {
-    if(type) {
+    if (type) {
       const recipes = doneRecipes.filter((recipe) => recipe.type === type);
       setFilteredRecipes(recipes);
     } else {
       setFilteredRecipes(doneRecipes);
     }
-  }
+  };
 
   const renderCards = () => (
     <div>
-      { filteredRecipes.map((recipe, index) => (
-        <div key={ recipe.name } className="card-container">
-          <img src={recipe.image} alt={recipe.name} />
-        </div>
-      ))}
+      { filteredRecipes.map((recipe, index) => {
+        const pathToRecipe = recipe.type === 'comida' ? `/comidas/${recipe.id}` : `/bebidas/${recipe.id}`;
+        return (
+        <Link
+          to={ pathToRecipe }
+          key={ recipe.name }
+          className="card-container"
+        >
+          <div>
+            <img src={ recipe.image } alt={ recipe.name } />  
+          </div>
+          <div>
+
+          </div>
+        </Link>
+      );})}
     </div>
   );
 
