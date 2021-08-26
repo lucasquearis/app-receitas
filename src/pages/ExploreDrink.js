8e93244df2f58f50d974001d5f0a9ee13156f99f
José Carlos <kalarruda@hotmail.com>
2021-08-26 11:45:47 -0300

    requisitos 70 e 71 - adiciona botoes nas paginas explorar bebidas e explorar comidas

--- src/pages/ExploreDrink.js
+++ src/pages/ExploreDrink.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 function ExploreDrink() {
   return (
     <div>
       <Header title="Explorar Bebidas" />
+      <Link>
+        <button data-testid="explore-by-ingredient">
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link>
+        <button data-testid="explore-surprise">
+          Me Surpreenda!
+        </button>
+      </Link>
     </div>
   );
 }
--- src/pages/ExploreFood.js
+++ src/pages/ExploreFood.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 function ExploreFood() {
   return (
     <div>
       <Header title="Explorar Comidas" />
+      <Link>
+        <button data-testid="explore-by-ingredient">
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link>
+        <button data-testid="explore-by-area">
+          Por Local de Origem
+        </button>
+      </Link>
+      <Link>
+        <button data-testid="explore-surprise">
+          Me Surpreenda!
+        </button>
+      </Link>
     </div>
   );
 }
