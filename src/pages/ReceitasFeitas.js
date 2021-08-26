478f04eefad1b0489cb953a9a83366bb8ca9d640
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-26 17:54:48 -0300

    complementando informacoes pagina de receitas feitas

--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Button from '../components/Button';
+import shareIcon from '../images/shareIcon.svg';
 
 function ReceitasFeitas() {
   const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
   const [filteredRecipes, setFilteredRecipes] = useState(doneRecipes);
+  const [linkShare, setLinkShare] = useState(false);
 
   const handleClickFilteredRecipes = (type) => {
     if (type) {
@@ -16,23 +18,59 @@ function ReceitasFeitas() {
     }
   };
 
+  const renderCardDetails = (recipe, index, path) => (
+    <div>
+      <div>
+        { recipe.category === 'Cocktail' ? recipe.Alcoholic : recipe.category }
+        <button
+          onClick={ () => {
+            // copy(`http://localhost:3000${location.pathname}`);
+            setLinkShare(true);
+          } }
+          type="button"
+        >
+          <img
+            src={ shareIcon }
+            alt="imagem de compartilhar"
+            data-testid={ `${index}-horizontal-share-btn` }
+          />
+        </button>
+        { linkShare && 'Link copiado!' }
+      </div>
+      <Link data-testid={ `${index}-horizontal-name` } to={ path }>
+        { recipe.name }
+      </Link>
+      <p data-testid={ `${index}-horizontal-done-date` }>
+        {`Data feita: ${recipe.doneDate}`}
+      </p>
+      <div className="tag-container">
+        {recipe.tags.map((tag) => (
+          <p data-testid={ `${index}-${tag}-horizontal-tag` }>
+            { tag }
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+
   const renderCards = () => (
     <div>
       { filteredRecipes.map((recipe, index) => {
         const pathToRecipe = recipe.type === 'comida' ? `/comidas/${recipe.id}` : `/bebidas/${recipe.id}`;
         return (
-        <Link
-          to={ pathToRecipe }
+        <div
           key={ recipe.name }
           className="card-container"
         >
-          <div>
-            <img src={ recipe.image } alt={ recipe.name } />  
-          </div>
-          <div>
-
-          </div>
-        </Link>
+          <Link to={ pathToRecipe }>
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ recipe.image }
+              alt={ recipe.name }
+            />  
+          </Link>
+          { renderCardDetails(recipe, index, pathToRecipe) }
+        </div>
       );})}
     </div>
   );
