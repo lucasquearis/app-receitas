1956d80f7b12a6711840f4f6e4c3680863e74f6b
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-26 16:15:24 -0300

    adicionando botoes a receitas feitas

--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import Header from '../components/Header';
+import Button from '../components/Button';
 
 function ReceitasFeitas() {
   return (
     <div>
       <Header title="Receitas Feitas" renderSearch={ false } />
+      <main>
+        <div className="button-container">
+          <Button
+            text="All"
+            onClick={ () => { }}
+            testId="filter-by-all-btn"
+            />
+          <Button
+            text="Food"
+            onClick={ () => { }}
+            testId="filter-by-food-btn"
+            />
+          <Button
+            text="Drinks"
+            onClick={ () => { }}
+            testId="filter-by-drink-btn"
+            />
+        </div>
+      </main>
     </div>
   );
 }
