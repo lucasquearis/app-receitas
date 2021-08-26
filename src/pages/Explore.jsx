d9bdb1fe7ef77f9a96200e0d90ec79e0c9d2eae3
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-08-26 11:25:42 -0300

    Estrutura pagina Explore e seus testes.

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-export default function () {
+export default function Explore() {
   return (
     <div>
-      <div>Explore</div>
+      <Header title="Explorar" />
+      <div className="explore-container">
+        <Link
+          to="/explorar/comidas"
+          data-testid="explore-food"
+          className="explore-btn"
+        >
+          Explorar Comidas
+        </Link>
+        <Link
+          to="/explorar/bebidas"
+          data-testid="explore-drinks"
+          className="explore-btn"
+        >
+          Explorar Bebidas
+        </Link>
+      </div>
       <Footer />
     </div>
   );
