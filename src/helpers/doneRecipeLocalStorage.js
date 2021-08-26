0660e08e1549eafbc5af79df2ab712b809ea986a
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-08-26 16:56:33 -0300

    muda chave type do localstorage de comidas finalizadas

--- src/helpers/doneRecipeLocalStorage.js
+++ src/helpers/doneRecipeLocalStorage.js
@@ -5,7 +5,7 @@ function saveDoneMeal(rcp) {
       ...doneRecipes,
       {
         id: rcp.idMeal,
-        type: 'meal',
+        type: 'comida',
         area: rcp.strArea ? rcp.strArea : '',
         category: rcp.strCategory ? rcp.strCategory : '',
         alcoholicOrNot: rcp.strAlcoholic ? rcp.strAlcoholic : '',
@@ -25,7 +25,7 @@ function saveDoneDrink(rcp) {
       ...doneRecipes,
       {
         id: rcp.idDrink,
-        type: 'drink',
+        type: 'bebida',
         area: rcp.strArea ? rcp.strArea : '',
         category: rcp.strCategory ? rcp.strCategory : '',
         alcoholicOrNot: rcp.strAlcoholic ? rcp.strAlcoholic : '',
