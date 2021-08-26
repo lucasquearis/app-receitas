2e053648c2197a5f4229af0de9bfd54c2ccc07b2
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-08-26 17:21:27 -0300

    aprovado requisitos searchbar e aprovado no lint

--- src/tests/HeaderSearchBar.test.js
+++ src/tests/HeaderSearchBar.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { screen } from '@testing-library/dom';
 import userEvent from '@testing-library/user-event';
-import MutationObserver from '@sheerun/mutationobserver-shim';
-import Header from '../components/Header/Header';
+// import MutationObserver from '@sheerun/mutationobserver-shim';
+import Header from '../components/Header';
 import renderWithRouter from './renderWithRouter';
 
 describe('Testa elementos na barra de busca', () => {
