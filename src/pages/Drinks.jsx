a417c26f046a4ca80e42bc2651f26df8d33b8ef6
Samuel Constantino <samuelicapuidados@gmail.com>
2021-08-26 09:15:17 -0300

    fix 3/4 warnings

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -15,7 +15,7 @@ export default function () {
 
   // ficou assim para passar no teste, mas fica meio quebrado ainda
   if (!drinks) {
-    alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+    console.log('Sinto muito, não encontramos nenhuma receita para esses filtros.');
     return (<Redirect to="/bebidas" />);
   }
 
