e5492b120f20dd7f002250f738b9b2644e793377
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-26 19:57:53 -0300

    implementa chamada de API em DrinkDetails

--- src/pages/drinkDetails/DrinkDetails.js
+++ src/pages/drinkDetails/DrinkDetails.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-const DrinkDetails = () => (
-  <div />
-);
+const DrinkDetails = ({ match: { params: id } }) => {
+  const [drink, setDrink] = useState(0);
+  useEffect(() => {
+    const fetchDrink = () => {
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id.id}`)
+        .then((resolve) => resolve.json())
+        .then((resolve) => setDrink(resolve.drinks[0]));
+    };
+    fetchDrink();
+  }, [id.id]);
+  console.log(drink);
+  return (
+    <div>oi</div>
+  );
+};
+
+DrinkDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.node.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default DrinkDetails;
