4cac582c1ac91ef375aa384d1b5679bf9216e5e4
Armando Guerra <82233849+armando771@users.noreply.github.com>
2021-08-25 23:21:39 -0300

    corrigindo conflito com o cypress requisito 11 (#374)

--- cypress/integration/header_spec.js
+++ cypress/integration/header_spec.js
@@ -130,7 +130,7 @@ describe('10 - Implemente um ícone para a tela de perfil, um título e um ícon
   });
 });
 
-describe('11 - Redirecione a pessoa usuária para a tela de perfil ao clicar no botão de perfil', () => {
+describe.only('11 - Redirecione a pessoa usuária para a tela de perfil ao clicar no botão de perfil', () => {
   it('A mudança de tela ocorre corretamente', () => {
     cy.visit('http://localhost:3000/comidas');
 
