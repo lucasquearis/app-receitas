693c93b04ddbda87ead8e9857b1c8751b135a718
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-08-25 23:38:04 -0300

    Add end points para pegar as comidas ou bebidas por categoria

--- src/services/index.js
+++ src/services/index.js
@@ -3,5 +3,7 @@ export {
   DRINKS_LIST,
   MEALS_CATEGORIES,
   DRINKS_CATEGORIES,
+  MEALS_BY_CATEGORY,
+  DRINKS_BY_CATEGORY,
 } from './endPoints';
 export { default as fetchApi } from './fetchApi';
