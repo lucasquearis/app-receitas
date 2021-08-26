d45ea7ab75332b810fecb983747a04c3c747a4b8
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-08-26 15:02:37 -0300

    Tentativa de resolve linter do CSS

--- src/pages/ExplorerDrinks.js
+++ src/pages/ExplorerDrinks.js
@@ -5,26 +5,20 @@ import FooterMenu from './FooterMenu';
 
 function ExplorerDrinks() {
   const [randomDrink, setRandomDrink] = useState();
-  const [redirect, setRedirect] = useState(false);
   const history = useHistory();
 
   useEffect(() => {
     const URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
     const getDrink = async () => {
       const requestDrink = await fetch(URL);
-      console.log(requestDrink);
       const { meals } = await requestDrink.json();
       setRandomDrink(meals[0]);
-      console.log(meals);
     };
     getDrink();
-  }, [redirect]);
+  }, []);
 
   const handleClick = () => {
     const drinkDinamic = `/explorar/bebidas/${randomDrink.idMeal}`;
-    // setRedirect(history.push(drinkDinamic));
-    console.log(drinkDinamic);
-    // getDrink();
     return <Redirect to={ drinkDinamic } />;
   };
   return (
