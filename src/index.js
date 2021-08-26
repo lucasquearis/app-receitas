8afd0741df61ea32973fc1ee8cf0ac29d279a792
cristianocsl <cslcristiano@gmail.com>
2021-08-26 20:22:41 -0300

    tentando consertar erro do npm run lint:style

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from './redux/store/index';
+import store from '../src/redux/store/index';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
