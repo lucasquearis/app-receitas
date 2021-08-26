962a07c0f81a2605e1cfd7693a230c8820993afe
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-08-26 14:33:46 -0300

    Fazendo pull

--- src/pages/IngredientsExploreDrink.js
+++ src/pages/IngredientsExploreDrink.js
@@ -6,6 +6,11 @@ function IngredientsExploreFood() {
   return (
     <div>
       <Header title="Explorar Ingredientes das Bebidas" />
+      <section>
+        {/* <img src={} alt="imagem da comida/bebida" data-testid="recipe-photo" /> */}
+        <h2 data-testid="recipe-title">nome da comida</h2>
+        {/* <button type="button" data-testid="share-btn"></button> */}
+      </section>
       <Footer />
     </div>
   );
