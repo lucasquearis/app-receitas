5992cf992ed16ca1336e95237998bf94ce009b03
Pedro <peg.gep27@gmail.com>
2021-08-26 15:02:26 -0300

    Requisitos 85, 86 e 87 - Redireciona o usuario baseado no botao que for clicado no tela de perfil. Se o botao clicado for Sair, o localStorage sera limpo

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import HeaderWithoutSearch from '../../components/header/HeaderWithoutSearch';
 
 const Profile = () => {
+  const [redirect, setRedirect] = useState({
+    bool: false,
+  });
+
   const emailObject = JSON.parse(localStorage.getItem('user')) === null ? ''
     : JSON.parse(localStorage.getItem('user'));
   const email = emailObject === '' ? '' : emailObject.email;
+
+  if (redirect.bool) {
+    return <Redirect to={ `${redirect.path}` } />;
+  }
   return (
     <div>
       <HeaderWithoutSearch>Perfil</HeaderWithoutSearch>
       <p data-testid="profile-email">{ email }</p>
-      <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
-      <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
-      <button type="button" data-testid="profile-logout-btn">Sair</button>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => {
+          setRedirect({
+            bool: true,
+            path: '/receitas-feitas',
+          });
+        } }
+      >
+        Receitas Feitas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => {
+          setRedirect({
+            bool: true,
+            path: '/receitas-favoritas',
+          });
+        } }
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => {
+          localStorage.clear();
+          setRedirect({
+            bool: true,
+            path: '/',
+          });
+        } }
+      >
+        Sair
+      </button>
     </div>
   );
 };
