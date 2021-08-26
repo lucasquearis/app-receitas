9cbbf27a5e0365ecfe91fd9ed587112439e97172
Pedro <peg.gep27@gmail.com>
2021-08-26 16:39:17 -0300

    Requisito 78 - Cria select com todas as area e busca e renderiza as 12 primeiras receitas referentes a area selecionada

--- src/pages/exploreFoodArea/ExploreFoodArea.js
+++ src/pages/exploreFoodArea/ExploreFoodArea.js
@@ -1,10 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CardsList from '../../components/CardsList/CardsList';
 import Header from '../../components/header/Header';
 
-const ExploreFoodArea = () => (
-  <div>
-    <Header>Explorar Origem</Header>
-  </div>
-);
+const ExploreFoodArea = () => {
+  const [areas, setAreas] = useState();
+  const [currentArea, setCurrentArea] = useState();
+  const [filteredFoods, setFilteredFoods] = useState({
+    bool: false,
+    foods: [],
+  });
+
+  useEffect(() => {
+    const getAreas = async () => {
+      const url = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
+      const response = await fetch(url);
+      const { meals } = await response.json();
+      setAreas(meals);
+    };
+    getAreas();
+    setCurrentArea('American');
+  }, []);
+
+  useEffect(() => {
+    if (currentArea !== undefined) {
+      const getFoodsByArea = async () => {
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${currentArea}`;
+        const response = await fetch(url);
+        const { meals } = await response.json();
+
+        let mealsArray;
+        const maxCards = 12;
+        if (meals.length > maxCards) {
+          mealsArray = meals.slice(0, maxCards);
+        } else {
+          mealsArray = meals;
+        }
+        setFilteredFoods({
+          bool: true,
+          foods: mealsArray,
+        });
+      };
+      getFoodsByArea();
+    }
+  }, [currentArea]);
+
+  const handleChange = ({ target: { value } }) => {
+    setCurrentArea(value);
+  };
+
+  if (areas !== undefined) {
+    return (
+      <div>
+        <Header>Explorar Origem</Header>
+        <select
+          data-testid="explore-by-area-dropdown"
+          onChange={ handleChange }
+        >
+          { areas.map(({ strArea }) => (
+            <option
+              key={ strArea }
+              data-testid={ `${strArea}-option` }
+            >
+              { strArea }
+            </option>
+          )) }
+        </select>
+        { filteredFoods.bool && (<CardsList array={ filteredFoods.foods } />) }
+      </div>
+    );
+  }
+  return null;
+};
 
 export default ExploreFoodArea;
