d9321f5bfe899f45ff43c85fc72dcb964b67dee8
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-08-26 15:31:53 -0300

    cria componente de loading

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Loading() {
+  return (
+    <img
+      src="https://i.pinimg.com/originals/e2/28/ff/e228ff2687af1aa373fe90477fa0830b.gif"
+      alt="Gif do personagem Sanji equilibrando bandejas de comida nas mãos"
+    />
+  );
+}
+
+export default Loading;
