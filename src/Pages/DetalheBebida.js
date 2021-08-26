e3df563ac66a7f71141143a2cad5dc4f645831df
Josimar Souza <contact.josimarsouza@gmail.com>
2021-08-26 11:10:18 -0300

    Finaliza estrutura da pagina DetalheBebidas

--- src/Pages/DetalheBebida.js
+++ src/Pages/DetalheBebida.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import * as BebidasAPI from '../service/BebidasAPI';
+import { buscarComidaAleatoria } from '../service/ComidasAPI';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import './DetalheBebida.css';
 
 export default function DetalheBebida() {
   const [drink, setDrink] = useState({});
+  const [drinkIngredients, setDrinkIngredients] = useState([]);
+  const [randomFood, setRamdomFood] = useState([]);
 
   useEffect(() => {
     const getDrink = async () => {
@@ -39,17 +45,97 @@ export default function DetalheBebida() {
         return `- ${ingredient[1]}`;
       });
 
-      console.log(readyIngredients);
+      setDrinkIngredients(readyIngredients);
+    };
+
+    const getRandomFood = async () => {
+      const food1 = await buscarComidaAleatoria();
+      const food2 = await buscarComidaAleatoria();
+
+      const foods = [food1[0], food2[0]];
+      setRamdomFood(foods);
     };
 
     getDrink();
+    getRandomFood();
   }, []);
 
-  console.log(drink);
+  const randomFoodCard = () => (
+    <section className="food-recomended">
+      <h5>Recomendadas</h5>
+      <div className="recomended-food-section-infos">
+        { randomFood.map((food, index) => (
+          <div
+            data-testid={ `${index}-recomendation-card` }
+            className="recomended-food-info"
+            key={ food.idMeal }
+          >
+            <img src={ food.strMealThumb } alt="foto da bebida" />
+            <p>{ food.strCategory }</p>
+            <h5>{ food.strMeal }</h5>
+          </div>
+        )) }
+      </div>
+    </section>
+  );
 
   return (
-    <section>
-      Oi
+    <section className="food-info">
+      <img
+        className="food-drink-image"
+        data-testid="recipe-photo"
+        src={ drink.strDrinkThumb }
+        alt="foto da comida"
+      />
+      <div className="title-drink-info">
+        <h4 data-testid="recipe-title">{ drink.strDrink }</h4>
+        <div className="share-favorite-drink-section">
+          <img
+            data-testid="share-btn"
+            className="share-drink-icon"
+            src={ shareIcon }
+            alt="icone de compartilhar"
+          />
+          <img
+            data-testid="favorite-btn"
+            src={ whiteHeartIcon }
+            alt="icone de favoritar"
+          />
+        </div>
+      </div>
+      <p
+        className="food-drink-category"
+        data-testid="recipe-category"
+      >
+        { drink.strCategory }
+      </p>
+      <div className="ingredients-drink-section">
+        <h5>Ingredients</h5>
+        <ul>
+          {
+            drinkIngredients.map((ingredient, index) => (
+              <li
+                data-testid={ `${index}-ingredient-name-and-measure` }
+                key={ `${index}` }
+              >
+                { ingredient }
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+      <div className="instructions-drink-section">
+        <h5>Instructions</h5>
+        <p data-testid="instructions">{ drink.strInstructions }</p>
+      </div>
+      { randomFoodCard() }
+      <button
+        className="start-drink-recipe-btn"
+        data-testid="start-recipe-btn"
+        type="button"
+      >
+        Iniciar Receita
+      </button>
     </section>
   );
 }
--- src/service/ComidasAPI.js
+++ src/service/ComidasAPI.js
@@ -11,3 +11,10 @@ export async function buscarComidaPeloID(id) {
   const response = await fetch(ENDPOINT).then((data) => data.json());
   return response.meals;
 }
+
+export async function buscarComidaAleatoria() {
+  const ENDPOINT = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+  const response = await fetch(ENDPOINT).then((data) => data.json());
+  return response.meals;
+}
