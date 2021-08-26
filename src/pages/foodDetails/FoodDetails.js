616dd64ca664b3860ecb749872291fbb8f270986
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-26 10:12:45 -0300

    implementa componente HeaderDetails a pagina FoodDetails

--- src/pages/foodDetails/FoodDetails.js
+++ src/pages/foodDetails/FoodDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import HeaderDetails from '../../components/CardDetails/HeaderDetails';
 
 const FoodDetails = ({ match: { params: id } }) => {
   const [meal, setMeal] = useState({});
@@ -7,13 +8,15 @@ const FoodDetails = ({ match: { params: id } }) => {
     const fecthDetails = () => {
       fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id.id}`)
         .then((response) => response.json())
-        .then((response) => setMeal(response));
+        .then((response) => setMeal(response.meals[0]));
     };
     fecthDetails();
   }, [id]);
-  console.log(meal);
+  const { strMeal, strMealThumb, strCategory } = meal;
   return (
-    <div />
+    <div>
+      <HeaderDetails image={ strMealThumb } title={ strMeal } category={ strCategory } />
+    </div>
   );
 };
 
