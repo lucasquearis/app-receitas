0ae25a32cf59d60519c4e9f8df03da33305e9f64
Euller Braz <euller.braz99@gmail.com>
2021-08-26 20:47:38 -0300

    Implementado tela de explorar

--- src/pages/ExploreDrink.jsx
+++ src/pages/ExploreDrink.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import ExploreRecipes from '../components/ExploreRecipes';
 
 export default function ExploreDrink() {
   return (
     <section>
       <Header showExploreIcon pageTitle="Explorar Bebidas" />
+      <ExploreRecipes />
     </section>
   );
 }
--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import ExploreRecipes from '../components/ExploreRecipes';
 
 export default function ExploreFood() {
   return (
     <section>
       <Header showExploreIcon pageTitle="Explorar Comidas" />
+      <ExploreRecipes foods />
     </section>
   );
 }
