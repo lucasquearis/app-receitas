fab84c83e62286f5810269e80e8304c782acb898
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-08-27 11:26:01 -0300

    favorite list

--- src/components/FavoriteCard.js
+++ src/components/FavoriteCard.js
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import shareIconPath from '../images/shareIcon.svg';
-import './css/DoneCard.css';
+// import './css/DoneCard.css';
 
-const FavoriteCard = ({ doneRecipe, index, handleClickShare }) => (
+const FavCard = ({ favRecipe, index, handleClickShare }) => (
   <div className="done-recipe-container">
     <Link
-      to={ `${doneRecipe.type}s/${doneRecipe.id}` }
+      to={ `${favRecipe.type}s/${favRecipe.id}` }
       className="horizontal-image-link"
     >
       <img
-        src={ doneRecipe.image }
+        src={ favRecipe.image }
         alt="Imagem da Receita"
         data-testid={ `${index}-horizontal-image` }
         className="horizontal-image"
@@ -23,15 +23,15 @@ const FavoriteCard = ({ doneRecipe, index, handleClickShare }) => (
           className="done-recipe-category"
           data-testid={ `${index}-horizontal-top-text` }
         >
-          { doneRecipe.type === 'comida' ? (
-            `${doneRecipe.area} - ${doneRecipe.category}`
+          { favRecipe.type === 'comida' ? (
+            `${favRecipe.area} - ${favRecipe.category}`
           ) : (
-            `${doneRecipe.alcoholicOrNot}`
+            `${favRecipe.alcoholicOrNot}`
           ) }
         </span>
         <input
           type="image"
-          id={ `${doneRecipe.type}s-${doneRecipe.id} ${index}` }
+          id={ `${favRecipe.type}s-${favRecipe.id} ${index}` }
           className="done-recipe-share-icon"
           data-testid={ `${index}-horizontal-share-btn` }
           src={ shareIconPath }
@@ -40,26 +40,26 @@ const FavoriteCard = ({ doneRecipe, index, handleClickShare }) => (
         />
       </div>
       <Link
-        to={ `${doneRecipe.type}s/${doneRecipe.id}` }
+        to={ `${favRecipe.type}s/${favRecipe.id}` }
         className="done-recipe-name-link"
       >
         <span
           className="done-recipe-name"
           data-testid={ `${index}-horizontal-name` }
         >
-          { doneRecipe.name }
+          { favRecipe.name }
         </span>
       </Link>
-      <span
+      {/* <span
         className="done-recipe-date"
         id={ `done-recipe-date-${index}` }
         data-testid={ `${index}-horizontal-done-date` }
       >
-        { `Feita em: ${doneRecipe.doneDate}` }
-      </span>
-      <div className="done-recipe-tag-container">
-        { (doneRecipe.type === 'comida') && (doneRecipe.tags.length > 0) ? (
-          (doneRecipe.tags.slice(0, 2)).map((tag, tagIndex) => (
+        { `Feita em: ${favRecipe.doneDate}` }
+      </span> */}
+      {/* <div className="done-recipe-tag-container">
+        { (favRecipe.type === 'comida') && (favRecipe.tags.length > 0) ? (
+          (favRecipe.tags.slice(0, 2)).map((tag, tagIndex) => (
             <span
               key={ tagIndex }
               className="done-recipe-tag"
@@ -71,15 +71,15 @@ const FavoriteCard = ({ doneRecipe, index, handleClickShare }) => (
         ) : (
           <> </>
         )}
-      </div>
+      </div> */}
     </div>
   </div>
 );
 
-FavoriteCard.propTypes = {
-  doneRecipe: PropTypes.PropTypes.shape().isRequired,
+FavCard.propTypes = {
+  favRecipe: PropTypes.PropTypes.shape().isRequired,
   index: PropTypes.number.isRequired,
   handleClickShare: PropTypes.func.isRequired,
 };
 
-export default FavoriteCard;
+export default FavCard;
--- src/pages/FavouriteRecipes.js
+++ src/pages/FavouriteRecipes.js
@@ -1,38 +1,108 @@
-import React, { useContext } from 'react';
-import AppContext from '../context/AppContext';
-import { Button } from '../components';
+import React, { useEffect, useState } from 'react';
+import { Button, FavCard } from '../components';
 import Header from '../components/Header';
+// import './css/DoneRecipes.css';
 
-const FavouriteRecipes = () => {
-  const { favoriteRcps, setFavoriteRcps } = useContext(AppContext);
+const copy = require('clipboard-copy');
+
+const DoneRecipes = () => {
+  const [rawFavRecipes, setRawFavRecipes] = useState([]);
+  const [favRecipes, setFavRecipes] = useState([]);
+  const [filterOption, setFilterOption] = useState('');
+  const [applyFilter, setApplyFilter] = useState(false);
+  const [shareData, setShareData] = useState([]);
+  const [shareAlert, setShareAlert] = useState(false);
+
+  useEffect(() => {
+    const getDoneRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (getDoneRecipes !== null) {
+      setRawFavRecipes(getDoneRecipes);
+      setFavRecipes(getDoneRecipes);
+    } else {
+      setRawFavRecipes([]);
+      setFavRecipes([]);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (applyFilter && (filterOption !== 'all')) {
+      setFavRecipes(rawFavRecipes.filter((recipe) => recipe.type === filterOption));
+    }
+    if (applyFilter && (filterOption === 'all')) {
+      setFavRecipes(rawFavRecipes);
+    }
+    setApplyFilter(false);
+  }, [applyFilter]);
+
+  useEffect(() => {
+    const INTERVAL = 5000;
+    if (shareData.length > 1) {
+      copy(shareData[0]);
+      const parentNode = document.querySelector(`#${shareData[1]}`);
+      const alertElement = document.createElement('span');
+      alertElement.className = 'share-alert';
+      alertElement.innerHTML = 'Link copiado!';
+      parentNode.after(alertElement);
+      setInterval(() => alertElement.remove(), INTERVAL);
+    }
+    setShareAlert(false);
+    setShareData([]);
+  }, [shareAlert]);
+
+  const handleFilterClick = ({ target: { className } }) => {
+    setFilterOption(className.split(' ', 2)[1]);
+    setApplyFilter(true);
+  };
+
+  const handleClickShare = ({ target: { id } }) => {
+    const formatId = (id.replace('-', '/')).split(' ', 2);
+    const recipeDetailURL = `http://localhost:3000/${formatId[0]}`;
+    const parentNodeId = `done-recipe-date-${formatId[1]}`;
+    setShareData([recipeDetailURL, parentNodeId]);
+    setShareAlert(true);
+  };
 
   return (
-    <>
+    <div className="done-recipes-container">
       <Header title="Receitas Favoritas" />
-
-      <Button
-        type="button"
-        id="filter-by-all-btn"
-        buttonText="All"
-        // onClick={ handleFilterClick }
-      />
-
-      <Button
-        type="button"
-        id="filter-by-food-btn"
-        buttonText="Foods"
-        // onClick={ handleFilterClick }
-      />
-
-      <Button
-        type="button"
-        id="filter-by-drink-btn"
-        buttonText="Foods"
-        // onClick={ handleFilterClick }
-      />
-
-    </>
+      <div className="filter-recipes-container">
+        <Button
+          type="button"
+          id="filter-by-all-btn"
+          className="recipes-categories all"
+          buttonText="All"
+          onClick={ handleFilterClick }
+          isDisable={ false }
+        />
+        <Button
+          type="button"
+          id="filter-by-food-btn"
+          className="recipes-categories comida"
+          buttonText="Food"
+          onClick={ handleFilterClick }
+          isDisable={ false }
+        />
+        <Button
+          type="button"
+          id="filter-by-drink-btn"
+          className="recipes-categories bebida"
+          buttonText="Drinks"
+          onClick={ handleFilterClick }
+          isDisable={ false }
+        />
+      </div>
+      <div className="fav-cards-container">
+        { favRecipes.map((favRecipe, index) => (
+          <FavCard
+            key={ favRecipe.id }
+            favRecipe={ favRecipe }
+            index={ index }
+            handleClickShare={ handleClickShare }
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
-export default FavouriteRecipes;
+export default DoneRecipes;
