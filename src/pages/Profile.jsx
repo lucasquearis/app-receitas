6c69cf9182c2c7ecd078046794255264d739b4db
Kevin Oliveira <kevin.zero@hotmail.com>
2021-08-27 19:09:45 -0300

    [GRUPO 27][KEVIN OLIVEIRA][REQUISITOS 82 A 87] (#573)
    * Finaliza requisitos 82 a 87 - Tela de perfil completa
    
    * Impede usuario de acessar pagina de perfil se nao tiver cadastrado

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import { Header, Footer } from '../components';
 
 function Profile() {
+  const [redirect, setRedirect] = useState(false);
+  const { email } = JSON.parse(localStorage.getItem('user')) || '';
+
+  const logout = () => {
+    localStorage.clear();
+    setRedirect(true);
+  };
+
+  if (redirect || !email) {
+    return <Redirect to="/" />;
+  }
+
   return (
-    <div>
+    <main>
       <Header title="Perfil" />
+      <p data-testid="profile-email">{email}</p>
+      <div>
+        <Link to="/receitas-feitas">
+          <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+        </Link>
+        <Link to="/receitas-favoritas">
+          <button type="button" data-testid="profile-favorite-btn">
+            Receitas Favoritas
+          </button>
+        </Link>
+        <button type="button" data-testid="profile-logout-btn" onClick={ logout }>
+          Sair
+        </button>
+      </div>
       <Footer />
-    </div>
+    </main>
   );
 }
 
