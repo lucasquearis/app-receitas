44cea2871eb5af6f535879b66fb857aabb1e192f
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-08-27 18:54:13 -0300

    Cria componente que renderiza a lista de ingredientes

---
+++ src/Components/IngredientsAndMeasures.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { objectOf, string } from 'prop-types';
+import createIngredientsAndMesure from '../helper/redoRecipe';
+
+function IngredientsAndMeasures({ recipe }) {
+  const [ingredients, setIngredients] = useState([]);
+  const [measures, setMeasures] = useState([]);
+
+  useEffect(() => {
+    setIngredients(createIngredientsAndMesure(recipe, 'ingredients'));
+    setMeasures(createIngredientsAndMesure(recipe, 'mesure'));
+  }, [recipe]);
+
+  const createRecipe = (item, key) => (
+    <li key={ key }>{`${item} - ${measures[key]}`}</li>
+  );
+
+  return (
+    <section>
+      <h1>Ingredients</h1>
+      <div>
+        <ul>
+          {ingredients.map((item, key) => createRecipe(item, key))}
+        </ul>
+      </div>
+    </section>
+  );
+}
+IngredientsAndMeasures.propTypes = {
+  recipe: objectOf(string).isRequired,
+};
+
+export default IngredientsAndMeasures;
