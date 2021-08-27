1287928cbe09ba7554c7953eaf5cfc3c5111a2ac
Gabriel Markyan <74433041+markyangit@users.noreply.github.com>
2021-08-27 10:57:49 -0300

    [GRUPO 17][Detalhes da Receita]  (#435)
    * feature - configurando os data-testid's para as paginas de detalhes
    
    * feature - realizando as requisiçoes para as API's de comidas/bebidas e mais algumas coisas do proximo requisito
    
    * requisito 35 incompleto - falta embed do video e ingredients list
    
    * requisito 35 completo - detalhes preenchidos

--- src/pages/DrinkDetails.jsx
+++ src/pages/DrinkDetails.jsx
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default function DrinkDetails() {
+export default function DrinkDetails(props) {
+  const [drinkDetails, setDrinkDetails] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const { match: { params: { id } } } = props;
+    const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+    const fetchDetails = async () => {
+      const { drinks } = await fetch(endpoint).then((data) => data.json());
+      setDrinkDetails(drinks);
+      setIsLoading(false);
+    };
+    fetchDetails();
+  }, []);
+
+  function renderDetails() {
+    const details = drinkDetails[0];
+    const allKeys = (Object.keys(details));
+    const ingredientKeys = allKeys.filter((key) => key.includes('strIngredient'));
+    const validIngredientKeys = ingredientKeys.filter(
+      (key) => !(details[key] === '' || !details[key]),
+    );
+    const measuresKeys = allKeys.filter((key) => key.includes('strMeasure'));
+    const validMeasuresKeys = measuresKeys.filter(
+      (key) => !(details[key] === '' || !details[key]),
+    );
+    return (
+      <main>
+        <img
+          data-testid="recipe-photo"
+          alt="imagem da receita"
+          src={ details.strDrinkThumb }
+        />
+        <h1 data-testid="recipe-title">{ details.strDrink }</h1>
+        <button type="button" data-testid="share-btn">SHARE</button>
+        <button type="button" data-testid="favorite-btn">FAVORITAR</button>
+        <p data-testid="recipe-category">{ details.strAlcoholic }</p>
+        {
+          validIngredientKeys.map((key, index) => (
+            <p
+              data-testid={ `${index}-ingredient-name-and-measure` }
+              key={ key }
+            >
+              { details[key] }
+              :
+              {
+                !details[validMeasuresKeys[index]]
+                  ? 'à gosto' : details[validMeasuresKeys[index]]
+              }
+            </p>
+          ))
+        }
+        <p data-testid="instructions">{ details.strInstructions }</p>
+        <div data-testid={ `${0}-recomendation-card` }>CARD</div>
+        <button type="button" data-testid="start-recipe-btn">INICIAR RECEITA</button>
+      </main>
+    );
+  }
   return (
-    <span>DrinkDetails</span>
+    isLoading ? <p>Loading...</p> : renderDetails()
   );
 }
+
+DrinkDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
--- src/pages/MealDetails.jsx
+++ src/pages/MealDetails.jsx
@@ -1,7 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default function MealDetails() {
+export default function MealDetails(props) {
+  const [mealDetails, setMealDetails] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const { match: { params: { id } } } = props;
+    const endpoint = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    const fetchDetails = async () => {
+      const { meals } = await fetch(endpoint).then((data) => data.json());
+      setMealDetails(meals);
+      setIsLoading(false);
+    };
+    fetchDetails();
+  }, []);
+
+  // source: https://stackoverflow.com/a/21607897, tks!
+  function getYouTubeId(url) {
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+    const match = url.match(regExp);
+    const ELEVEN = 11;
+    return (match && match[2].length === ELEVEN)
+      ? match[2]
+      : null;
+  }
+
+  function renderDetails() {
+    const details = mealDetails[0];
+    const allKeys = (Object.keys(details));
+    const ingredientKeys = allKeys.filter((key) => key.includes('strIngredient'));
+    const validIngredientKeys = ingredientKeys.filter(
+      (key) => !(details[key] === '' || !details[key]),
+    );
+    const measuresKeys = allKeys.filter((key) => key.includes('strMeasure'));
+    const validMeasuresKeys = measuresKeys.filter(
+      (key) => !(details[key] === '' || !details[key]),
+    );
+    return (
+      <main>
+        <img
+          data-testid="recipe-photo"
+          alt="imagem da receita"
+          src={ details.strMealThumb }
+        />
+        <h1 data-testid="recipe-title">{ details.strMeal }</h1>
+        <button type="button" data-testid="share-btn">SHARE</button>
+        <button type="button" data-testid="favorite-btn">FAVORITAR</button>
+        <p data-testid="recipe-category">{ details.strCategory }</p>
+        {
+          validIngredientKeys.map((key, index) => (
+            <p
+              data-testid={ `${index}-ingredient-name-and-measure` }
+              key={ key }
+            >
+              { details[key] }
+              :
+              { details[validMeasuresKeys[index]] }
+            </p>
+          ))
+        }
+        <p data-testid="instructions">{ details.strInstructions }</p>
+        <div data-testid="video">
+          <iframe
+            title="recipe title"
+            width="420"
+            height="315"
+            src={ `https://youtube.com/embed/${getYouTubeId(details.strYoutube)}` }
+          />
+        </div>
+        <div data-testid={ `${0}-recomendation-card` }>CARD</div>
+        <button type="button" data-testid="start-recipe-btn">INICIAR RECEITA</button>
+      </main>
+    );
+  }
+
   return (
-    <span>MealDetails</span>
+    isLoading ? <p>Loading...</p> : renderDetails()
   );
 }
+
+MealDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
