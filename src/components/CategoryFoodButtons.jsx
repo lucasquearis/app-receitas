3ee5d70bd154a73a004bf33b683a04897b27fbdc
Gabriel Takaki <gabitj@icloud.com>
2021-08-27 18:10:10 -0300

    pull

--- src/components/CategoryFoodButtons.jsx
+++ src/components/CategoryFoodButtons.jsx
@@ -1,14 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCategory } from '../redux/actions/mainActions';
+import ItemCard from './ItemCard';
 
 function CategoryFoodButtons() {
   const cinco = 5;
+  const doze = 12;
   const categories = useSelector((state) => state.reducerCategories.categories.meals);
   const dispatch = useDispatch();
+  const [categoryClick, setCategoryClick] = useState([]);
+
+  const renderCategoryFilter = async (category) => {
+    try {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
+      const data = await response.json();
+      setCategoryClick(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     dispatch(fetchCategory());
+    renderCategoryFilter();
   }, [dispatch]);
 
   return (
@@ -19,9 +33,22 @@ function CategoryFoodButtons() {
             type="button"
             key={ `${category.strCategory}-category-filter` }
             data-testid={ `${category.strCategory}-category-filter` }
+            onClick={ () => renderCategoryFilter(category.strCategory) }
           >
             {category.strCategory}
           </button>
+        ))
+      }
+      {
+        categoryClick.meals && categoryClick.meals.map((dish, index) => index < doze && (
+          <ItemCard
+            title={ dish.strMeal }
+            // data-testid={ `${index}-recipe-card` }
+            thumb={ dish.strMealThumb }
+            id={ dish.idMeal }
+            index={ index }
+            key={ index }
+          />
         ))
       }
     </div>
