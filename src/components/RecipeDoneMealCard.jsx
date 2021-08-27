e855ce608004560562af24a02d96a281e33ebc7b
Samuel Constantino <samuelicapuidados@gmail.com>
2021-08-27 14:58:48 -0300

    implementa requisitos 54 e 55: adiciona cartoes para comida e bebida

---
+++ src/components/RecipeDoneMealCard.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+
+export default function RecipeDoneCard({
+  id,
+  area,
+  category,
+  name,
+  image,
+  doneDate,
+  tagsName,
+  index,
+}) {
+  return (
+    <Link
+      to={ `/comidas/${id}` }
+      className="mealDoneCard"
+      data-testid={ `${id}-recipe-card` }
+    >
+      <img
+        width="300px"
+        src={ image }
+        alt={ name }
+        data-testid={ `${index}-horizontal-image` }
+      />
+
+      <h2 data-testid={ `${index}-horizontal-name` }>
+        {name}
+      </h2>
+
+      <p data-testid={ `${index}-horizontal-top-text` }>
+        { `${area} - ${category}` }
+      </p>
+
+      <p data-testid={ `${index}-horizontal-done-date` }>
+        {doneDate}
+      </p>
+
+      { tagsName.map((tag) => (
+        <p
+          key={ tag }
+          data-testid={ `${index}-${tag}-horizontal-tag` }
+        >
+          {tag}
+
+        </p>)) }
+
+      <img
+        data-testid={ `${index}-horizontal-share-btn` }
+        src={ shareIcon }
+        alt="share-icon"
+      />
+
+    </Link>
+  );
+}
+
+RecipeDoneCard.defaultProps = {
+  tagsName: null,
+};
+
+RecipeDoneCard.propTypes = {
+  id: PropTypes.number.isRequired,
+  area: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  doneDate: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  tagsName: PropTypes.arrayOf(
+    PropTypes.string,
+  ),
+};
--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -1,35 +1,11 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Header from '../components/Header';
-import RecipeDoneCard from '../components/RecipeDoneCard';
+import RecipeDoneMealCard from '../components/RecipeDoneMealCard';
+import RecipeDoneDrinkCard from '../components/RecipeDoneDrinkCard';
 import recipesDoneMock from '../data/recipesDoneMock';
 
 function RecipesDone() {
-//   const doneRecipesMock = [
-//     {
-//       id: '52771',
-//       type: 'comida',
-//       area: 'Italian',
-//       category: 'Vegetarian',
-//       alcoholicOrNot: '',
-//       name: 'Spicy Arrabiata Penne',
-//       image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-//       doneDate: '23/06/2020',
-//       tags: ['Pasta', 'Curry'],
-//     },
-//     {
-//       id: '178319',
-//       type: 'bebida',
-//       area: '',
-//       category: 'Cocktail',
-//       alcoholicOrNot: 'Alcoholic',
-//       name: 'Aquamarine',
-//       image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-//       doneDate: '23/06/2020',
-//       tags: [],
-//     },
-//   ];
-
   return (
     <>
       <Header title="Refeitas Feitas" />
@@ -65,23 +41,45 @@ function RecipesDone() {
         <div>
           {
             recipesDoneMock.map(({
+              id,
+              type,
+              area,
+              category,
+              alcoholicOrNot,
               name,
               image,
-              category,
-              id,
+              doneDate,
               tags },
-            index) => (
-              <RecipeDoneCard
-                key={ +id }
-                link=""
-                id={ +id }
-                img={ image }
-                name={ name }
-                date="26092021"
-                category={ category }
-                index={ +index }
-                tagsName={ tags }
-              />))
+            index) => {
+              if (type === 'comida') { // foto, nome, categoria, area, data, tags, botao share
+                return (
+                  <RecipeDoneMealCard
+                    key={ +id }
+                    id={ +id }
+                    area={ area }
+                    category={ category }
+                    name={ name }
+                    image={ image }
+                    doneDate={ doneDate }
+                    tagsName={ tags }
+                    index={ +index }
+                  />
+                );
+              }
+
+              return (
+                <RecipeDoneDrinkCard
+                  key={ +id }
+                  id={ +id }
+                  alcoholicOrNot={ alcoholicOrNot }
+                  name={ name }
+                  image={ image }
+                  doneDate={ doneDate }
+                  tagsName={ tags }
+                  index={ +index }
+                />
+              );
+            })
           }
         </div>
       </main>
