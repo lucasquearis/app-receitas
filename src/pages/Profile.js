9bebae5059875c12f8dc9e372f9e5b26b24c6c4c
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-08-27 17:26:33 -0300

    pega o email do localStorage

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const email = localStorage.getItem('email');
   return (
     <div>
       <p>Profile</p>
-      <span data-testid="profile-email">Email</span>
+      <span data-testid="profile-email">{email}</span>
       <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
       <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
       <button type="button" data-testid="profile-logout-btn">Sair</button>
