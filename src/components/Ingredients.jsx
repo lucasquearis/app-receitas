dde0b152e490000cb7b39ee55c26c98b73331d86
Júlio Mota <juliomotacampos@gmail.com>
2021-08-27 19:32:31 -0300

    Adiciona funcao de ingredients

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,31 +1,63 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import fetchRecipe from '../Redux/actions/fetchRecipes';
+import PropTypes from 'prop-types';
+import fetchRecipes from '../Redux/actions/fetchRecipes';
 
 class Ingredients extends Component {
   constructor(props) {
     super(props);
 
     this.filterIngredients = this.filterIngredients.bind(this);
+    this.getIngredients = this.getIngredients.bind(this);
+    this.getMeasurements = this.getMeasurements.bind(this);
+  }
+
+  getIngredients() {
+    const { recipe } = this.props;
+    const object = recipe[0];
+    const keys = Object.keys(object);
+    const values = Object.values(object);
+
+    const ingredients = keys.filter((item, index) => (
+      item.includes('strIngredient') && values[index] !== ''
+    ));
+
+    return ingredients;
+  }
+
+  getMeasurements() {
+    const { recipe } = this.props;
+    const object = recipe[0];
+    const keys = Object.keys(object);
+    const values = Object.values(object);
+
+    const measurements = keys.filter((item, index) => (
+      item.includes('strMeasure') && values[index] !== ''
+    ));
+
+    return measurements;
   }
 
   filterIngredients() {
     const { recipe } = this.props;
-    const max = 15
-    const ingredient = recipe.filter(({ strIngredient })=> {
-      return Object.keys(recipe)
-    })
-    console.log(ingredient)
-  //   return (
-  //     ingredientList.map(({ strIngredients }, index) => (
-  //       <li
-  //         key={ index }
-  //         data-testid={ `${index}-ingredient-name-and-measure` }
-  //       >
-  //         { `${strIngredients}: ${measureList[index]}` }
-  //       </li>
-  //     ))
-  //   );
+    const object = recipe[0];
+
+    const ingredients = this.getIngredients();
+
+    return (
+      <div>
+        {
+          ingredients.map((ingredient, index) => (
+            <li
+              key={ index }
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              { `${object[ingredient]}` }
+            </li>
+          ))
+        }
+      </div>
+    );
   }
 
   render() {
@@ -42,8 +74,12 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispach) => ({
-  fetchRecipe: (id) => dispach(fetchRecipe(id)),
+  setRecipes: (id) => dispach(fetchRecipes(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Ingredients);
 
+Ingredients.propTypes = {
+  recipe: PropTypes.objectOf(PropTypes.object),
+  setRecipe: PropTypes.func,
+}.isRequired;
