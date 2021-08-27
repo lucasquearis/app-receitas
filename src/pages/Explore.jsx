cea728a88e9967214035119fdcdc8406028f8cf2
Caroline Benichio Teixeira <78769105+carolbenichio@users.noreply.github.com>
2021-08-27 17:28:34 -0300

    Tela de Explorar (#545)
    * Requisitos 33 - 38: Lista de recomendados
    
    * Requisitos 67, 68, 69 - Tela de Explorar
    
    * Conflitos no Lock resolvidos I

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,6 +8,16 @@ function Explore() {
   return (
     <div>
       <Header name="Explorar" />
+      <Link to="/explorar/comidas">
+        <Button data-testid="explore-food">
+          Explorar Comidas
+        </Button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <Button data-testid="explore-drinks">
+          Explorar Bebidas
+        </Button>
+      </Link>
       <Footer />
     </div>
   );
