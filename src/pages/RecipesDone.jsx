37f967056d46342b388864820a215dedb95f0afa
Samuel Constantino <samuelicapuidados@gmail.com>
2021-08-27 16:29:29 -0300

    refatora página RecipesDone

--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -16,76 +16,85 @@ function RecipesDone() {
     if (filterDrinks) return recipesDoneMock.filter(({ type }) => type === 'bebida');
     return recipesDoneMock;
   };
+
+  const renderFilterButtons = () => (
+    <section>
+      <Button
+        variant="primary"
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ () => setfilterRecipesDone({
+          filterMeals: true,
+          filterDrinks: false,
+        }) }
+      >
+        Food
+      </Button>
+
+      <Button
+        variant="primary"
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ () => setfilterRecipesDone({
+          filterMeals: false,
+          filterDrinks: true,
+        }) }
+      >
+        Drinks
+      </Button>
+
+      <Button
+        variant="primary"
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ () => setfilterRecipesDone({
+          filterMeals: false,
+          filterDrinks: false,
+        }) }
+      >
+        All
+      </Button>
+    </section>
+  );
+
+  const renderFilterRecipesDone = () => (
+    <section>
+      {
+        recipesDone().map(({
+          id,
+          type,
+          area,
+          category,
+          alcoholicOrNot,
+          name,
+          image,
+          doneDate,
+          tags },
+        index) => (
+          <RecipeDoneCard
+            key={ +id }
+            id={ +id }
+            type={ type }
+            area={ area }
+            category={ category }
+            alcoholicOrNot={ alcoholicOrNot }
+            name={ name }
+            image={ image }
+            doneDate={ doneDate }
+            tagsName={ tags }
+            index={ +index }
+          />
+        ))
+      }
+    </section>
+  );
+
   return (
     <>
       <Header title="Refeitas Feitas" />
       <main>
-        <div>
-          <Button
-            variant="primary"
-            type="button"
-            data-testid="filter-by-food-btn"
-            onClick={ () => setfilterRecipesDone({
-              filterMeals: true,
-              filterDrinks: false,
-            }) }
-          >
-            Food
-          </Button>
-
-          <Button
-            variant="primary"
-            type="button"
-            data-testid="filter-by-drink-btn"
-            onClick={ () => setfilterRecipesDone({
-              filterMeals: false,
-              filterDrinks: true,
-            }) }
-          >
-            Drinks
-          </Button>
-
-          <Button
-            variant="primary"
-            type="button"
-            data-testid="filter-by-all-btn"
-            onClick={ () => setfilterRecipesDone({
-              filterMeals: false,
-              filterDrinks: false,
-            }) }
-          >
-            All
-          </Button>
-        </div>
-        <div>
-          {
-            recipesDone().map(({
-              id,
-              type,
-              area,
-              category,
-              alcoholicOrNot,
-              name,
-              image,
-              doneDate,
-              tags },
-            index) => (
-              <RecipeDoneCard
-                key={ +id }
-                id={ +id }
-                type={ type }
-                area={ area }
-                category={ category }
-                alcoholicOrNot={ alcoholicOrNot }
-                name={ name }
-                image={ image }
-                doneDate={ doneDate }
-                tagsName={ tags }
-                index={ +index }
-              />
-            ))
-          }
-        </div>
+        { renderFilterButtons() }
+        { renderFilterRecipesDone() }
       </main>
     </>
   );
