86a48cdbc606f12e849d025abda5267d09583e5a
Jossany Moura <mjossany@gmail.com>
2021-08-27 15:18:58 -0300

    Requisito 32 - Implementa lógica do requisito 32 para redirecionar ao clicar no card de um item

--- src/components/CardsList/CardsList.js
+++ src/components/CardsList/CardsList.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 
 const CardsList = ({ array }) => {
+  const [redirect, setRedirect] = useState(false);
+  const [itemId, setItemId] = useState();
+  const [url, setUrl] = useState('');
   let type;
   if (array !== undefined) {
     if (array[0].idDrink === undefined) type = 'Meal';
@@ -15,21 +19,43 @@ const CardsList = ({ array }) => {
       newArray = array;
     }
 
+    const handleClick = (id, foodDrink) => {
+      setRedirect(true);
+      setItemId(id);
+      if (foodDrink === 'idDrink') {
+        setUrl('bebidas');
+      } else {
+        setUrl('comidas');
+      }
+    };
+
+    if (redirect) {
+      return <Redirect to={ `/${url}/${itemId}` } />;
+    }
+
     return (
       <div>
-        { newArray.map((obj, index) => (
-          <div
-            key={ index }
-            data-testid={ `${index}-recipe-card` }
-          >
-            <img
-              data-testid={ `${index}-card-img` }
-              src={ obj[`str${type}Thumb`] }
-              alt={ obj[`str${type}`] }
-            />
-            <h3 data-testid={ `${index}-card-name` }>{ obj[`str${type}`] }</h3>
-          </div>
-        )) }
+        { newArray.map((obj, index) => {
+          const objKeys = Object.keys(obj);
+          console.log(objKeys[0]);
+          return (
+            <div
+              role="button"
+              tabIndex={ 0 }
+              key={ index }
+              data-testid={ `${index}-recipe-card` }
+              onClick={ () => handleClick(obj[objKeys[0]], objKeys[0]) }
+              onKeyDown={ () => handleClick() }
+            >
+              <img
+                data-testid={ `${index}-card-img` }
+                src={ obj[`str${type}Thumb`] }
+                alt={ obj[`str${type}`] }
+              />
+              <h3 data-testid={ `${index}-card-name` }>{ obj[`str${type}`] }</h3>
+            </div>
+          );
+        }) }
       </div>
     );
   }
