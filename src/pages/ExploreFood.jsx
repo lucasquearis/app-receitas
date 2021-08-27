d58ec63a8a29b92c0035385225edd87f91ef46d7
Merlone <alestori7@gmail.com>
2021-08-27 17:56:38 -0300

    implementação parcial de rotas

--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
 import HeaderWithoutSearch from '../components/HeaderWithoutSearch';
+import useRedirect from '../hooks/useRedirect';
 
-function ExploreFood() {
+export default function ExploreFood() {
+  const { shouldRedirect, redirect } = useRedirect();
+
+  if (redirect.should) return <Redirect to={ redirect.path } />;
+
   return (
     <div>
       <HeaderWithoutSearch>Explorar Comidas</HeaderWithoutSearch>
       <Button
         variant="success"
         data-testid="explore-by-ingredient"
+        onClick={ () => shouldRedirect('/explorar/comidas/ingredientes') }
       >
         Por Ingredientes
       </Button>
       <Button
         variant="primary"
         data-testid="explore-by-area"
+        onClick={ () => shouldRedirect('/explorar/comidas/area') }
       >
         Por Local de Origem
       </Button>
       <Button
         variant="warning"
-        data-testid="explore-by-surprise"
+        data-testid="explore-surprise"
+        onClick={ () => shouldRedirect('/explorar/comidas/{random}') }
       >
         Me Surpreenda!
       </Button>
@@ -29,5 +38,3 @@ function ExploreFood() {
     </div>
   );
 }
-
-export default ExploreFood;
--- src/tests/explore-food.test.js
+++ src/tests/explore-food.test.js
@@ -8,7 +8,7 @@ import {
   exploreButtonId,
   exploreByArea,
   exploreByIngredient,
-  exploreBySurprise,
+  exploreSurprise,
   exploreFoodId,
   VALID_EMAIL,
   VALID_PASSWORD,
@@ -17,53 +17,38 @@ import renderWithRouter from './helpers/renderWithRouter';
 
 let currentHistory;
 
-beforeAll(() => {
+beforeEach(() => {
   const { history } = renderWithRouter(<App />);
   loginSteps(VALID_EMAIL, VALID_PASSWORD);
   const loginButton = screen.getByTestId(buttonId);
   userEvent.click(loginButton);
-  userEvent.click(exploreButtonId);
+  const exploreButton = screen.getByTestId(exploreButtonId);
+  userEvent.click(exploreButton);
+  userEvent.click(screen.getByTestId(exploreFoodId));
   currentHistory = history;
 });
 
 describe('Tests rendering of explore food page', () => {
-  beforeAll(() => {
-    userEvent.click(screen.getByTestId(exploreFoodId));
-  });
-
   it('Should render an element with text testid "explore-by-ingredient"', () => {
     const exploreByIngredientBtn = screen.getByTestId(exploreByIngredient);
     expect(exploreByIngredientBtn).toBeInTheDocument();
-
-    it('Element should have text "Por Ingredientes"', () => {
-      expect(exploreByIngredientBtn).toHaveTextContent('Por Ingredientes');
-    });
+    expect(exploreByIngredientBtn).toHaveTextContent('Por Ingredientes');
   });
 
   it('Should render an element with testid "explore-by-area"', () => {
     const exploreByAreaBtn = screen.getByTestId(exploreByArea);
     expect(exploreByAreaBtn).toBeInTheDocument();
-
-    it('Element should have text "Por Local de Origem"', () => {
-      expect(exploreByAreaBtn).toHaveTextContent('Por Local de Origem');
-    });
+    expect(exploreByAreaBtn).toHaveTextContent('Por Local de Origem');
   });
 
-  it('Should render an element with testid "explore-by-surprise"', () => {
-    const exploreBySurpriseBtn = screen.getByTestId(exploreBySurprise);
-    expect(exploreBySurpriseBtn).toBeInTheDocument();
-
-    it('Element should have text "Me Surpreenda!"', () => {
-      expect(exploreBySurpriseBtn).toHaveTextContent('Me Surpreenda!');
-    });
+  it('Should render an element with testid "explore-surprise"', () => {
+    const exploreSurpriseBtn = screen.getByTestId(exploreSurprise);
+    expect(exploreSurpriseBtn).toBeInTheDocument();
+    expect(exploreSurpriseBtn).toHaveTextContent('Me Surpreenda!');
   });
 });
 
 describe('Tests routing of buttons on explore food page', () => {
-  beforeAll(() => {
-    userEvent.click(screen.getByTestId(exploreFoodId));
-  });
-
   it('"Por Ingrediente" should redirect to "/explorar/comidas/ingredientes"', () => {
     userEvent.click(screen.getByTestId(exploreByIngredient));
     expect(currentHistory.location.pathname).toEqual('/explorar/comidas/ingredientes');
