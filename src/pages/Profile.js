5d3e0603197ece0c4919deabdfa7898b5bbf3074
Gustavo-Mourao <gcmbh1@gmail.com>
2021-08-27 17:02:57 -0300

    Adiciona REQ 82-87

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,53 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button } from '../components';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const Profile = () => (
-  <Header title="Perfil" />
-);
+function Profile() {
+  const email = JSON.parse(localStorage.getItem('user'));
+  const receiveEmail = email.email;
+  const history = useHistory();
 
+  const doneRecipes = () => {
+    history.push('/receitas-feitas');
+  };
+
+  // npm run cy -- --spec cypress/integration/
+
+  const favoriteRecipes = () => {
+    history.push('/receitas-favoritas');
+  };
+
+  const loginPage = () => {
+    history.push('/');
+    localStorage.clear();
+  };
+
+  return (
+    <div>
+      <Header title="Perfil" />
+      <h1 data-testid="profile-email">{ receiveEmail }</h1>
+      <Button
+        className="profile-done-btn"
+        type="submit"
+        buttonText="Receitas Feitas"
+        onClick={ doneRecipes }
+      />
+      <Button
+        className="profile-favorite-btn"
+        type="submit"
+        buttonText="Receitas Favoritas"
+        onClick={ favoriteRecipes }
+      />
+      <Button
+        className="profile-logout-btn"
+        type="submit"
+        buttonText="Sair"
+        onClick={ loginPage }
+      />
+      <Footer />
+    </div>
+  );
+}
 export default Profile;
