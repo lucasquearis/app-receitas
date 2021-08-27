d97a0e000dd9bc547dc5d8c8ba5531185526c432
rachidb5 <66842598+rachidb5@users.noreply.github.com>
2021-08-27 08:56:54 -0300

    Adiciona Pagina de explorar (#495)

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,6 +7,20 @@ function Explorar() {
   return (
     <div>
       <Header title="Explorar" renderSearch={ false } />
+      <Link to="/explorar/comidas">
+        <div className="card">
+          <div className="card-body" data-testid="explore-food">
+            Explorar Comidas
+          </div>
+        </div>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <div className="card">
+          <div className="card-body" data-testid="explore-drinks">
+            Explorar Bebidas
+          </div>
+        </div>
+      </Link>
       <Footer />
     </div>
   );
