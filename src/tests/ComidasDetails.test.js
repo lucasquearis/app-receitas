1f67e1dc9bc022276ef6fe2f1d14d3a3ea1fa74f
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-27 20:12:12 -0300

    lint

--- src/tests/ComidasDetails.test.js
+++ src/tests/ComidasDetails.test.js
@@ -8,6 +8,12 @@ import fetchMock from '../../cypress/mocks/fetch';
 jest.mock('clipboard-copy', () => jest.fn());
 const copy = require('clipboard-copy');
 
+const favoriteButton = 'favorite-btn';
+const startRecipeButton = 'start-recipe-btn';
+const foodName = 'Spicy Arrabiata Penne';
+const foodImage = 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg';
+const whiteHeart = 'whiteHeartIcon.svg';
+
 describe('Testes para a pagina de detalhes de comidas', () => {
   beforeEach(() => {
     jest.spyOn(global, 'fetch');
@@ -21,13 +27,13 @@ describe('Testes para a pagina de detalhes de comidas', () => {
     await screen.findByTestId('recipe-photo');
     await screen.findByTestId('recipe-title');
     await screen.findByTestId('share-btn');
-    await screen.findByTestId('favorite-btn');
+    await screen.findByTestId(favoriteButton);
     await screen.findByTestId('recipe-category');
     await screen.findByTestId('0-ingredient-name-and-measure');
     await screen.findByTestId('instructions');
     await screen.findByTestId('video');
     await screen.findByTestId('0-recomendation-card');
-    await screen.findByTestId('start-recipe-btn');
+    await screen.findByTestId(startRecipeButton);
   });
   it('Verifica se foram feitas duas requisicoes a API', async () => {
     act(() => {
@@ -40,21 +46,22 @@ describe('Testes para a pagina de detalhes de comidas', () => {
     });
   });
 
-  it('Verifica se o botao de iniciar receita desaparece caso ja tenha sido feita', async () => {
+  it('Verifica se o botao de iniciar desaparece caso ja tenha sido feita', async () => {
     const doneRecipes = [{
-      "id": "52771",
-      "type": "comida",
-      "area": "Italian",
-      "category": "Vegetarian",
-      "alcoholicOrNot": "",
-      "name": "Spicy Arrabiata Penne",
-      "image": "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-      "doneDate": "22/6/2020",
-      "tags": ["Pasta", "Curry"]
+      id: '52771',
+      type: 'comida',
+      area: 'Italian',
+      category: 'Vegetarian',
+      alcoholicOrNot: '',
+      name: foodName,
+      image: foodImage,
+      doneDate: '22/6/2020',
+      tags: ['Pasta', 'Curry'],
     }];
     localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
     renderWithRouter(<ComidasDetails match={ { params: { id: '52771' } } } />);
-    waitForElementToBeRemoved(null).catch(() => expect(screen.queryByTestId('start-recipe-btn')).toBeNull());
+    waitForElementToBeRemoved(null)
+      .catch(() => expect(screen.queryByTestId(startRecipeButton)).toBeNull());
   });
 
   it('Verifica se o texto muda caso a receita ja tenha sido iniciada', async () => {
@@ -66,13 +73,15 @@ describe('Testes para a pagina de detalhes de comidas', () => {
     };
     localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipes));
     renderWithRouter(<ComidasDetails match={ { params: { id: '52771' } } } />);
-    const continueRecipeButton = await screen.findByTestId('start-recipe-btn');
+    const continueRecipeButton = await screen.findByTestId(startRecipeButton);
     expect(continueRecipeButton).toHaveTextContent('Continuar Receita');
   });
 
-  it('Verifica se o link para comecar receita redireciona para a pagina correta', async () => {
-    const { history } = renderWithRouter(<ComidasDetails match={ { params: { id: '52771' } } } />);
-    const continueRecipeButton = await screen.findByTestId('start-recipe-btn');
+  it('Verifica se o link para comecar redireciona para a pagina correta', async () => {
+    const { history } = renderWithRouter(
+      <ComidasDetails match={ { params: { id: '52771' } } } />,
+    );
+    const continueRecipeButton = await screen.findByTestId(startRecipeButton);
 
     userEvent.click(continueRecipeButton);
 
@@ -93,40 +102,40 @@ describe('Testes para a pagina de detalhes de comidas', () => {
 
   it('Verifica se o coracao do botao de favoritar vem preenchido', async () => {
     const favoriteRecipes = [{
-      "id": "52771",
-      "type": "comida",
-      "area": "Italian",
-      "category": "Vegetarian",
-      "alcoholicOrNot": "",
-      "name": "Spicy Arrabiata Penne",
-      "image": "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+      id: '52771',
+      type: 'comida',
+      area: 'Italian',
+      category: 'Vegetarian',
+      alcoholicOrNot: '',
+      name: foodName,
+      image: foodImage,
     }];
     localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
     renderWithRouter(<ComidasDetails match={ { params: { id: '52771' } } } />);
-    const favoriteImage = await screen.findByTestId('favorite-btn');
+    const favoriteImage = await screen.findByTestId(favoriteButton);
 
     expect(favoriteImage).toHaveAttribute('src', 'blackHeartIcon.svg');
   });
 
   it('Verifica se o coracao do botao de favoritar vem vazio', async () => {
     renderWithRouter(<ComidasDetails match={ { params: { id: '52771' } } } />);
-    const favoriteImage = await screen.findByTestId('favorite-btn');
+    const favoriteImage = await screen.findByTestId(favoriteButton);
 
-    expect(favoriteImage).toHaveAttribute('src', 'whiteHeartIcon.svg');
+    expect(favoriteImage).toHaveAttribute('src', whiteHeart);
   });
 
   it('Verifica que se clicar no botao favorita ou desfavorita a receita', async () => {
     renderWithRouter(<ComidasDetails match={ { params: { id: '52771' } } } />);
-    const favoriteImage = await screen.findByTestId('favorite-btn');
+    const favoriteImage = await screen.findByTestId(favoriteButton);
 
-    expect(favoriteImage).toHaveAttribute('src', 'whiteHeartIcon.svg');
+    expect(favoriteImage).toHaveAttribute('src', whiteHeart);
     userEvent.click(favoriteImage);
     expect(favoriteImage).toHaveAttribute('src', 'blackHeartIcon.svg');
     userEvent.click(favoriteImage);
-    expect(favoriteImage).toHaveAttribute('src', 'whiteHeartIcon.svg');
+    expect(favoriteImage).toHaveAttribute('src', whiteHeart);
   });
 
-  it('Verifica se a receita eh salva corretamente no localStorage ao clicar no botao de fav', async () => {
+  it('Verifica se a receita esta no localStorage ao clicar no botao de fav', async () => {
     const expectedFavoriteRecipes = [
       {
         id: '52771',
@@ -134,12 +143,12 @@ describe('Testes para a pagina de detalhes de comidas', () => {
         area: 'Italian',
         category: 'Vegetarian',
         alcoholicOrNot: '',
-        name: 'Spicy Arrabiata Penne',
-        image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+        name: foodName,
+        image: foodImage,
       },
     ];
     renderWithRouter(<ComidasDetails match={ { params: { id: '52771' } } } />);
-    const favoriteImage = await screen.findByTestId('favorite-btn');
+    const favoriteImage = await screen.findByTestId(favoriteButton);
 
     userEvent.click(favoriteImage);
     const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
