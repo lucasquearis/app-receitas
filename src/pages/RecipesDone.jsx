117684ee4cd21a3ecdff251b26c6995f6b97cc74
Samuel Constantino <samuelicapuidados@gmail.com>
2021-08-27 09:55:37 -0300

    separa mock de receitas feitas em pasta /data/

--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -2,32 +2,33 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Header from '../components/Header';
 import RecipeDoneCard from '../components/RecipeDoneCard';
+import recipesDoneMock from '../data/recipesDoneMock';
 
 function RecipesDone() {
-  const doneRecipesMock = [
-    {
-      id: '52771',
-      type: 'comida',
-      area: 'Italian',
-      category: 'Vegetarian',
-      alcoholicOrNot: '',
-      name: 'Spicy Arrabiata Penne',
-      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-      doneDate: '23/06/2020',
-      tags: ['Pasta', 'Curry'],
-    },
-    {
-      id: '178319',
-      type: 'bebida',
-      area: '',
-      category: 'Cocktail',
-      alcoholicOrNot: 'Alcoholic',
-      name: 'Aquamarine',
-      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-      doneDate: '23/06/2020',
-      tags: [],
-    },
-  ];
+//   const doneRecipesMock = [
+//     {
+//       id: '52771',
+//       type: 'comida',
+//       area: 'Italian',
+//       category: 'Vegetarian',
+//       alcoholicOrNot: '',
+//       name: 'Spicy Arrabiata Penne',
+//       image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+//       doneDate: '23/06/2020',
+//       tags: ['Pasta', 'Curry'],
+//     },
+//     {
+//       id: '178319',
+//       type: 'bebida',
+//       area: '',
+//       category: 'Cocktail',
+//       alcoholicOrNot: 'Alcoholic',
+//       name: 'Aquamarine',
+//       image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+//       doneDate: '23/06/2020',
+//       tags: [],
+//     },
+//   ];
 
   return (
     <>
@@ -63,7 +64,7 @@ function RecipesDone() {
         </div>
         <div>
           {
-            doneRecipesMock.map(({
+            recipesDoneMock.map(({
               name,
               image,
               category,
