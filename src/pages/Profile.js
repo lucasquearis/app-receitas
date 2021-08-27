c03c83d800ce650f6335e6fd044579396591d98c
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-08-27 17:31:07 -0300

    redireciona para as paginas corretas

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 function Profile() {
@@ -7,9 +8,20 @@ function Profile() {
     <div>
       <p>Profile</p>
       <span data-testid="profile-email">{email}</span>
-      <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
-      <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
-      <button type="button" data-testid="profile-logout-btn">Sair</button>
+      <Link to="/receitas-favoritas">
+        <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/receitas-feitas">
+        <button type="button" data-testid="profile-logout-btn">Sair</button>
+      </Link>
       <Footer />
     </div>
   );
