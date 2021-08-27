e676a9e871387df6bfaa24e627c54fe0b80686fa
Rodrigo <rodrigo.facury14@gmail.com>
2021-08-26 21:00:18 -0300

    Pagina de receitas favoritas quase finalizada

--- src/pages/ReceitasFavoritas.js
+++ src/pages/ReceitasFavoritas.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import FavoriteCard from '../components/FavoriteCard';
 import Header from '../components/Header';
 
 function ReceitasFavoritas() {
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+
+  const getAllFavorite = () => {
+    setFavoriteRecipes(JSON.parse(localStorage.getItem('favoriteRecipes')));
+  };
+
+  const updateFavorite = (updatedRecipes) => {
+    setFavoriteRecipes(updatedRecipes);
+  };
+
+  const showFood = () => {
+    const allFavorite = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const onlyFood = allFavorite.filter((recipe) => recipe.type === 'comida');
+    setFavoriteRecipes(onlyFood);
+  };
+
+  const showDrinks = () => {
+    const allFavorite = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const onlyDrinks = allFavorite.filter((recipe) => recipe.type === 'bebida');
+    setFavoriteRecipes(onlyDrinks);
+  };
+
+  useEffect(() => {
+    getAllFavorite();
+  }, []);
+
   return (
-    <Header titulo="Receitas Favoritas" />
+    <div>
+      <Header titulo="Receitas Favoritas" />
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ getAllFavorite }
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ showFood }
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ showDrinks }
+      >
+        Drinks
+      </button>
+      { favoriteRecipes.map((recipe, index) => (
+        <FavoriteCard
+          key={ recipe.name }
+          recipe={ recipe }
+          updateFavorite={ updateFavorite }
+          index={ index }
+        />
+      )) }
+    </div>
   );
 }
 
