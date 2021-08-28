f2c75e30498ea53f95b7518c67d0ce4ce3ade1fa
Rodrigo Merlone <alestori7@gmail.com>
2021-08-28 12:01:34 -0300

    [Grupo 24] [Explore Food & Drinks] (#543)
    * páginas de explorar comidas e bebidas
    
    * [Grupo 24] [Explore Food & Drinks] [Tests] (#551)
    
    * testes (falta o de receita aleatória)
    
    * corrige linha 6 de explore.test.js
    
    * [Grupo 24] [Explore Food] (#552)
    
    * testes (falta o de receita aleatória)
    
    * Renderiza elementos da págian de explorar comidas
    
    * implementação parcial de rotas
    
    * corrige erro na linha 9 de explore-drinks.test.js
    
    * [Grupo 24] [Explore Drinks] (#565)
    
    * testes (falta o de receita aleatória)
    
    * Renderiza elementos da págian de explorar comidas
    
    * implementação parcial de rotas
    
    * corrige erro na linha 9 de explore-drinks.test.js
    
    * implementação de componentes da página de drinks + correção de testes
    
    * implementação parcial de rotas
    
    * [Grupo 24] [Explore Food & Drinks] [Random] (#574)
    
    * testes (falta o de receita aleatória)
    
    * Renderiza elementos da págian de explorar comidas
    
    * implementação parcial de rotas
    
    * corrige erro na linha 9 de explore-drinks.test.js
    
    * implementação de componentes da página de drinks + correção de testes
    
    * implementação parcial de rotas

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
+import useRedirect from '../hooks/useRedirect';
 import HeaderWithoutSearch from '../components/HeaderWithoutSearch';
+import useRandom from '../hooks/useRandom';
 
 function ExploreDrinks() {
+  const { shouldRedirect, redirect } = useRedirect();
+  const [random, error] = useRandom('drink');
+
+  const randomDrink = () => {
+    if (!error) {
+      shouldRedirect(`/bebidas/${random.idDrink}`);
+    }
+  };
+
+  if (redirect.should) return <Redirect to={ redirect.path } />;
+
   return (
     <div>
       <HeaderWithoutSearch>Explorar Bebidas</HeaderWithoutSearch>
+      <Button
+        variant="success"
+        data-testid="explore-by-ingredient"
+        onClick={ () => shouldRedirect('/explorar/bebidas/ingredientes') }
+      >
+        Por Ingredientes
+      </Button>
+      <Button
+        variant="primary"
+        data-testid="explore-by-area"
+        onClick={ () => shouldRedirect('/explorar/bebidas/area') }
+      >
+        Por Local de Origem
+      </Button>
+      <Button
+        variant="warning"
+        data-testid="explore-surprise"
+        onClick={ randomDrink }
+      >
+        Me Surpreenda!
+      </Button>
       <Footer />
     </div>
   );
--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
 import HeaderWithoutSearch from '../components/HeaderWithoutSearch';
+import useRandom from '../hooks/useRandom';
+import useRedirect from '../hooks/useRedirect';
 
-function ExploreFood() {
+export default function ExploreFood() {
+  const { shouldRedirect, redirect } = useRedirect();
+  const [random, error] = useRandom('meal');
+
+  const randomMeal = () => {
+    if (!error) {
+      shouldRedirect(`/comidas/${random.idMeal}`);
+    }
+  };
+
+  if (redirect.should) return <Redirect to={ redirect.path } />;
+
   return (
     <div>
       <HeaderWithoutSearch>Explorar Comidas</HeaderWithoutSearch>
+      <Button
+        variant="success"
+        data-testid="explore-by-ingredient"
+        onClick={ () => shouldRedirect('/explorar/comidas/ingredientes') }
+      >
+        Por Ingredientes
+      </Button>
+      <Button
+        variant="primary"
+        data-testid="explore-by-area"
+        onClick={ () => shouldRedirect('/explorar/comidas/area') }
+      >
+        Por Local de Origem
+      </Button>
+      <Button
+        variant="warning"
+        data-testid="explore-surprise"
+        onClick={ randomMeal }
+      >
+        Me Surpreenda!
+      </Button>
       <Footer />
     </div>
   );
 }
-
-export default ExploreFood;
