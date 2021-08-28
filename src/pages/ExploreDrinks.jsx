264cbefcda28b58035ef4d03fd2fe988ec42be1b
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-08-28 18:26:47 -0300

    Resolve requisito 70, 71, 72 e 73 - Tela de explorar bebidas e comidas por ingredientes, origem e supresa

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,6 +7,26 @@ export default function ExploreDrinks() {
   return (
     <main>
       <Header title="Explorar Bebidas" />
+
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          className="explore-button"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          className="explore-button"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
+
       <Footer />
     </main>
   );
--- src/pages/ExploreMeals.jsx
+++ src/pages/ExploreMeals.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,6 +7,36 @@ export default function ExploreMeals() {
   return (
     <main>
       <Header title="Explorar Comidas" />
+
+      <Link to="/explorar/comidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          className="explore-button"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/area">
+        <button
+          type="button"
+          data-testid="explore-by-area"
+          className="explore-button"
+        >
+          Por Local de Origem
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/ingredientes">
+        { /* verificar pra onde vai */ }
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          className="explore-button"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
+
       <Footer />
     </main>
   );
