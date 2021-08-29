bdb895ff2c3b10e31da9fe8cf7dd32ca2ace6334
André Moreno <deco616@hotmail.com>
2021-08-28 23:07:57 -0300

    Requisito 54 a 59 - Tela de receitas feitas

---
+++ src/components/testLocal.js
@@ -0,0 +1,28 @@
+export default function localStoreTest() {
+  const doneRecipes = [
+    {
+      id: '52771',
+      type: 'comida',
+      area: 'Italian',
+      category: 'Vegetarian',
+      alcoholicOrNot: '',
+      name: 'Spicy Arrabiata Penne',
+      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+      doneDate: '23/06/2020',
+      tags: ['Pasta', 'Curry'],
+    },
+    {
+      id: '178319',
+      type: 'bebida',
+      area: '',
+      category: 'Cocktail',
+      alcoholicOrNot: 'Alcoholic',
+      name: 'Aquamarine',
+      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+      doneDate: '23/06/2020',
+      tags: [],
+    },
+  ];
+
+  localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+}
