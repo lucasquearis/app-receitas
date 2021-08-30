76a6b2a89d0edc5e17445e947ebd25538b64ecc7
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-08-30 17:00:29 -0300

    Refatora funçao de requisiçao a api

--- src/services/FetchDetails.jsx
+++ src/services/FetchDetails.jsx
@@ -1,6 +1,5 @@
 const getDetails = async (api) => {
-  const endpoint = api;
-  const response = await fetch(endpoint);
+  const response = await fetch(api);
   if (!response.ok) return console.log('Falha na requisição de detalhes');
   const result = response.json();
   return result;
