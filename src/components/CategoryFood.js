99d9d723546e6e33fb2c53bbc80de2f15c52cca4
Adriana Biberg <adribiberg@hotmail.com>
2021-08-30 17:36:43 -0300

    Cria botoes de categoria

--- src/components/CategoryFood.js
+++ src/components/CategoryFood.js
@@ -1,22 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function CategoryFood() {
+  const [foodRecipes, setFoodRecipes] = useState([]);
+  const [foodCategories, setFoodCategories] = useState([]);
+  const maxCategories = 5;
+  const maxList = 12;
+
   useEffect(() => {
-    const foodEndpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-    const foodCategoriesEndpoint = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+    const getFood = async () => {
+      const foodApi = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+      const { meals } = await fetch(foodApi).then((data) => data.json());
+      setFoodRecipes(meals);
+    };
 
-  //   fetch(foodEndpoint)
-  //     .then((res) => res.json())
-  //     .then(({ meals }) => setFoodRecipes(meals));
+    const getCategories = async () => {
+      const foodApiCategories = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+      const { meals } = await fetch(foodApiCategories).then((data) => data.json());
+      setFoodCategories(meals);
+    };
 
-  //   fetch(foodCategoriesEndpoint)
-  //     .then((res) => res.json())
-  //     .then(({ meals }) => setFoodCategories(meals));
+    getFood();
+    getCategories();
   }, []);
+
+  const categoriesBtn = () => {
+    const categoryBtn = foodCategories.slice(0, maxCategories).map(({ strCategory }) => (
+      <button
+        data-testid={ `${strCategory}-category-filter` }
+        type="button"
+        key={ strCategory }
+      >
+        { strCategory }
+      </button>
+    ));
+    return categoryBtn;
+  };
 
   return (
     <div>
-      <p>Categories</p>
+      <div>
+        { categoriesBtn() }
+      </div>
+      <div>
+        { foodRecipes.slice(0, maxList).map((meal) => (
+          <p key={ meal.idMeal }>{ meal.strMeal}</p>)) }
+      </div>
     </div>
   );
 }
--- src/pages/RecipesList.js
+++ src/pages/RecipesList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import CategoryFood from '../components/CategoryFood';
 
 function RecipeList() {
   return (
     <div>
       <p>Recipe List</p>
+      <CategoryFood />
       <Footer />
     </div>
   );
