9416856effea6dbcf936010bf91462abd54885ec
lbasques <82237299+lbasques@users.noreply.github.com>
2021-08-30 00:56:07 -0300

    Merge [Requisitos 82 a 87 (#618)

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Perfil() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  function getEmail() {
+    return (
+      <p type="text" data-testid="profile-email">
+        {user.email}
+      </p>
+    );
+  }
   return (
     <div>
-      <h1 data-testid="page-title">Página de Perfil</h1>
+      {user && getEmail()}
+      <Link to="/receitas-feitas">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+
+        >
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => {
+            localStorage.clear();
+          } }
+        >
+          Sair
+        </button>
+      </Link>
     </div>
   );
 }
