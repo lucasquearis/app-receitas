1d9109babdd351d8b473c5500e7e115739531df7
cristianocsl <82241160+cristianocsl@users.noreply.github.com>
2021-08-30 16:13:44 -0300

    requisitos 67 a 69 (#610)

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +8,12 @@ function Explore() {
   return (
     <>
       <Header title="Explorar" />
+      <Link to="/explorar/comidas">
+        <Button data-testid="explore-food" type="button">Explorar Comidas</Button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <Button data-testid="explore-drinks" type="button">Explorar Bebidas</Button>
+      </Link>
       <Footer />
     </>
   );
