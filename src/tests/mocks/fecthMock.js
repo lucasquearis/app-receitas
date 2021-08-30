a149dec094c05325b0a414a2eabf4964514a80fe
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-08-30 13:08:39 -0300

    desenvolvido testes para os componentenentes de tela principal para comidas e para bebidas

--- src/tests/mocks/fecthMock.js
+++ src/tests/mocks/fecthMock.js
@@ -1,5 +1,8 @@
 import oneDrink from '../../../cypress/mocks/oneDrink';
-import { soupMeals, oneMeal, ginDrinks, meals, drinks } from './respMoks';
+import {
+  soupMeals, oneMeal, ginDrinks, meals, drinks, mealCategories, drinkCategories,
+  chickenMeals, cocktailDrinks,
+} from './respMoks';
 
 const fetchMock = async (url) => ({
   status: 200,
@@ -17,6 +20,18 @@ const fetchMock = async (url) => ({
 
     case 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=gin':
       return ginDrinks;
+
+    case 'https://www.themealdb.com/api/json/v1/1/list.php?c=list':
+      return mealCategories;
+
+    case 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list':
+      return drinkCategories;
+
+    case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken':
+      return chickenMeals;
+
+    case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail':
+      return cocktailDrinks;
 
     default: return { ...meals, ...drinks };
     }
--- src/tests/mocks/respMoks.js
+++ src/tests/mocks/respMoks.js
@@ -4,5 +4,12 @@ import oneDrink from '../../../cypress/mocks/oneDrink';
 import ginDrinks from '../../../cypress/mocks/ginDrinks';
 import meals from '../../../cypress/mocks/meals';
 import drinks from '../../../cypress/mocks/drinks';
+import mealCategories from '../../../cypress/mocks/mealCategories';
+import drinkCategories from '../../../cypress/mocks/drinkCategories';
+import chickenMeals from '../../../cypress/mocks/chickenMeals';
+import cocktailDrinks from '../../../cypress/mocks/cocktailDrinks';
 
-export { soupMeals, oneMeal, oneDrink, ginDrinks, meals, drinks };
+export {
+  soupMeals, oneMeal, oneDrink, ginDrinks, meals, drinks, mealCategories, drinkCategories,
+  chickenMeals, cocktailDrinks,
+};
