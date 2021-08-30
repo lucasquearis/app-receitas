189eaf2878f6769a3a03173ac80be843e63eef1c
thosijulio <thosijulio@gmail.com>
2021-08-30 17:54:45 -0300

    correcao do arquivo genericFetchAPI

--- src/services/genericFetchAPI.js
+++ src/services/genericFetchAPI.js
@@ -1,4 +1,4 @@
-export default function genericFetchAPI(mealOrCocktail, type, start, search) {
+function genericFetchAPI(mealOrCocktail, type, start, search) {
   return fetch(`https://www.the${mealOrCocktail}db.com/api/json/v1/1/${type}.php?${start}=${search}`)
     .then((response) => response.json())
     .then((data) => data);
