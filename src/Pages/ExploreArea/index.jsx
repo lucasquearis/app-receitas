b1aa5bdceb5ccbd747ffb6df8b4bbad5c5f5f463
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-30 16:21:44 -0400

    create function to get areas

--- src/Pages/ExploreArea/index.jsx
+++ src/Pages/ExploreArea/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from '../../Components/Header';
 import BottomMenu from '../../Components/Footer/BottomMenu';
+import fetchApi from '../../Helpers/fetchApi';
 
 function ExploreArea() {
+  const [areas, setAreas] = useState([]);
+
+  useEffect(() => {
+    const getArea = async () => {
+      const response = await fetchApi('https://www.themealdb.com/api/json/v1/1/', 'list.php?a=list');
+      setAreas(response.meals);
+    };
+    getArea();
+  }, []);
+
+  const handleChange = ({ target: { value } }) => {
+    console.log(value);
+  };
+
   return (
     <div>
       <Header
@@ -11,8 +26,11 @@ function ExploreArea() {
         searchButton={ false }
       />
       <div>
-        <select data-testid="explore-by-area-dropdown">
-          <option>Brasil</option>
+        <select
+          data-testid="explore-by-area-dropdown"
+          onChange={ (event) => handleChange(event) }
+        >
+          {areas.map((current) => <option key={ current }>{current.strArea}</option>)}
         </select>
         <div>
           oi
