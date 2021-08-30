7fa4fdfa925a63247807849f50b7d7e0cf854905
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-30 15:59:42 -0300

    inicia o protótipo da tela

--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -3,8 +3,19 @@ import Header from '../components/Header';
 
 export default function RecipesDone() {
   return (
-    <span>
-      <Header title="Receitas Feitas" />
-    </span>
+    <main>
+      <header>
+        <Header title="Receitas Feitas" />
+      </header>
+      <button type="button" data-testid="filter-by-all-btn">All</button>
+      <button type="button" data-testid="filter-by-food-btn">Food</button>
+      <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      <img alt="imagem da receita" data-testid={ `${0}-horizontal-image` } />
+      <p data-testid={ `${0}-horizontal-top-text` }>Category</p>
+      <h1 data-testid={ `${0}-horizontal-name` }>Título</h1>
+      <p data-testid={ `${0}-horizontal-done-date` }>Data que a receita foi feita</p>
+      <button type="button" data-testid={ `${0}-horizontal-share-btn` }>SHARE</button>
+      <p data-testid={ `${0}-${1}-horizontal-tag` }>Tag</p>
+    </main>
   );
 }
