40b7d1a8554f0625d993fabfac27e91c9a8afd7d
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-08-30 12:11:43 -0300

    R47| Adiciona páginas de receitas em progresso

--- src/pages/BebidasEmProcesso.js
+++ src/pages/BebidasEmProcesso.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Loading from '../components/Loading';
 
 export default function BebidasEmProcesso() {
+  const [drink, setDrink] = useState();
+  const location = useLocation();
+  const URL_DRINK = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+  useEffect(() => {
+    const api = async () => {
+      const idApi = location.pathname.split('/')[2];
+      console.log(idApi);
+      const response = await fetch(`${URL_DRINK}${idApi}`);
+      const data = await response.json();
+      setDrink(data.drinks[0]);
+    };
+    api();
+  }, []);
+
+  const setIngredients = () => {
+    const ingredients1 = Object.keys(drink)
+      .filter((ingredient) => ingredient
+        .includes('strIngredient')
+        && drink[ingredient] !== null && drink[ingredient] !== '');
+    return ingredients1.map((jonas, index) => (
+      <li
+        data-testid={ `${index}-ingredient-step` }
+        key={ index }
+      >
+        {drink[jonas]}
+        -
+        {drink[`strMeasure${index + 1}`]}
+      </li>));
+  };
+
+  if (drink === undefined) {
+    return <Loading />;
+  }
+
   return (
     <div>
-      BebidasEmProcesso
+      {/* <p>{drink.idDrink}</p> */}
+      <img src={ drink.strDrinkThumb } alt="recipe" data-testid="recipe-photo" />
+      <h2 data-testid="recipe-title">{drink.strDrink}</h2>
+      <button type="button" data-testid="share-btn">Compartilhar</button>
+      <button type="button" data-testid="favorite-btn">Favorito</button>
+      <p data-testid="recipe-category">{drink.strAlcoholic}</p>
+      <ul>
+        {setIngredients()}
+      </ul>
+      <p data-testid="instructions">{drink.strInstructions}</p>
+      <button type="button" data-testid="finish-recipe-btn">Finalizar Receita</button>
     </div>
   );
 }
--- src/pages/ComidasEmProcesso.js
+++ src/pages/ComidasEmProcesso.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Loading from '../components/Loading';
 
-const ComidasEmProcesso = () => (
-  <div>
-    ComidasEmProcesso
-  </div>
-);
+const ComidasEmProcesso = () => {
+  const [food, setFood] = useState();
+  const location = useLocation();
+  const URL_FOOD = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+  useEffect(() => {
+    const api = async () => {
+      const idApi = location.pathname.split('/')[2];
+      const response = await fetch(`${URL_FOOD}${idApi}`);
+      const data = await response.json();
+      setFood(data.meals[0]);
+    };
+    api();
+  }, []);
+
+  if (food === undefined) {
+    return <Loading />;
+  }
+
+  const setIngredients = () => {
+    const ingredients1 = Object.keys(food)
+      .filter((ingredient) => ingredient
+        .includes('strIngredient')
+        && food[ingredient] !== null && food[ingredient] !== '');
+    return ingredients1.map((jonas, index) => (
+      <li
+        data-testid={ `${index}-ingredient-step` }
+        key={ index }
+      >
+        {food[jonas]}
+        -
+        {food[`strMeasure${index + 1}`]}
+      </li>));
+  };
+
+  return (
+    <div>
+      <img src={ food.strMealThumb } alt="recipe" data-testid="recipe-photo" />
+      <h2 data-testid="recipe-title">{food.strMeal}</h2>
+      <button type="button" data-testid="share-btn">Compartilhar</button>
+      <button type="button" data-testid="favorite-btn">Favorito</button>
+      <p data-testid="recipe-category">{food.strCategory}</p>
+      <ul>
+        {setIngredients()}
+      </ul>
+      <p data-testid="instructions">{food.strInstructions}</p>
+      <button type="button" data-testid="finish-recipe-btn">Finalizar Receita</button>
+    </div>
+  );
+};
 
 export default ComidasEmProcesso;
