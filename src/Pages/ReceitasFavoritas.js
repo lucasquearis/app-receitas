7ec1cda04e2884c71c294504480fb509ecb53ca0
Josimar Souza <contact.josimarsouza@gmail.com>
2021-08-30 17:01:56 -0300

    Adicionada seçao de filtros no pagina

--- src/Pages/ReceitasFavoritas.js
+++ src/Pages/ReceitasFavoritas.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from '../Components/Header';
+import './ReceitasFavoritas.css';
 
 export default function Perfil() {
   return (
     <section>
       <Header title="Receitas Favoritas" />
+      <div className="filter-section">
+        <button data-testid="filter-by-all-btn" type="button">All</button>
+        <button data-testid="filter-by-food-btn" type="button">Food</button>
+        <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+      </div>
     </section>
   );
 }
