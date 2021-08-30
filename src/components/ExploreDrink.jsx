eaba8e0e5b158a4a0b4d721f510e0cc2f9bcafc7
Patrick Dack <patrickdack89@gmail.com>
2021-08-30 15:58:50 -0300

    Requisito 72

--- src/components/ExploreDrink.jsx
+++ src/components/ExploreDrink.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 function ExploreDrink() {
+  const [location, setLocation] = useState({
+    ingredient: false,
+    explore: false,
+  });
+
+  const redirect = (path) => {
+    setLocation({ ...location, [path]: true });
+  };
+
+  if (location.ingredient) return <Redirect to="/explorar/bebidas/ingredientes" />;
+
   return (
     <div>
       <button
         type="button"
         data-testid="explore-by-ingredient"
+        onClick={ () => redirect('ingredient') }
       >
         Por Ingredientes
       </button>
--- src/components/ExploreRecipe.jsx
+++ src/components/ExploreRecipe.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 function ExploreRecipe() {
+  const [location, setLocation] = useState({
+    ingredient: false,
+    explore: false,
+  });
+
+  const redirect = (path) => {
+    setLocation({ ...location, [path]: true });
+  };
+
+  if (location.ingredient) return <Redirect to="/explorar/comidas/ingredientes" />;
+
   return (
     <div>
       <button
         type="button"
         data-testid="explore-by-ingredient"
+        onClick={ () => redirect('ingredient') }
       >
         Por Ingredientes
       </button>
