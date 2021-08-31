41a00876abbe25037de79f77ca243d26ea80001d
matheustkaczyk <matheus111904@hotmail.com>
2021-08-31 19:28:17 -0300

    testando nova ordem de rotas

--- src/App.js
+++ src/App.js
@@ -25,31 +25,29 @@ function App() {
     // muito obrigado ao meu amigo Gedeão por ajudar à montar e organizar as minhas rotas.
     <BrowserRouter>
       <Switch>
-        <Route exact path="/receitas-favoritas" component={ FavoriteRecipes } />
-        <Route exact path="/receitas-feitas" component={ FinishedRecipes } />
-        <Route exact path="/perfil" component={ Profile } />
-        <Route exact path="/explorar/comidas/area" component={ SearchByOrigin } />
-        <Route exact path="/comidas/:id/in-progress" component={ RecipeInProgress } />
-        <Route exact path="/bebidas/:id/in-progress" component={ DrinkInProgress } />
+        <Route path="/comidas/:id/in-progress" component={ RecipeInProgress } />
+        <Route path="/bebidas/:id/in-progress" component={ DrinkInProgress } />
+        <Route path="/bebidas/:id" component={ DrinkDetails } />
+        <Route path="/comidas/:id" component={ FoodDetails } />
+        <Route path="/explorar/comidas/area" component={ SearchByOrigin } />
         <Route
-          exact
           path="/explorar/bebidas/ingredientes"
           component={ SearchDrinksByIngredients }
         />
         <Route
-          exact
           path="/explorar/comidas/ingredientes"
           component={ SearchFoodByIngredients }
         />
-        <Route exact path="/explorar/bebidas" component={ SearchDetailsDrinks } />
-        <Route exact path="/explorar/comidas" component={ SearchDetailsFood } />
-        <Route exact path="/explorar" component={ Search } />
-        <Route exact path="/bebidas/:id" component={ DrinkDetails } />
-        <Route exact path="/comidas/:id" component={ FoodDetails } />
-        <Route exact path="/bebidas" component={ Drinks } />
-        <Route exact path="/comidas" component={ Recipes } />
+        <Route path="/explorar/bebidas" component={ SearchDetailsDrinks } />
+        <Route path="/explorar/comidas" component={ SearchDetailsFood } />
+        <Route path="/receitas-favoritas" component={ FavoriteRecipes } />
+        <Route path="/receitas-feitas" component={ FinishedRecipes } />
+        <Route path="/perfil" component={ Profile } />
+        <Route path="/explorar" component={ Search } />
+        <Route path="/bebidas" component={ Drinks } />
+        <Route path="/comidas" component={ Recipes } />
+        <Route path="*" component={ NotFound } />
         <Route exact path="/" component={ Login } />
-        <Route exact path="*" component={ NotFound } />
       </Switch>
     </BrowserRouter>
   );
