b79feb1567f2ca915483cc7ab6e6844b9a1abe97
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-08-31 01:27:38 -0300

    requisitos 39 e 40"

--- src/components/ButtonDrinks.jsx
+++ src/components/ButtonDrinks.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 
 function ButtonDrinks() {
-  const id = 178319;
+  const ido = 178319;
+  const [none, setNone] = useState(true);
+  const [inProgress, setInProgress] = useState(false);
   const setHistory = useHistory();
   const handleClick = () => {
-    setHistory.push(`/bebidas/${id}/in-progress`);
+    setHistory.push(`/bebidas/${ido}/in-progress`);
   };
+
+  useEffect(() => {
+    setNone(localStorage.getItem('doneRecipes')
+      && (JSON.parse(localStorage.getItem('doneRecipes')).some(
+        (item) => item.id === ido,
+      )));
+    setInProgress(localStorage.getItem('inProgressRecipes')
+      && Object.keys(JSON.parse(localStorage.getItem('inProgressRecipes')).some((itemId) => itemId === ido)));
+  }, []);
 
   return (
     <div className="div-button-details">
       <button
+        hidden={ none }
         className="button-details"
         type="button"
         data-testid="start-recipe-btn"
         onClick={ handleClick }
       >
-        iniciar receita
+        {inProgress ? 'Continuar Receita' : 'Iniciar Receita'}
       </button>
     </div>
   );
--- src/components/ButtonFoods.jsx
+++ src/components/ButtonFoods.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 
 function ButtonFoods() {
-  const id = 52771;
+  const ido = 52771;
+  const [none, setNone] = useState(true);
+  const [inProgress, setInProgress] = useState(false);
   const setHistory = useHistory();
   const handleClick = () => {
-    setHistory.push(`/comidas/${id}/in-progress`);
+    setHistory.push(`/comidas/${ido}/in-progress`);
   };
+
+  useEffect(() => {
+    setNone(localStorage.getItem('doneRecipes')
+      && (JSON.parse(localStorage.getItem('doneRecipes')).some(
+        (item) => item.id === ido,
+      )));
+    setInProgress(localStorage.getItem('inProgressRecipes')
+      && Object.keys(JSON.parse(localStorage.getItem('inProgressRecipes')).some((itemId) => itemId === ido)));
+  }, []);
 
   return (
     <div className="div-button-details">
       <button
+        hidden={ none }
         className="button-details"
         type="button"
         data-testid="start-recipe-btn"
         onClick={ handleClick }
       >
-        iniciar receita
+        {inProgress ? 'Continuar Receita' : 'Iniciar Receita'}
       </button>
     </div>
   );
