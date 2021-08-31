585578ad5e6725366ffc604510111486cee83451
Ana Laura Alves Ramos <82340476+anaalvsr@users.noreply.github.com>
2021-08-31 16:30:55 -0300

    [GRUPO 10] Explorar comidas e bebidas  (#438)
    * req 70 e 71
    
    * req 72 e 73
    
    * req 74

---
+++ src/pages/ExploreDrinks.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import { fetchRandomDrink } from '../services/randomsAPI';
+
+function ExploreDrinks() {
+  const [drinkRandom, setDrinkRandom] = useState();
+
+  useEffect(() => {
+    fetchRandomDrink()
+      .then((response) => setDrinkRandom(response.drinks[0].idDrink));
+  });
+
+  return (
+    <>
+      <Header />
+      <div>
+        <Link to="/explorar/bebidas/ingredientes">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to={ `/bebidas/${drinkRandom}` }>
+          <button
+            type="button"
+            data-testid="explore-surprise"
+          >
+            Me Surpreenda!
+          </button>
+        </Link>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default ExploreDrinks;
