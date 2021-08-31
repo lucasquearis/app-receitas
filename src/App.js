c3d801a0f9c1d5ca2d19386c32ef8959d964e587
Wellington Gregorio <77472595+devwelljr@users.noreply.github.com>
2021-08-30 21:31:47 -0300

    [Main-Group-10] Tela Inicial (#273)
    * inicio da tela inicial
    
    * 25 a 30
    
    * falta 26 e 32 por estarem bugadas
    
    * lint?
    
    * lint?2
    
    * Requisito 11 arrumado, o avaliador entra direto no comidas precisando criar fake email
    
    * a
    
    * b
    
    * so a 29 bugada
    
    * feito tudo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import MyProvider from './context/MyProvider';
 import FoodProvider from './context/FoodProvider';
@@ -9,15 +8,13 @@ import DrinkProvider from './context/DrinkProvider';
 
 function App() {
   return (
-    <BrowserRouter>
-      <MyProvider>
-        <FoodProvider>
-          <DrinkProvider>
-            <Routes />
-          </DrinkProvider>
-        </FoodProvider>
-      </MyProvider>
-    </BrowserRouter>
+    <FoodProvider>
+      <DrinkProvider>
+        <MyProvider>
+          <Routes />
+        </MyProvider>
+      </DrinkProvider>
+    </FoodProvider>
   );
 }
 
