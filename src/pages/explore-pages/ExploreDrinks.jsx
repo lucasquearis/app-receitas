1d4b912b2992f719f663c578a0fdd774627cdb47
Gabriel Takaki <gabitj@icloud.com>
2021-08-31 16:37:35 -0300

    implementa as rotas nas telas de explorar bebidas e comidas

--- src/pages/explore-pages/ExploreDrinks.jsx
+++ src/pages/explore-pages/ExploreDrinks.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import '../../styles/Header.css';
 import Footer from '../../components/Footer';
 import '../../styles/Footer.css';
+import '../../styles/Explore.css';
 
 function ExploreDrinks() {
   return (
     <div>
       <Header brand="Explorar Bebidas" className="img-search" />
+      <div className="div-explore">
+        <Link to="/explorar/bebidas/ingredientes">
+          <button
+            data-testid="explore-by-ingredient"
+            type="button"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to="/explorar/comidas/area">
+          <button
+            data-testid="explore-by-area"
+            type="button"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button
+            data-testid="explore-surprise"
+            type="button"
+          >
+            Me Surpreenda!
+          </button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
--- src/pages/explore-pages/ExploreFoods.jsx
+++ src/pages/explore-pages/ExploreFoods.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import '../../styles/Header.css';
 import Footer from '../../components/Footer';
@@ -8,6 +9,32 @@ function ExploreFoods() {
   return (
     <div>
       <Header brand="Explorar Comidas" className="img-search" />
+      <div className="div-explore">
+        <Link to="/explorar/comidas/ingredientes">
+          <button
+            data-testid="explore-by-ingredient"
+            type="button"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to="/explorar/comidas/area">
+          <button
+            data-testid="explore-by-area"
+            type="button"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+        <Link to="/explorar/comidas">
+          <button
+            data-testid="explore-surprise"
+            type="button"
+          >
+            Me Surpreenda!
+          </button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
