8ef278579e7703237c1df198656b8ef89253d6b1
Janderson555 <71929294+Janderson555@users.noreply.github.com>
2021-08-31 15:00:56 -0300

    main-group-6-req-22-a-24 (#764)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
 import '../CSS/Footer.css';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <footer className="footer" data-testid="footer">
-      <button
-        src="../images/drinkIcon.svg"
-        className="drinks-botton"
-        aria-label="drinks-botton"
-        name="drinks-botton"
-        type="button"
-        data-testid="drinks-bottom-btn"
-      />
-      <button
-        src="../images/exploreIcon.svg"
-        className="explore-botton"
-        aria-label="explore-bottom"
-        name="explore-bottom"
-        type="button"
-        data-testid="explore-bottom-btn"
-      />
-      <button
-        src="../images/mealIcon.svg"
-        className="food-botton"
-        aria-label="food-bottom"
-        name="food-bottom"
-        type="button"
-        data-testid="food-bottom-btn"
-      />
+      <Link to="/bebidas">
+        <img
+          name="drinks-botton"
+          className="drinks-botton"
+          data-testid="drinks-bottom-btn"
+          src="../images/drinkIcon.svg"
+          alt="drinks-botton"
+        />
+      </Link>
+      <Link to="/explorar">
+        <img
+          name="explore-bottom"
+          className="explore-botton"
+          data-testid="explore-bottom-btn"
+          src="../images/exploreIcon.svg"
+          alt="explore-bottom"
+        />
+      </Link>
+      <Link to="/comidas">
+        <img
+          name="food-bottom"
+          className="food-botton"
+          data-testid="food-bottom-btn"
+          src="../images/mealIcon.svg"
+          alt="food-bottom"
+        />
+      </Link>
     </footer>
   );
 }
