66d7ba74559268ea5b1eab4b3d7b4a498c0accf7
Gabriel Takaki <gabitj@icloud.com>
2021-08-31 15:56:30 -0300

    pagina de explorar

--- src/pages/explore-pages/Explore.jsx
+++ src/pages/explore-pages/Explore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import '../../styles/Header.css';
 import Footer from '../../components/Footer';
@@ -8,6 +9,14 @@ function Explore() {
   return (
     <div>
       <Header brand="Explorar" className="img-search" />
+      <div className="div-explore">
+        <Link to="/explorar/comidas">
+          <button data-testid="explore-food" type="button">Explorar Comidas</button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button data-testid="explore-drinks" type="button">Explorar Bebidas</button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
