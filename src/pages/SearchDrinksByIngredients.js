a5515ca1206e996eae24def174eee0c60dad8298
Renan Santiago <renansferreira8@gmail.com>
2021-08-31 08:56:47 -0300

    Group 3 explore ingredients (#711)
    * Resolve requisitos 37 e 39
    
    * Resolve requisitos 37 e 39(ajustes no lint)
    
    * Resolve requisitos 37 e 39(ajustes no lint)
    
    * Resolve requisitos 75 e 76
    
    * Resolve requisitos 75 e 76

--- src/pages/SearchDrinksByIngredients.js
+++ src/pages/SearchDrinksByIngredients.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import FooterMenu from '../components/FooterMenu';
 import profileIcon from '../images/profileIcon.svg';
+import DrinksContext from '../context/DrinksContext';
+import IngredientCard from '../components/IngredientsCard';
+import './ingredientDrinks.css';
 
-const SearchDrinksByIngredients = () => (
-  <div>
-    <button type="button">
-      <img src={ profileIcon } alt="profile" data-testid="profile-top-btn" />
-    </button>
-    <p data-testid="page-title">Explorar Ingredientes</p>
-    <FooterMenu />
-  </div>
-);
+const SearchDrinksByIngredients = () => {
+  const { ingredients } = useContext(DrinksContext);
+  const NUMBER_OF_CARDS = 12;
+
+  return (
+    <div className="ingredient-container">
+      <header>
+        <button type="button">
+          <img src={ profileIcon } alt="profile" data-testid="profile-top-btn" />
+        </button>
+        <p data-testid="page-title">Explorar Ingredientes</p>
+      </header>
+      <div className="ingredient-list">
+        { ingredients.slice(0, NUMBER_OF_CARDS).map((ingredient, index) => (
+          <IngredientCard
+            key={ index }
+            ingredientImg={ ingredient.strIngredient1 }
+            ingredientName={ ingredient.strIngredient1 }
+            index={ index }
+            path="thecocktaildb"
+          />
+        ))}
+      </div>
+      <FooterMenu />
+    </div>
+  );
+};
 
 export default SearchDrinksByIngredients;
