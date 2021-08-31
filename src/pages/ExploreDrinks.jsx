d0b4a4474a451fab9003a8b29c93ba0fc8cb5664
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-08-31 12:04:05 -0300

    Resolve requisitos 70 até 74 e os testes.

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { fetchRandomDrink } from '../services/fetchRandoms';
 
 export default function ExploreDrinks() {
+  const [randomDrink, setRandomDrink] = useState(0);
+  useEffect(() => {
+    fetchRandomDrink()
+      .then((response) => setRandomDrink(response.drinks[0].idDrink));
+  });
   return (
     <div>
       <Header title="Explorar Bebidas" search={ false } />
+      <div className="explore-container">
+        <Link
+          to="/explorar/bebidas/ingredientes"
+          data-testid="explore-by-ingredient"
+          className="explore-btn"
+        >
+          Por Ingredientes
+        </Link>
+        <Link
+          to={ `/bebidas/${randomDrink}` }
+          data-testid="explore-surprise"
+          className="explore-btn"
+        >
+          Me Surpreenda!
+        </Link>
+      </div>
       <Footer />
     </div>
   );
--- src/pages/ExploreMeals.jsx
+++ src/pages/ExploreMeals.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { fetchRandomMeal } from '../services/fetchRandoms';
 
 export default function ExploreMeals() {
+  const [randomMeal, setRandomMeal] = useState(0);
+  useEffect(() => {
+    fetchRandomMeal()
+      .then((response) => setRandomMeal(response.meals[0].idMeal));
+  });
   return (
     <div>
       <Header title="Explorar Comidas" search={ false } />
+      <div className="explore-container">
+        <Link
+          to="/explorar/comidas/ingredientes"
+          data-testid="explore-by-ingredient"
+          className="explore-btn"
+        >
+          Por Ingredientes
+        </Link>
+        <Link
+          to="/explorar/comidas/area"
+          data-testid="explore-by-area"
+          className="explore-btn"
+        >
+          Por Local de Origem
+        </Link>
+        <Link
+          to={ `/comidas/${randomMeal}` }
+          data-testid="explore-surprise"
+          className="explore-btn"
+        >
+          Me Surpreenda!
+        </Link>
+      </div>
       <Footer />
     </div>
   );
