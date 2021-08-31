dbc53c76297a84e1858f672d5a3e22264eba67a4
martinsph <ph.mmartins@gmail.com>
2021-08-31 15:03:25 -0300

    add req 67 ate 69

--- src/pages/explore/ExploreDrinks.jsx
+++ src/pages/explore/ExploreDrinks.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
-export default function ExploreDrinks() {
+export default function Explore() {
   return (
-    <>
-      <Header title="Explorar Bebidas" />
-      ExploreDrinks
+    <section>
+      <Header />
+      <h1>Explorar</h1>
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          data-testid="explore-by-ingredient"
+          type="button"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      {/* <Link to="">
+        <button
+          data-testid="explore-surprise"
+          type="button"
+        >
+          Me Surpreenda!
+        </button>
+      </Link> */}
       <Footer />
-    </>
+    </section>
   );
 }
