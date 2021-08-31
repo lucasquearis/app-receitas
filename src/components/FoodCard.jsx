17bdaea2041c8384cd8dbb37e8419fbb14248e07
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-08-31 18:44:17 -0300

    Estiliza Cards

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import '../styles/FoodCard.css'
 
 function FoodCard({ food, index }) {
   return (
     <Link to={ `/comidas/${food.idMeal}` }>
-      <li
-        index={ food.idMeal }
-        name={ food.strMeal }
-        data-testid={ `${index}-recipe-card` }
-      >
-        <img
-          data-testid={ `${index}-card-img` }
-          src={ food.strMealThumb }
-          alt={ food.strMeal }
-        />
-        <p data-testid={ `${index}-card-name` }>{food.strMeal}</p>
-      </li>
+      <div className="foodcard">
+        <li
+          index={ food.idMeal }
+          name={ food.strMeal }
+          data-testid={ `${index}-recipe-card` }
+        >
+          <img
+            data-testid={ `${index}-card-img` }
+            src={ food.strMealThumb }
+            alt={ food.strMeal }
+          />
+          <p data-testid={ `${index}-card-name` }>{food.strMeal}</p>
+        </li>
+      </div>
     </Link>
   );
 }
