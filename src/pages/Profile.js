068006ec78443baa9cd8e6a86e1481d4e95e799c
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-08-31 15:12:20 -0300

    Req 83 profile, continuacao

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 import profileIcon from '../images/profileIcon.svg';
+
+const handleClick = () => {
+  return (<Redirect to="/" />);
+};
+
+const emailStorage = JSON.parse(localStorage.getItem('user'));
+console.log(emailStorage);
 
 const Profile = () => (
   <div>
-    <button type="button">
+    <label htmlFor="email-perfil">
+      Email:
+      <input id="email-perfil" data-testid="profile-email" />
+    </label>
+    <button
+      name="Receitas Feitas"
+      type="button"
+      data-testid="profile-done-btn"
+    >
+      Receitas Feitas
+    </button>
+    <button
+      name="Receitas Favoritas"
+      type="button"
+      data-testid="profile-favorite-btn"
+      onClick={ handleClick }
+    >
+      Receitas Favoritas
+    </button>
+    <button
+      name="Sair"
+      type="button"
+      data-testid="profile-logout-btn"
+    >
+      Sair
+    </button>
+    <button type="button" onClick={ handleClick }>
       <img src={ profileIcon } alt="profile" data-testid="profile-top-btn" />
     </button>
     <p data-testid="page-title">Perfil</p>
