3360f0ca36c662890b6b91edf08915d8eb926834
Josimar Souza <contact.josimarsouza@gmail.com>
2021-08-31 11:23:43 -0300

    Adiciona funcionalidade nos filtros

--- src/Pages/ReceitasFavoritas.js
+++ src/Pages/ReceitasFavoritas.js
@@ -28,12 +28,14 @@ localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
 
 export default function Perfil() {
   const [favorites, setFavorites] = useState([]);
+  const [favoritesFiltered, setFavoritesFiltered] = useState([]);
 
   useEffect(() => {
     const getFavorites = () => {
       const favoritesResults = JSON.parse(localStorage.getItem('favoriteRecipes'));
       if (favoritesResults !== null) {
         setFavorites(favoritesResults);
+        setFavoritesFiltered(favoritesResults);
       }
     };
 
@@ -44,18 +46,59 @@ export default function Perfil() {
     const newFavorites = favorites.filter((favorite) => favorite.id !== id);
     localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorites));
     setFavorites(newFavorites);
+
+    const newFilter = favoritesFiltered.filter((favorite) => favorite.id !== id);
+    setFavoritesFiltered(newFilter);
+  };
+
+  const onFilterClick = ({ target: { name } }) => {
+    if (name === 'all') {
+      setFavoritesFiltered(favorites);
+      return;
+    }
+    if (name === 'food') {
+      const newFilter = favorites.filter((favorite) => (
+        favorite.type === 'comida'));
+
+      setFavoritesFiltered(newFilter);
+      return;
+    }
+    // bebida
+    const newFilter = favorites.filter((favorite) => favorite.type === 'bebida');
+    setFavoritesFiltered(newFilter);
   };
 
   return (
     <section>
       <Header title="Receitas Favoritas" />
       <div className="filter-section">
-        <button data-testid="filter-by-all-btn" type="button">All</button>
-        <button data-testid="filter-by-food-btn" type="button">Food</button>
-        <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+          name="all"
+          onClick={ onFilterClick }
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          type="button"
+          name="food"
+          onClick={ onFilterClick }
+        >
+          Food
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+          name="drink"
+          onClick={ onFilterClick }
+        >
+          Drinks
+        </button>
       </div>
       <div className="favorite-section">
-        { favorites.map((favorite, index) => (
+        { favoritesFiltered.map((favorite, index) => (
           <FavoriteCards
             key={ favorite.id }
             recipe={ favorite }
