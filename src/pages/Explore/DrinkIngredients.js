01959646cc3e77cacb1e07a1db3dd2a2cbbb97e7
Daniel <danielbped@gmail.com>
2021-08-31 17:18:43 -0300

    Resolve requisitos 75 e 76

--- src/pages/Explore/DrinkIngredients.js
+++ src/pages/Explore/DrinkIngredients.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import MenuInferior from '../../components/MenuInferior';
 import Header from '../../components/Header';
+import Context from '../../context/Context';
+import IngredientCard from '../../components/IngredientCard';
 
 export default function DrinkIngredients() {
+  const {
+    drinkIngredients: { list, loading },
+    requestDrinkIngredients,
+  } = useContext(Context);
+  useEffect(() => {
+    if (loading) {
+      requestDrinkIngredients();
+    }
+  });
+
+  const ingredients = [];
+  const maxIngredients = 12;
+  for (let index = 0; index < maxIngredients; index += 1) {
+    if (!loading) {
+      ingredients.push(list.drinks[index]);
+    }
+  }
+
+  const showIngredients = () => (
+    <div>
+      { ingredients.map((ingredient, index) => (
+        <IngredientCard
+          ingredient={ ingredient }
+          key={ index }
+          index={ index }
+          type="cocktail"
+        />
+      ))}
+    </div>
+  );
   return (
     <div>
       <Header
@@ -10,6 +42,7 @@ export default function DrinkIngredients() {
         title="Explorar Ingredientes"
         search={ false }
       />
+      { showIngredients() }
       <MenuInferior />
     </div>
   );
