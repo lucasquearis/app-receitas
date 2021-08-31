815d404a7f84b11465958af2526a8b1810310a93
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-08-30 21:33:03 -0300

    prepara page FoodByOrigin com os arquivos do Store e Loading

--- src/pages/FoodByOrigin.jsx
+++ src/pages/FoodByOrigin.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+// import { v4 } from 'uuid';
 import Footer from '../components/Footer';
 import HeaderWithSearch from '../components/HeaderWithSearch';
+import { getMeal } from '../redux/actions';
 
 function FoodByOrigin() {
+  const dispatch = useDispatch();
+  const { reducerAPI: { loading, meals } } = useSelector((state) => state);
+  console.log(meals);
+
+  useEffect(() => {
+    dispatch(getMeal());
+  }, [dispatch]);
+
+  if (loading) {
+    return <h2>Carregando...</h2>;
+  }
   return (
     <div>
       <HeaderWithSearch>Explorar Origem</HeaderWithSearch>
