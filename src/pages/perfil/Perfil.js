dc9fe8256b28153904b4b61997bea6cfa65ed796
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-08-31 14:34:39 -0300

    abrindo PR

--- src/pages/perfil/Perfil.js
+++ src/pages/perfil/Perfil.js
@@ -4,8 +4,8 @@ import Header from '../../components/Header';
 export default class Perfil extends Component {
   render() {
     return (
-      <div>
-        <Header titulo="Perfil" />
+      <div data>
+        <Header title="Perfil" />
       </div>
     );
   }
