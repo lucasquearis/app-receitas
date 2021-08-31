1908672f5965d902056b6cf6783666042877e506
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-08-30 22:10:28 -0300

    implementa renderizaçao das comidas na page foodByOrigin

--- src/pages/FoodByOrigin.jsx
+++ src/pages/FoodByOrigin.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// import { v4 } from 'uuid';
+import { v4 } from 'uuid';
 import Footer from '../components/Footer';
 import HeaderWithSearch from '../components/HeaderWithSearch';
 import { getMeal } from '../redux/actions';
@@ -8,7 +8,6 @@ import { getMeal } from '../redux/actions';
 function FoodByOrigin() {
   const dispatch = useDispatch();
   const { reducerAPI: { loading, meals } } = useSelector((state) => state);
-  console.log(meals);
 
   useEffect(() => {
     dispatch(getMeal());
@@ -18,10 +17,20 @@ function FoodByOrigin() {
     return <h2>Carregando...</h2>;
   }
   return (
-    <div>
+    <>
       <HeaderWithSearch>Explorar Origem</HeaderWithSearch>
+      <ul>
+        {
+          meals.map(({ strMealThumb, strMeal }) => (
+            <li key={ v4() }>
+              <img alt={ strMeal } src={ strMealThumb } />
+              <p>{ strMeal }</p>
+            </li>
+          ))
+        }
+      </ul>
       <Footer />
-    </div>
+    </>
   );
 }
 
