a69480670e68142483db3620e647b30432214c0d
Andre Jaques <andrejaques.b@gmail.com>
2021-09-01 08:22:28 -0300

    fixing some empty calls in FinishRecipeButton component

--- src/components/FinishRecipeButton.jsx
+++ src/components/FinishRecipeButton.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function FinishRecipeButton(props) {
-  const { id, type, enType } = props;
-  const newEnType = enType === 'meals' ? enType : 'cocktails';
+  const { id, type } = props;
   const [done, setDone] = useState(true);
   const [inProgress, setInProgress] = useState(false);
+  console.log(setInProgress, setDone);
 
   return (
     <Link to={ `/${type}/${id}/in-progress` }>
@@ -25,7 +25,6 @@ function FinishRecipeButton(props) {
 FinishRecipeButton.propTypes = {
   type: PropTypes.string,
   id: PropTypes.string,
-  enType: PropTypes.string,
 }.isRequired;
 
 export default FinishRecipeButton;
