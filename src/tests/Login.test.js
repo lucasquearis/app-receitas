2e15f8a1d5605b7816e5d08ec2a18635784a199d
Gabriel Viana <g.viana87@hotmail.com>
2021-09-01 18:36:01 -0300

    Teste Meals

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
-
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
--- src/tests/Meals.test.js
+++ src/tests/Meals.test.js
@@ -1,39 +1,69 @@
-// import React from 'react';
-// import userEvent from '@testing-library/user-event';
-// import { screen, fireEvent } from '@testing-library/dom';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import meals from '../../cypress/mocks/meals';
+import App from '../App';
 
-// import renderWithRouter from '../renderWithRouter';
-// import Meals from '../pages/Meals';
-// import mealMock from './mealMock';
+const INPUT_EMAIL_SELECTOR = 'email-input';
+const INPUT_PASSWORD_SELECTOR = 'password-input';
+const LOGIN_BUTTON_SELECTOR = 'login-submit-btn';
+const VALID_EMAIL = 'alguem@email.com';
+const VALID_PASSWORD = '1234567';
+const PAGE_TITLE_SELECTOR = 'page-title';
+const FOOTER_SELECTOR = 'footer';
+const TWELVE = 12;
+const ALL_CATEGORIY = 'All-category-filter';
+// const BEEF_CATEGORY = 'Beef-category-filter';
+// const BREAKFAST_CATEGORY = 'Breakfast-category-filter';
+// const CHICKEN_CATEGORY = 'Chicken-category-filter';
+// const DESSERT_CATEGORY = 'Dessert-category-filter';
+// const GOAT_CATEGORY = 'Goat-category-filter';
 
-// describe('Testa tela Meals', () => {
-//   beforeAll(mealMock);
+const mockFetchMeals = async () => ({
+  json: async () => meals,
+});
 
-//   it('Verifica se existem 12 cards de comida na tela', () => {
-//     renderWithRouter(<Meals />);
+describe('Teste da page Comidas', () => {
+  it('Testa se renderiza 12 receitas', async () => {
+    renderWithRouter(<App />);
 
-//     const mokeMeal = {
-//       idMeal: '52977',
-//       strMeal: 'Corba',
-//       strMealThumb: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
-//     };
+    userEvent.type(screen.getByTestId(INPUT_EMAIL_SELECTOR), VALID_EMAIL);
+    userEvent.type(screen.getByTestId(INPUT_PASSWORD_SELECTOR), VALID_PASSWORD);
+    userEvent.click(screen.getByTestId(LOGIN_BUTTON_SELECTOR));
 
-//     const mealCardName = screen.getByText(mokeMeal.strMeal);
-//     const mealCardThumb = screen.getByText(mokeMeal.strMealThumb);
+    global.fetch = jest.fn(mockFetchMeals);
+    expect(await screen.findAllByRole('listitem')).toHaveLength(TWELVE);
+  });
 
-//     const DOZE = 12;
-//     for (let index = 0; index < DOZE; index += 1) {
-//       const mealCard = screen.findByTestId(`${index}-recipe-card`);
-//       expect(mealCard).toBeInTheDocument();
-//     }
+  it('Testa se existem seis botões de filtro', () => {
+    renderWithRouter(<App />);
 
-//     expect(mealCardName).toBeInTheDocument();
-//     expect(mealCardThumb).toBeInTheDocument();
-//   });
+    userEvent.type(screen.getByTestId(INPUT_EMAIL_SELECTOR), VALID_EMAIL);
+    userEvent.type(screen.getByTestId(INPUT_PASSWORD_SELECTOR), VALID_PASSWORD);
+    userEvent.click(screen.getByTestId(LOGIN_BUTTON_SELECTOR));
 
-//   it('Verifica se existem 5 botões na tela', async () => {
-//     renderWithRouter(<Meals />);
-//     const button = await screen.findByRole('button', { name: 'Beef' });
-//     expect(button).toBeInTheDocument();
-//   });
-// });
+    expect(screen.getByTestId(ALL_CATEGORIY)).toBeInTheDocument();
+    // expect(screen.getByTestId(BREAKFAST_CATEGORY)).toBeInTheDocument();
+  });
+
+  it('Testa se renderiza Header', () => {
+    renderWithRouter(<App />);
+
+    userEvent.type(screen.getByTestId(INPUT_EMAIL_SELECTOR), VALID_EMAIL);
+    userEvent.type(screen.getByTestId(INPUT_PASSWORD_SELECTOR), VALID_PASSWORD);
+    userEvent.click(screen.getByTestId(LOGIN_BUTTON_SELECTOR));
+
+    expect(screen.getByTestId(PAGE_TITLE_SELECTOR)).toBeInTheDocument();
+  });
+
+  it('Testa se renderiza Footer', () => {
+    renderWithRouter(<App />);
+
+    userEvent.type(screen.getByTestId(INPUT_EMAIL_SELECTOR), VALID_EMAIL);
+    userEvent.type(screen.getByTestId(INPUT_PASSWORD_SELECTOR), VALID_PASSWORD);
+    userEvent.click(screen.getByTestId(LOGIN_BUTTON_SELECTOR));
+
+    expect(screen.getByTestId(FOOTER_SELECTOR)).toBeInTheDocument();
+  });
+});
