4d8faeed21a0725844d952b3eab2598d4bec2525
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-01 14:56:42 -0300

    refatora rotas

--- src/App.js
+++ src/App.js
@@ -27,17 +27,17 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/comidas/:id/in-progress" component={ RecipeInProgress } />
-        <Route path="/bebidas/:id/in-progress" component={ DrinkInProgress } />
         <Route path="/explorar/comidas/ingredientes" component={ FoodIngredients } />
-        <Route path="/explorar/bebidas/ingredientes" component={ DrinksIngredients } />
         <Route path="/explorar/comidas/area" component={ SearchByOrigin } />
         <Route path="/comidas/:id" component={ FoodDetails } />
-        <Route path="/bebidas/:id" component={ DrinkDetails } />
         <Route path="/explorar/comidas" component={ SearchDetailsFood } />
-        <Route path="/explorar/bebidas" component={ SearchDetailsDrinks } />
-        <Route exact path="/explorar" component={ Search } />
         <Route exact path="/comidas" component={ Recipes } />
+        <Route path="/bebidas/:id/in-progress" component={ DrinkInProgress } />
+        <Route path="/explorar/bebidas/ingredientes" component={ DrinksIngredients } />
+        <Route path="/bebidas/:id" component={ DrinkDetails } />
+        <Route path="/explorar/bebidas" component={ SearchDetailsDrinks } />
         <Route exact path="/bebidas" component={ Drinks } />
+        <Route exact path="/explorar" component={ Search } />
         <Route path="/receitas-feitas" component={ FinishedRecipes } />
         <Route path="/perfil" component={ Profile } />
         <Route path="/receitas-favoritas" component={ FavoriteRecipes } />
