d1ab8a14eea22be6560a6f7a4fa17236aa451bd4
Kevin Oliveira <kevin.zero@hotmail.com>
2021-09-01 20:35:01 -0300

    Finaliza estilizacao pagina explorar

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import RestaurantIcon from '@material-ui/icons/Restaurant';
+import LocalBarIcon from '@material-ui/icons/LocalBar';
 import { Header, Footer } from '../components';
+import { ExploreContent, ExploreBtn } from '../UI globalStyles';
 
 function Explore() {
   return (
     <div>
       <Header title="Explorar" />
-      <Link data-testid="explore-food" to="/explorar/comidas">Explorar Comidas</Link>
-      <Link data-testid="explore-drinks" to="/explorar/bebidas">Explorar Bebidas</Link>
+      <ExploreContent>
+        <ExploreBtn>
+          <Link data-testid="explore-food" to="/explorar/comidas">
+            <RestaurantIcon />
+            <p>Explorar Comidas</p>
+          </Link>
+        </ExploreBtn>
+        <ExploreBtn>
+          <Link data-testid="explore-drinks" to="/explorar/bebidas">
+            <LocalBarIcon />
+            <p>Explorar Bebidas</p>
+          </Link>
+        </ExploreBtn>
+      </ExploreContent>
       <Footer />
     </div>
   );
