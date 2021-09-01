f0fc6fbbed9e5880fd135c2325964ca3b490c35c
Pedro <peg.gep27@gmail.com>
2021-09-01 18:40:22 -0300

    Estiliza pagina de perfil

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import FooterMenu from '../../components/FooterMenu/FooterMenu';
 import HeaderWithoutSearch from '../../components/header/HeaderWithoutSearch';
+import './Profile.css';
 
 const Profile = () => {
   const [redirect, setRedirect] = useState({
@@ -18,44 +19,49 @@ const Profile = () => {
   return (
     <div>
       <HeaderWithoutSearch>Perfil</HeaderWithoutSearch>
-      <p data-testid="profile-email">{ email }</p>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-        onClick={ () => {
-          setRedirect({
-            bool: true,
-            path: '/receitas-feitas',
-          });
-        } }
-      >
-        Receitas Feitas
-      </button>
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-        onClick={ () => {
-          setRedirect({
-            bool: true,
-            path: '/receitas-favoritas',
-          });
-        } }
-      >
-        Receitas Favoritas
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ () => {
-          localStorage.clear();
-          setRedirect({
-            bool: true,
-            path: '/',
-          });
-        } }
-      >
-        Sair
-      </button>
+      <div className="div-profile-buttons">
+        <p data-testid="profile-email">{ email }</p>
+        <button
+          className="profile-button"
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => {
+            setRedirect({
+              bool: true,
+              path: '/receitas-feitas',
+            });
+          } }
+        >
+          Receitas Feitas
+        </button>
+        <button
+          className="profile-button"
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => {
+            setRedirect({
+              bool: true,
+              path: '/receitas-favoritas',
+            });
+          } }
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          className="profile-button"
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => {
+            localStorage.clear();
+            setRedirect({
+              bool: true,
+              path: '/',
+            });
+          } }
+        >
+          Sair
+        </button>
+      </div>
       <FooterMenu />
     </div>
   );
