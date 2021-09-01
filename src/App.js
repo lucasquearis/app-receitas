3519f1bf8207490e0b2fc9d6078614c6079cda67
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-09-01 18:16:57 -0300

    Finaliza testes da tela Explorar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Provider from './context/Provider';
 import { Login, Drinks, Meals, MealDetails, DrinkDetails, MealInProgress,
   DrinkInProgress, Explore, ExploreMeals, ExploreDrinks, ExploreMealByIngredients,
@@ -11,42 +11,40 @@ import { Login, Drinks, Meals, MealDetails, DrinkDetails, MealInProgress,
 function App() {
   return (
     <Provider>
-      <BrowserRouter>
-        <Switch>
-          <Route
-            path="/comidas/:id/in-progress"
-            render={ (props) => <MealInProgress { ...props } /> }
-          />
-          <Route
-            path="/bebidas/:id/in-progress"
-            render={ (props) => <DrinkInProgress { ...props } /> }
-          />
-          <Route path="/comidas/:id" render={ (props) => <MealDetails { ...props } /> } />
-          <Route
-            path="/bebidas/:id"
-            render={ (props) => <DrinkDetails { ...props } /> }
-          />
-          <Route
-            path="/explorar/comidas/ingredientes"
-            component={ ExploreMealByIngredients }
-          />
-          <Route
-            path="/explorar/bebidas/ingredientes"
-            component={ ExploreDrinkByIngredients }
-          />
-          <Route path="/explorar/bebidas/area" component={ NotFound } />
-          <Route path="/explorar/comidas/area" component={ ExploreMealByArea } />
-          <Route path="/explorar/comidas" component={ ExploreMeals } />
-          <Route path="/explorar/bebidas" component={ ExploreDrinks } />
-          <Route path="/receitas-feitas" component={ RecipesDone } />
-          <Route path="/receitas-favoritas" component={ RecipesFavorites } />
-          <Route path="/perfil" component={ Profile } />
-          <Route path="/comidas" component={ Meals } />
-          <Route path="/bebidas" component={ Drinks } />
-          <Route path="/explorar" component={ Explore } />
-          <Route exact path="/" component={ Login } />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route
+          path="/comidas/:id/in-progress"
+          render={ (props) => <MealInProgress { ...props } /> }
+        />
+        <Route
+          path="/bebidas/:id/in-progress"
+          render={ (props) => <DrinkInProgress { ...props } /> }
+        />
+        <Route path="/comidas/:id" render={ (props) => <MealDetails { ...props } /> } />
+        <Route
+          path="/bebidas/:id"
+          render={ (props) => <DrinkDetails { ...props } /> }
+        />
+        <Route
+          path="/explorar/comidas/ingredientes"
+          component={ ExploreMealByIngredients }
+        />
+        <Route
+          path="/explorar/bebidas/ingredientes"
+          component={ ExploreDrinkByIngredients }
+        />
+        <Route path="/explorar/bebidas/area" component={ NotFound } />
+        <Route path="/explorar/comidas/area" component={ ExploreMealByArea } />
+        <Route path="/explorar/comidas" component={ ExploreMeals } />
+        <Route path="/explorar/bebidas" component={ ExploreDrinks } />
+        <Route path="/receitas-feitas" component={ RecipesDone } />
+        <Route path="/receitas-favoritas" component={ RecipesFavorites } />
+        <Route path="/perfil" component={ Profile } />
+        <Route path="/comidas" component={ Meals } />
+        <Route path="/bebidas" component={ Drinks } />
+        <Route path="/explorar" component={ Explore } />
+        <Route exact path="/" component={ Login } />
+      </Switch>
     </Provider>
   );
 }
---
+++ src/tests/Explore.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Verifica a tela Explore', () => {
+  it('Verifica se existem 2 botões na tela', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/explorar');
+
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(2);
+  });
+
+  it('Verifica os botões Explorar Comidas/Bebidas', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/explorar');
+
+    const buttonMeals = screen.getByRole('button', { name: /Explorar Comidas/i });
+    expect(buttonMeals).toBeInTheDocument();
+
+    const buttonDrinks = screen.getByRole('button', { name: /Explorar Bebidas/i });
+    expect(buttonDrinks).toBeInTheDocument();
+  });
+
+  it('Verifica rota do botão Explorar Comidas', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/explorar');
+
+    const button = screen.getByRole('button', { name: /Explorar Comidas/i });
+    userEvent.click(button);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/explorar/comidas');
+  });
+
+  it('Verifica rota do botão Explorar Bebidas', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/explorar');
+
+    const button = screen.getByRole('button', { name: /Explorar Bebidas/i });
+    userEvent.click(button);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/explorar/bebidas');
+  });
+});
