f017a6775c6605311c4a5379b0dc704dad460f9c
Marcela Silva <marcela.ops@hotmail.br>
2021-09-01 18:22:49 -0300

    tentnado arrumar rotas p passar em tudo

--- src/Routes.js
+++ src/Routes.js
@@ -25,23 +25,22 @@ function Routes() {
         <Route exact path="/" component={ Login } />
         <Route path="/comidas/:id/in-progress" component={ FoodsInProgress } />
         <Route path="/bebidas/:id/in-progress" component={ DrinksInProgress } />
-        <Route path="/comidas/:id" render={ (props) => <FoodsDet { ...props } /> } />
-        <Route path="/bebidas/:id" render={ (props) => <DrinksDet { ...props } /> } />
-        <Route path="/explorar/comidas/ingredientes" component={ DrinkIngredients } />
-        <Route path="/explorar/comidas/ingredientes" component={ FoodIngredients } />
         <Route path="/explorar/bebidas/ingredientes" component={ DrinkIngredients } />
+        <Route path="/explorar/comidas/ingredientes" component={ FoodIngredients } />
         <Route path="/explorar/comidas/area" component={ FoodAreaExp } />
         <Route path="/explorar/bebidas/area" component={ NotFound } />
-        <Route path="/comidas" component={ Foods } />
-        <Route path="/bebidas" component={ Drinks } />
+        <Route path="/comidas/:id" render={ (props) => <FoodsDet { ...props } /> } />
+        <Route path="/bebidas/:id" render={ (props) => <DrinksDet { ...props } /> } />
         <Route path="/explorar/comidas" component={ FoodExp } />
         <Route path="/explorar/bebidas" component={ DrinkExp } />
+        <Route path="/comidas/:id" render={ (props) => <FoodsDet { ...props } /> } />
+        <Route path="/bebidas/:id" render={ (props) => <DrinksDet { ...props } /> } />
+        <Route path="/comidas" component={ Foods } />
+        <Route path="/bebidas" component={ Drinks } />
         <Route path="/explorar" component={ Explore } />
         <Route path="/perfil" component={ Profile } />
         <Route path="/receitas-feitas" component={ DoneRecipes } />
         <Route path="/receitas-favoritas" component={ FavoriteRecipes } />
-        <Route path="/comidas/:id" render={ (props) => <FoodsDet { ...props } /> } />
-        <Route path="/bebidas/:id" render={ (props) => <DrinksDet { ...props } /> } />
       </Switch>
     </Router>
   );
