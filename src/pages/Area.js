6bce5ee41c5d44e73316afb9089352051f7723e2
Armando <a.guerra58@hotmail.com>
2021-08-31 21:15:06 -0300

    Requisitos 78 80 81 part 79

--- src/pages/Area.js
+++ src/pages/Area.js
@@ -1,12 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { fetchByArea, fetchFoods, fetchFilterByArea } from '../services/mealAPI';
 
 function Area() {
+  const LOCATION = useLocation().pathname;
+  const MAX = 12;
+  const [area, SetArea] = useState([]);
+  const [receitas, setReceitas] = useState();
+  const [areaSelected, setAreaSelected] = useState({
+    selected: 'All',
+  });
+  useEffect(() => {
+    const GetAreaOrigin = async () => {
+      const list = await fetchByArea();
+      const { meals } = list;
+      const all = { strArea: 'All' };
+      SetArea([all, ...meals]);
+      const foods = await fetchFoods();
+      setReceitas([...foods.meals]);
+    };
+    GetAreaOrigin();
+  }, []);
+
+  useEffect(() => {
+    const GetRecipes = async () => {
+      if (areaSelected.selected === 'All') {
+        const foods = await fetchFoods();
+        setReceitas([...foods.meals]);
+      } else {
+        const list = await fetchFilterByArea(areaSelected.selected);
+        const { meals } = list;
+        setReceitas([...meals]);
+      }
+    };
+    GetRecipes();
+  }, [areaSelected]);
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setAreaSelected({
+      selected: value,
+    });
+  }
+
+  if (!area || !receitas) return <p>Loading...</p>;
+  if (LOCATION === '/explorar/bebidas/area') return <p>Not Found</p>;
   return (
     <div>
       <Header title="Explorar Origem" />
-      <h1>Area</h1>
+      <select data-testid="explore-by-area-dropdown" onChange={ handleChange }>
+        {
+          area.map(({ strArea }, index) => (
+            <option
+              data-testid={ `${strArea}-option` }
+              value={ strArea }
+              key={ index }
+            >
+              { strArea }
+            </option>
+          ))
+        }
+      </select>
+      {
+        receitas && receitas.slice(0, MAX).map((recipe, index) => (
+          <div data-testid={ `${index}-recipe-card` } key={ index }>
+            <Link to={ `/comidas/${recipe.idMeal}` }>
+              <img
+                data-testid={ `${index}-card-img` }
+                src={ recipe.strMealThumb }
+                alt="thumbnail recipe"
+                width="100"
+              />
+              <p data-testid={ `${index}-card-name` }>
+                { recipe.strMeal }
+              </p>
+            </Link>
+          </div>
+        ))
+      }
       <Footer />
     </div>
   );
