5e6bba46dae873a94a28fa4ab153c072769067e4
Eric Kreis <erickreis793@gmail.com>
2021-09-02 15:07:51 -0300

    [ Requisito 77 ]

--- src/pages/ExploreByIngredients/index.js
+++ src/pages/ExploreByIngredients/index.js
@@ -1,41 +1,53 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useDataContext } from '../../context/DataProvider';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
+import { getFilters } from '../../services';
+
 export default function ExploreByIngredients() {
   const history = useHistory();
-  const { ingredients, loading } = useDataContext();
+  const { ingredients, loading, setLoading, setData } = useDataContext();
 
   const types = history.location.pathname.includes('/comidas')
-    ? { type: 'food', url: '/comidas' }
-    : { type: 'drinks', url: '/bebidas' };
+    ? { type: 'food', response: 'meals', url: '/comidas' }
+    : { type: 'drinks', response: 'drinks', url: '/bebidas' };
+
+  const handleSendFilter = async (name) => {
+    setLoading(true);
+    const recipes = await getFilters(types.type, { filter: 'ingredient', text: name });
+    setData((prevData) => ({ ...prevData, [types.type]: recipes[types.response] }));
+    setLoading(false);
+    history.push(`${types.url}`);
+  };
 
   // Cria os cards de ingredientes;
   const maxLength = 12;
   const ingredientsCards = () => ingredients[types.type].slice(0, maxLength)
     .map((ingredient, index) => (
-      <section
+      <button
+        type="button"
         data-testid={ `${index}-ingredient-card` }
         key={ index }
+        onClick={ () => (
+          handleSendFilter(ingredient.strIngredient || ingredient.strIngredient1)
+        ) }
       >
-        <Link to={ `${types.url}` }>
-          <img
-            data-testid={ `${index}-card-img` }
-            src={
-              types.type === 'food'
-                ? `https://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-Small.png`
-                : `https://www.thecocktaildb.com/images/ingredients/${ingredient.strIngredient1}-Small.png`
-            }
-            alt={ ingredient.strIngredient || ingredient.strIngredient1 }
-          />
-          <h4 data-testid={ `${index}-card-name` }>
-            { ingredient.strIngredient || ingredient.strIngredient1 }
-          </h4>
-        </Link>
-      </section>
+        <img
+          data-testid={ `${index}-card-img` }
+          src={
+            types.type === 'food'
+              ? `https://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-Small.png`
+              : `https://www.thecocktaildb.com/images/ingredients/${ingredient.strIngredient1}-Small.png`
+          }
+          alt={ ingredient.strIngredient || ingredient.strIngredient1 }
+        />
+        <h4 data-testid={ `${index}-card-name` }>
+          { ingredient.strIngredient || ingredient.strIngredient1 }
+        </h4>
+      </button>
     ));
 
   return (
