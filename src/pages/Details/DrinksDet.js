aebef2e2ffe9c87178f8540d1e994cf25db36fea
MIkaela Braga <bragamikaela@gmail.com>
2021-09-02 18:11:54 -0300

    tenta resolver algo

--- src/pages/Details/DrinksDet.js
+++ src/pages/Details/DrinksDet.js
@@ -38,27 +38,25 @@ function DrinksDetails() {
   }, [id]);
 
   useEffect(() => {
-    if (recipesDrink !== undefined) {
-      const getIngredientsAndMeasures = () => {
-        const key = Object.keys(recipesDrink[0])
-          .filter((item) => item.includes('strIngredient'));
-        const ingredientNotEmpty = key
-          .filter((item) => (
-            recipesDrink[0][item] !== '' && recipesDrink[0][item] !== null));
-        const ingredientsList = ingredientNotEmpty
-          .map((keyDrink) => recipesDrink[0][keyDrink]);
-        setIngredients(ingredientsList);
+    const getIngredientsAndMeasures = () => {
+      const key = Object.keys(recipesDrink[0])
+        .filter((item) => item.includes('strIngredient'));
+      const ingredientNotEmpty = key
+        .filter((item) => (
+          recipesDrink[0][item] !== '' && recipesDrink[0][item] !== null));
+      const ingredientsList = ingredientNotEmpty
+        .map((keyDrink) => recipesDrink[0][keyDrink]);
+      setIngredients(ingredientsList);
 
-        const keyMeasure = Object.keys(recipesDrink[0])
-          .filter((item) => item.includes('strMeasure'));
-        const measureNoEmpty = keyMeasure
-          .filter((item) => (
-            recipesDrink[0][item] !== '' && recipesDrink[0][item] !== null));
-        const measureList = measureNoEmpty.map((kMeasure) => recipesDrink[0][kMeasure]);
-        setMeasure(measureList);
-      };
-      getIngredientsAndMeasures();
-    }
+      const keyMeasure = Object.keys(recipesDrink[0])
+        .filter((item) => item.includes('strMeasure'));
+      const measureNoEmpty = keyMeasure
+        .filter((item) => (
+          recipesDrink[0][item] !== '' && recipesDrink[0][item] !== null));
+      const measureList = measureNoEmpty.map((kMeasure) => recipesDrink[0][kMeasure]);
+      setMeasure(measureList);
+    };
+    getIngredientsAndMeasures();
   }, [recipesDrink]);
 
   useEffect(() => {
