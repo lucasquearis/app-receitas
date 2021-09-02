aa9777b258973c08abbc8540c580bc073a050d21
thiagopcdev <thiagopc_dev@hotmail.com>
2021-09-02 15:21:06 -0300

    Corrige uns problemas

--- src/pages/DrinkExplore/DrinkExplore.js
+++ src/pages/DrinkExplore/DrinkExplore.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import ExploreDrink from '../../components/ExploreDrink';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer/Footer';
+import './DrinkExplore.css';
 
 function DrinkExplore() {
   return (
-    <div>
+    <div className="drink-explore-container">
       <Header title="Explorar Bebidas" />
       <ExploreDrink />
       <Footer />
