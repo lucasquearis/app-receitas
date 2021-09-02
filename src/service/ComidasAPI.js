b385c77a1c9634fdeb5f6ca3abd42ffdd4e11114
Luciano Lanes <llaneslopes@gmail.com>
2021-09-02 18:15:32 -0300

    Alterado arquivo ComidasAPI

--- src/service/ComidasAPI.js
+++ src/service/ComidasAPI.js
@@ -1,20 +1,17 @@
 export async function buscarCategorias() {
   const ENDPOINT = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
-
   const response = await fetch(ENDPOINT).then((data) => data.json());
   return response.meals;
 }
 
 export async function buscarTodasComidasPorLetra(letra) {
   const ENDPOINT = `https://www.themealdb.com/api/json/v1/1/search.php?f=${letra}`;
-
   const response = await fetch(ENDPOINT).then((data) => data.json());
   return response.meals;
 }
 
 export async function buscarComidaPorNome(nome) {
   const ENDPOINT = `https://www.themealdb.com/api/json/v1/1/search.php?s=${nome}`;
-
   const response = await fetch(ENDPOINT).then((data) => data.json());
   return response.meals;
 }
