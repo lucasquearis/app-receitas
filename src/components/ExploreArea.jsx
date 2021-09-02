bdae1d24d616ccfe49a31b886d0610f3b37cb412
Patrick Dack <patrickdack89@gmail.com>
2021-09-02 02:08:38 -0300

    Estiliza pagina de Explorar por Origem

--- src/components/ExploreArea.jsx
+++ src/components/ExploreArea.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Form } from 'react-bootstrap';
+import './exploreArea.css';
 
 function ExploreArea({ data, onChange }) {
   return (
-    <select data-testid="explore-by-area-dropdown" onChange={ onChange }>
+    <select
+      data-testid="explore-by-area-dropdown"
+      onChange={ onChange }
+      className="dropdown"
+      aria-label="Default select example"
+    >
       <option data-testid="All-option" value="">All</option>
       {
         data.map((area, index) => (
