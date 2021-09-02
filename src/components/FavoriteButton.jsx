76eaf01272253c8fe35be97585fe455bad5ad6f6
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-09-01 21:54:13 -0300

    inicia botao favoritos

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -3,12 +3,30 @@ import React, { useState } from 'react';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 function FavoriteButton() {
+  // const { localStorageItems, setLocalStorageItems } = useContext(MyContext);
   const [heart] = useState(whiteHeartIcon);
+
+  const saveLocalStorage = () => {
+    const recipes = {
+      id,
+      type: 'comidas',
+      area: getRecipe.strArea,
+      category: getRecipe.strCategory,
+      alcoholicOrNot: '',
+      name: getRecipe.strMeal,
+      image: getRecipe.strMealThumb,
+    };
+    localStorage.setItem('favoriteRecipes', JSON.stringify(recipes));
+  };
+  // setLocalStorageItems(...localStorageItems, recipes);
+
+  // return localStorage.setItem('doneRecipes', JSON.stringify([recipes]));
 
   return (
     <button
       type="button"
       data-testid="favorite-btn"
+      onClick={ saveLocalStorage }
     >
       <img src={ heart } alt="Favorite" data-testid="favorite-btn" />
     </button>
