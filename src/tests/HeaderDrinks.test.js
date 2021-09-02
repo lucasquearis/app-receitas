20c174591dc3157bfd124dc88dcd1250a8449208
Luiz Henrique <luishenriqueee2010@hotmail.com>
2021-09-02 19:33:33 -0300

    Adiciona 95% de cobertura de testes no headerDrinks e headerMeals

---
+++ src/tests/HeaderDrinks.test.js
@@ -0,0 +1,164 @@
+import React from 'react';
+import { screen, cleanup } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from './helpers/renderWithRouter';
+import { HeaderDrinks } from '../components';
+import { bananaSearch, emptySearch } from './mocks/bananaSearch';
+import userEvent from '@testing-library/user-event';
+
+const fetchMock = () => {
+  jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
+    status: 200,
+    ok: true,
+    json: () => Promise.resolve(bananaSearch),
+  }));
+};
+
+const fetchEmptyMock = () => {
+  jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
+    status: 200,
+    ok: true,
+    json: () => Promise.resolve(emptySearch),
+  }));
+};
+
+
+describe('Testa a página de detalhes das receitas', () => {
+  beforeEach(cleanup); 
+
+  fetchMock()
+  it('Verifica se a página contém os elementos esperados', async () => {
+    const promise = Promise.resolve();
+
+    await act(async () => {
+      renderWithRouter(<HeaderDrinks />);
+      const profileBtn = screen.getByTestId('profile-top-btn');
+      const title = screen.getByTestId('page-title');
+      const searchBtn = screen.getByTestId('search-top-btn');
+  
+      expect(profileBtn).toBeInTheDocument();
+      expect(title).toBeInTheDocument();
+      expect(searchBtn).toBeInTheDocument();
+      await act(() => promise);
+    });
+  });
+
+  fetchMock()
+  it('Faz pesquisa filtrando por ingredientes', async () => {
+    const promise = Promise.resolve();
+
+    await act(async () => {
+      renderWithRouter(<HeaderDrinks />);
+    });
+    const activeSearchBtn = screen.getByTestId('search-top-btn');
+
+    userEvent.click(activeSearchBtn);
+    const ingredients = screen.getByTestId('ingredient-search-radio');
+    const searchInput = screen.getByTestId('search-input');
+    const searchBtn = screen.getByTestId('exec-search-btn');
+
+    userEvent.click(ingredients);
+    userEvent.type(searchInput, 'chicken')
+    userEvent.click(searchBtn);
+    await act(() => promise);
+  });
+
+  fetchMock()
+  it('Faz pesquisa filtrando por nome', async () => {
+    const promise = Promise.resolve();
+    await act(async () => {
+      renderWithRouter(<HeaderDrinks />);
+    });
+    const activeSearchBtn = screen.getByTestId('search-top-btn');
+
+    userEvent.click(activeSearchBtn);
+    const name = screen.getByTestId('name-search-radio');
+    const searchInput = screen.getByTestId('search-input');
+    const searchBtn = screen.getByTestId('exec-search-btn');
+
+    userEvent.click(name);
+    userEvent.type(searchInput, 'Brown Stew Chicken')
+    userEvent.click(searchBtn);
+    await act(() => promise);
+  });
+
+  fetchMock()
+  it('Faz pesquisa filtrando por primeira letra', async () => {
+    const promise = Promise.resolve();
+
+    await act(async () => {
+      renderWithRouter(<HeaderDrinks />);
+    });
+    const activeSearchBtn = screen.getByTestId('search-top-btn');
+
+    userEvent.click(activeSearchBtn);
+    const firstLetter = screen.getByTestId('first-letter-search-radio');
+    const searchInput = screen.getByTestId('search-input');
+    const searchBtn = screen.getByTestId('exec-search-btn');
+
+    userEvent.click(firstLetter);
+    userEvent.type(searchInput, 'a')
+    userEvent.click(searchBtn);
+    await act(() => promise);
+
+  });
+
+  fetchMock()
+  it('Faz pesquisa sem filtro', async () => {
+    const promise = Promise.resolve();
+    await act(async () => {
+      renderWithRouter(<HeaderDrinks />);
+    });
+    const activeSearchBtn = screen.getByTestId('search-top-btn');
+
+    userEvent.click(activeSearchBtn);
+    const searchBtn = screen.getByTestId('exec-search-btn');
+    userEvent.click(searchBtn);
+    await act(() => promise);
+
+
+  });
+
+  fetchMock()
+  it('Testa se aparece um alert quando a pesquisa por uma letra tem mais de uma letra', async () => {
+    const promise = Promise.resolve();
+    await act(async () => {
+      renderWithRouter(<HeaderDrinks />);
+    });
+    const activeSearchBtn = screen.getByTestId('search-top-btn');
+
+    userEvent.click(activeSearchBtn);
+    const firstLetter = screen.getByTestId('first-letter-search-radio');
+    const searchInput = screen.getByTestId('search-input');
+    const searchBtn = screen.getByTestId('exec-search-btn');
+
+    userEvent.click(firstLetter);
+    userEvent.type(searchInput, 'aaa')
+    userEvent.click(searchBtn);
+    await act(() => promise);
+
+    expect(window.alert)
+  });
+
+  fetchEmptyMock()
+  it('Testa cenario em que a pesquisa nao tem correspondencia', async () => {
+    const promise = Promise.resolve();
+    await act(async () => {
+      renderWithRouter(<HeaderDrinks />);
+    });
+    const activeSearchBtn = screen.getByTestId('search-top-btn');
+
+    userEvent.click(activeSearchBtn);
+    const firstLetter = screen.getByTestId('first-letter-search-radio');
+    const searchInput = screen.getByTestId('search-input');
+    const searchBtn = screen.getByTestId('exec-search-btn');
+
+    userEvent.click(firstLetter);
+    userEvent.type(searchInput, 'ugwadawgb')
+    userEvent.click(searchBtn);
+
+    await act(() => promise);
+
+    expect(window.alert)
+  });
+});
---
+++ src/tests/mocks/bananaSearch.js
@@ -0,0 +1,38 @@
+export const bananaSearch = {
+  drinks: [
+      {
+          strDrink: "Banana Cantaloupe Smoothie",
+          strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/uqxqsy1468876703.jpg",
+          idDrink: "12708"
+      },
+      {
+          strDrink: "Banana Daiquiri",
+          strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/k1xatq1504389300.jpg",
+          idDrink: "11064"
+      },
+      {
+          strDrink: "Banana Strawberry Shake",
+          strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/vqquwx1472720634.jpg",
+          idDrink: "12656"
+      },
+      {
+          strDrink: "Banana Strawberry Shake Daiquiri",
+          strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/uvypss1472720581.jpg",
+          idDrink: "12658"
+      },
+      {
+          strDrink: "Fruit Shake",
+          strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/q0fg2m1484430704.jpg",
+          idDrink: "12674"
+      },
+      {
+          strDrink: "Sweet Bananas",
+          strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/sxpcj71487603345.jpg",
+          idDrink: "12724"
+      }
+  ]
+}
+
+export const emptySearch = {
+  drinks: null
+}
