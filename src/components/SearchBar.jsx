cf4f005caeb80d254cec64663988c1749c56f9fc
Andre Jaques <andrejaques.b@gmail.com>
2021-09-02 20:05:28 -0300

    trying something new

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,9 +14,14 @@ function SearchBar() {
   const { removeDisplayList } = useContext(myContext);
   const dispatch = useDispatch();
   const { meals } = useSelector((state) => state.recipes.foods);
-  const drinks = useSelector((state) => state.recipes.foods.drinks);
+  const { drinks } = useSelector((state) => state.recipes.foods);
+
   const handleChange = ({ target: { value, name } }) => {
-    setSearch({ ...search, [name]: value });
+    if (search) {
+      setSearch({ ...search, [name]: value });
+    } else {
+      setSearch({ [name]: value });
+    }
   };
 
   const requestIngredient = (value) => {
