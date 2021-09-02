801c52698a80eb5168296e5ad237fb9ae11b09b1
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-09-02 18:28:34 -0300

    estilizando pagina de explorar bebidas

--- src/pages/BebidasExplorar.js
+++ src/pages/BebidasExplorar.js
@@ -3,26 +3,29 @@ import { Link } from 'react-router-dom';
 import RecipesContext from '../context/RecipesContext';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../styles/Explore.css';
 
 function BebidasExplorar() {
   const { drinkRandom } = useContext(RecipesContext);
   return (
     <div>
       <Header title="Explorar Bebidas" renderSearch={ false } />
-      <Link to="/explorar/bebidas/ingredientes">
-        <div className="card">
-          <div className="card-body" data-testid="explore-by-ingredient">
-            Por Ingredientes
-          </div>
-        </div>
-      </Link>
-      <Link to={ `/bebidas/${drinkRandom}` }>
-        <div className="card">
-          <div className="card-body" data-testid="explore-surprise">
-            Me Surpreenda!
-          </div>
-        </div>
-      </Link>
+      <div className="d-flex flex-column align-items-center mt-4 w-100">
+        <Link
+          to="/explorar/bebidas/ingredientes"
+          data-testid="explore-by-ingredient"
+          className="btn btn-info explore-button mb-3"
+        >
+          Por Ingredientes
+        </Link>
+        <Link
+          to={ `/bebidas/${drinkRandom}` }
+          data-testid="explore-surprise"
+          className="btn btn-info explore-button mb-3"
+        >
+          Me Surpreenda!
+        </Link>
+      </div>
       <Footer />
     </div>
   );
