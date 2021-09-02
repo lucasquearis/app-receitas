4a0114cac5d8e1442b0b2aaa2cb622a642f0ce61
Marcela Silva <marcela.ops@hotmail.br>
2021-09-01 22:14:19 -0300

    iniciando `FavoriteRecipes`

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React/* , { useState } */ from 'react';
 import Header from '../components/Header';
 
-function FoodAreaExp() {
+function FavoriteRecipes() {
+  const favoritesLocalStorage = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+
+  console.log(favoritesLocalStorage);
+  // const [favorites, setFavorites] = useState(favoritesLocalStorage);
+
   return (
-    <Header title="Receitas Favoritas" search={ false } />
+    <>
+      <Header title="Receitas Favoritas" search={ false } />
+
+      <div>
+        <button data-testid="filter-by-all-btn" type="button">All</button>
+        <button type="button" data-testid="filter-by-food-btn">Food</button>
+        <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      </div>
+
+      <div>
+        { favoritesLocalStorage.map((favorite, index) => (
+          <div key={ favoritesLocalStorage.id }>
+            {/* <Link to={path...}> */}
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ favorite.image }
+              alt={ favorite.name }
+            />
+            {/* </Link> */}
+          </div>
+
+        ))}
+
+      </div>
+    </>
   );
 }
 
-export default FoodAreaExp;
+export default FavoriteRecipes;
