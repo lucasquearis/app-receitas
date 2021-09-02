bcf4ce6eb007a3e76b37f717b8be6afcdc118a0a
walttoncoelho <walltton@outlook.com>
2021-09-02 10:27:59 -0300

    Atualizando Footer Fixed

--- src/pages/explorar/ingredientes/DrinksIngredients.jsx
+++ src/pages/explorar/ingredientes/DrinksIngredients.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../../components/Header';
+import MenuFooter from '../../../components/MenuFooter';
 
 export default function DrinksIngredients() {
   return (
     <div>
       <Header title="Explorar Ingredientes" />
       <p>Explorar Ingredientes de Bebidas</p>
+      <MenuFooter />
     </div>
   );
 }
--- src/pages/explorar/ingredientes/FoodIngredients.jsx
+++ src/pages/explorar/ingredientes/FoodIngredients.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../../components/Header';
+import MenuFooter from '../../../components/MenuFooter';
 
 export default function FoodIngredients() {
   return (
     <div>
       <Header title="Explorar Ingredientes" />
       <p>Explorar Ingredientes de Comidas</p>
+      <MenuFooter />
     </div>
   );
 }
