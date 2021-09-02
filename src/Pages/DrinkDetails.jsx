d9494b4e0ef627454f8569441756adc0513c34c3
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-02 17:41:22 -0300

    Refatora page drinkDetails

--- src/Pages/DrinkDetails.jsx
+++ src/Pages/DrinkDetails.jsx
@@ -1,40 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
-import Carousel from 'react-bootstrap/Carousel';
+import { useDispatch } from 'react-redux';
 import Loading from '../Components/Loading';
 import RecipeHeader from '../Components/RecipeHeader';
+import RenderRecommendations from '../Components/RenderRecommendations';
 import IngredientsAndMeasures from '../Components/IngredientsAndMeasures';
-import { createRecomendationsFood } from '../helper/renderRecomendations';
 import * as required from '../helper/requiredDetails';
+import setRecipeDetails from '../Redux/actions/actionSetRecipeDetails';
 
 function DrinkDetails() {
-  const [recipe, setRecipe] = useState({});
-  const [recomendation, setRecomendation] = useState([]);
+  const [recipe, setRecipe] = useState([]);
+  const [recommendation, setRecommendation] = useState([]);
   const [doneRecipe, setDoneRecipe] = useState(true);
   const [progressRecipe, setProgressRecipe] = useState(false);
+  const dispatch = useDispatch();
+
   const { id } = useParams();
   const { push } = useHistory();
 
   const { consultDrink,
     getMealsRecommendations,
     verificationDoneRecipe,
-    verificationProgressRecipe } = required;
+    verificatioinProgressRecipe } = required;
 
   useEffect(() => {
     setRecipe(consultDrink(id));
-    setRecomendation(getMealsRecommendations);
+    setRecommendation(getMealsRecommendations());
     setDoneRecipe(verificationDoneRecipe(id));
-    setProgressRecipe(verificationProgressRecipe(id, 'cocktails'));
+    setProgressRecipe(verificatioinProgressRecipe(id));
   }, [id, consultDrink,
     getMealsRecommendations,
     verificationDoneRecipe,
-    verificationProgressRecipe, recipe]);
+    verificatioinProgressRecipe]);
 
   const handleRedirect = () => {
-    push({ pathname: `/bebidas/${id}/in-progress`,
-      search: '?query=abc',
-      state: recipe });
+    dispatch(setRecipeDetails(recipe));
+    push(`/bebidas/${id}/in-progress`);
   };
+
+  console.log(recommendation);
 
   if (recipe.length === 0) {
     return <Loading />;
@@ -42,10 +46,11 @@ function DrinkDetails() {
   return (
     <section>
       <RecipeHeader
-        thumb={ recipe.strMealThumb }
-        title={ recipe.strMeal }
-        category={ recipe.strCategory }
+        thumb={ recipe.strDrinkThumb }
+        title={ recipe.strDrinkThumb }
+        category={ recipe.strAlcoholic }
         recipe={ recipe }
+        type="bebida"
       />
       <IngredientsAndMeasures
         recipe={ recipe }
@@ -53,9 +58,12 @@ function DrinkDetails() {
       <div>
         <p data-testid="instructions">{recipe.strInstructions}</p>
       </div>
-      <Carousel>
-        {recomendation.map((item, index) => createRecomendationsFood(item, index))}
-      </Carousel>
+      <div>
+        <RenderRecommendations
+          recommendation={ recommendation }
+          key
+        />
+      </div>
       <div>
         {!doneRecipe && (
           <button
