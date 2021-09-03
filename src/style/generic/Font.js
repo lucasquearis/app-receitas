68feb2fcb5c3e2f770128a0b4d171f6a85fedbe0
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-03 02:18:16 -0300

    Corrigindo problemas com a fonte

--- src/style/generic/Font.js
+++ src/style/generic/Font.js
@@ -1,12 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
-import epilogue from '../../fonts/Epilogue/Epilogue-VariableFont_wght.ttf';
 
-const Font = createGlobalStyle`  @font-face {
-    font-family: Epilogue;
-    src: url(${epilogue}) format('truetype');
-  }
-
-  body {
+const Font = createGlobalStyle`  body {
     font-family: Epilogue , sans-serif;
   }
 `;
