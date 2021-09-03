d4076be5aa320869d4dcb90ac9acb0c6921af850
marcelaops <81485596+marcelaops@users.noreply.github.com>
2021-09-03 16:54:47 -0300

    Revert "[ Main group 15 ] - Teste Evaluator (#962)"
    This reverts commit 530ed55ad64a6c965daffcdd3145329fb950d32b.

--- src/Routes.js
+++ src/Routes.js
@@ -23,38 +23,22 @@ function Routes() {
     <Router>
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route exact path="/bebidas/:id/in-progress" component={ DrinksInProgress } />
-        <Route exact path="/comidas/:id/in-progress" component={ FoodsInProgress } />
-        <Route
-          exact
-          path="/explorar/bebidas/ingredientes"
-          component={ DrinkIngredients }
-        />
-        <Route
-          exact
-          path="/explorar/comidas/ingredientes"
-          component={ FoodIngredients }
-        />
-        <Route exact path="/explorar/comidas/area" component={ FoodAreaExp } />
-        <Route exact path="/explorar/bebidas/area" component={ NotFound } />
-        <Route
-          exact
-          path="/comidas/:id"
-          render={ (props) => <FoodsDet { ...props } /> }
-        />
-        <Route
-          exact
-          path="/bebidas/:id"
-          render={ (props) => <DrinksDet { ...props } /> }
-        />
-        <Route exact path="/explorar/comidas" component={ FoodExp } />
-        <Route exact path="/explorar/bebidas" component={ DrinkExp } />
-        <Route exact path="/comidas" component={ Foods } />
-        <Route exact path="/bebidas" component={ Drinks } />
-        <Route exact path="/explorar" component={ Explore } />
-        <Route exact path="/perfil" component={ Profile } />
-        <Route exact path="/receitas-feitas" component={ DoneRecipes } />
-        <Route exact path="/receitas-favoritas" component={ FavoriteRecipes } />
+        <Route path="/bebidas/:id/in-progress" component={ DrinksInProgress } />
+        <Route path="/comidas/:id/in-progress" component={ FoodsInProgress } />
+        <Route path="/explorar/bebidas/ingredientes" component={ DrinkIngredients } />
+        <Route path="/explorar/comidas/ingredientes" component={ FoodIngredients } />
+        <Route path="/explorar/comidas/area" component={ FoodAreaExp } />
+        <Route path="/explorar/bebidas/area" component={ NotFound } />
+        <Route path="/comidas/:id" render={ (props) => <FoodsDet { ...props } /> } />
+        <Route path="/bebidas/:id" render={ (props) => <DrinksDet { ...props } /> } />
+        <Route path="/explorar/comidas" component={ FoodExp } />
+        <Route path="/explorar/bebidas" component={ DrinkExp } />
+        <Route path="/comidas" component={ Foods } />
+        <Route path="/bebidas" component={ Drinks } />
+        <Route path="/explorar" component={ Explore } />
+        <Route path="/perfil" component={ Profile } />
+        <Route path="/receitas-feitas" component={ DoneRecipes } />
+        <Route path="/receitas-favoritas" component={ FavoriteRecipes } />
       </Switch>
     </Router>
   );
---
+++ src/pages/Details/FoodDet.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MenuInferior from '../../components/MenuInferior';
+
+export default function DrinkIngredients() {
+  return (
+    <div>
+      <MenuInferior />
+    </div>
+  );
+}
