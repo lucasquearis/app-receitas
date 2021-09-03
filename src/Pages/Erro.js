8afad37839d1d27eaffbf9f32566671f8f7fba5d
Josimar Souza <contact.josimarsouza@gmail.com>
2021-09-03 11:14:39 -0300

    Estiliza pagina de erro

--- src/Pages/Erro.js
+++ src/Pages/Erro.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Error from '../images/Error.gif';
+import './Erro.css';
 
 export default function Erro() {
   return (
-    <h1>Not Found</h1>
+    <div className="error-section">
+      <h1>Not Found</h1>
+      <h3>Que tal pedir uma pizza?</h3>
+      <img src={ Error } alt="Error gif" />
+    </div>
   );
 }
