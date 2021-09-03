4302c16a21c2938e158593a118cfe9b9ea5eeb6f
Rodrigo Merlone <alestori7@gmail.com>
2021-09-03 13:17:32 -0300

    [Grupo 24] [Styles] [Done] (#1072)
    * botões de categorias
    
    * detalhes comidas
    
    * detalhes comidas pronto
    
    * detalhes bebidas
    
    * drinks in progress
    
    * erro lint
    
    * food in progress
    
    * done
    
    * erro lint
    
    * erros lint
    
    * socorro
    
    * ESLINT DE VOLTA

--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -36,49 +36,64 @@ function RecipesDone() {
     const types = type === 'comida' ? 'comida' : 'bebida';
 
     return (
-      <div key={ index } className="recipes-card">
-        <img
-          src={ image }
-          alt="recipe"
-          data-testid={ `${index}-horizontal-image` }
-          width="250"
-        />
-        <div>
-          {alcoholicOrNot === ''
-            ? (
-              <p
-                data-testid={ `${index}-horizontal-top-text` }
-              >
-                { `${area} - ${category}` }
-              </p>)
-            : (
-              <p
-                data-testid={ `${index}-horizontal-top-text` }
-              >
-                { alcoholicOrNot }
-              </p>)}
+      <div
+        key={ index }
+        className="done-recipe-card"
+      >
+        <div className="name-photo">
+          <img
+            src={ image }
+            alt="recipe"
+            data-testid={ `${index}-horizontal-image` }
+            className="done-img"
+          />
+          <h4
+            data-testid={ `${index}-horizontal-name` }
+          >
+            { name }
+          </h4>
+          <div>
+            {alcoholicOrNot === ''
+              ? (
+                <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  { `${area} - ${category}` }
+                </p>)
+              : (
+                <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  { alcoholicOrNot }
+                </p>)}
+          </div>
         </div>
-        <p
-          data-testid={ `${index}-horizontal-name` }
-        >
-          { name }
-        </p>
-        <p
-          data-testid={ `${index}-horizontal-done-date` }
-        >
-          { doneDate }
-        </p>
+        <div className="done-details">
+          <strong>Feita em:</strong>
+          <p
+            data-testid={ `${index}-horizontal-done-date` }
+          >
+            { doneDate }
+          </p>
+          {tags && <strong>Tags</strong>}
+          { tags && (
+            tags.map((item) => {
+              const tagItem = (
+                <p
+                  key={ item }
+                  data-testid={ `${index}-${item}-horizontal-tag` }
+                >
+                  { item }
+                </p>
+              );
+              return tagItem;
+            })) }
+        </div>
         <ShareBtn
           testid={ `${index}-horizontal-share-btn` }
           id={ id }
           type={ types }
         />
-        { !tags ? null : tags.map((item) => {
-          const tagItem = (
-            <p key={ item } data-testid={ `${index}-${item}-horizontal-tag` }>{ item }</p>
-          );
-          return tagItem;
-        }) }
       </div>
     );
   }
@@ -86,29 +101,29 @@ function RecipesDone() {
   return (
     <div>
       <HeaderWithoutSearch>Receitas Feitas</HeaderWithoutSearch>
-      <nav>
+      <div className="cat-btns">
         <Button
-          variant="success"
+          variant="info"
           type="button"
           data-testid="filter-by-all-btn"
         >
           All
         </Button>
         <Button
-          variant="success"
+          variant="info"
           type="button"
           data-testid="filter-by-food-btn"
         >
           Food
         </Button>
         <Button
-          variant="success"
+          variant="info"
           type="button"
           data-testid="filter-by-drink-btn"
         >
           Drinks
         </Button>
-      </nav>
+      </div>
 
       <div>
         { loading
