cb5a55ad9c17dbed9d4d2978d38ff7cd34c3a6cd
José Reginaldo Da Silva Neto <netosilv44445@gmail.com>
2021-09-03 00:06:32 -0300

    Fixes

--- src/pages/explorar/index.jsx
+++ src/pages/explorar/index.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../../components/header';
 import Footer from '../../components/Footer';
+import { ExploreButtons, ExploreWrapper } from './styles';
+import { RecipesContext } from '../../context/RecipesContext';
 
-const Explorar = ({ match: { params: { param1, param2 } } }) => (
-  <>
-    <Header title="Explorar" hideSearch routeParams={ [param1, param2] } />
-    <Footer />
-  </>
-);
-
+const Explorar = ({ match: { params: { param1, param2 } } }) => {
+  const { history } = useContext(RecipesContext);
+  if (param1 === undefined) {
+    return (
+      <>
+        <Header title="Explorar" hideSearch routeParams={ [param1, param2] } />
+        <ExploreWrapper>
+          <ExploreButtons
+            data-testid="explore-food"
+            onClick={ () => history.push('/explorar/comidas') }
+          >
+            Explorar Comidas
+          </ExploreButtons>
+          <ExploreButtons
+            data-testid="explore-drinks"
+            onClick={ () => history.push('/explorar/bebidas') }
+          >
+            Explorar Bebidas
+          </ExploreButtons>
+        </ExploreWrapper>
+        <Footer />
+      </>
+    );
+  }
+  return <h1>Loading</h1>;
+};
 Explorar.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.objectOf(String),
---
+++ src/pages/explorar/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ExploreWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 50px;
+  align-items: center;
+  height: 70vh;
+  margin-top: 80px;
+`;
+
+export const ExploreButtons = styled.button``;
