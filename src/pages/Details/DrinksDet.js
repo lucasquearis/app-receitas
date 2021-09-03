ca7f06f6c08638e7020ddca39b71b0eb4d8b32d1
MIkaela Braga <bragamikaela@gmail.com>
2021-09-03 16:27:36 -0300

    iniciando resoluçao

--- src/pages/Details/DrinksDet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Foods() {
-  return (
-    <h1>Detalhes Bebidas</h1>
-  );
-}
-
-export default Foods;
--- src/pages/Details/FoodsDet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Foods() {
-  return (
-    <h1>Detalhes Comidas</h1>
-  );
-}
-
-export default Foods;
