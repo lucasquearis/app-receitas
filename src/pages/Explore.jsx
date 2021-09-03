d9b07d317bdd600dbbb134f71c7b0ee33021d8d1
Thiago da Paz <thiagopazjunior@gmail.com>
2021-09-03 00:42:49 -0300

    Adicionando Explore.

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
 import Footer from '../components/Footer';
-import Header from '../components/Header';
 
 function Explore() {
+  const history = useHistory();
   return (
-    <div>
-      <Header />
-      <h1>Explore</h1>
+    <>
+      <div>
+        <h1 data-testid="page-title">Explorar</h1>
+        <img
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+          alt="Botão que direciona para a tela de perfil"
+        />
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="explore-food"
+          onClick={ () => history.push('/explorar/comidas') }
+        >
+          Explorar Comidas
+        </button>
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          onClick={ () => history.push('/explorar/bebidas') }
+        >
+          Explorar Bebidas
+        </button>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
